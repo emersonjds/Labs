4beb06c0da14bab779be90e4103dc1737dc8deb5
Emerson Jr <emerson_jds@live.com>
2016-10-21 00:44:52 -0200

    Working with Events and Layers

---
+++ javascript/js-course/assets/js/script.js
@@ -0,0 +1,9 @@
+function changeOver () {
+	//troca a classe quando o mouse estiver em cima do elemento
+	document.getElementById("myElement").className = "h1MouseOver"; 
+}
+
+function changeOut () {
+	//troca a classe quando o mouse sair de cima do elemento
+	document.getElementById("myElement").className = "h1Mouse";
+}
