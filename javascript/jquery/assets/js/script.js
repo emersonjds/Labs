e6b57cb110ebee63b985114aa4382e6e2af9d1af
Emerson Jr <emerson_jds@live.com>
2016-11-12 21:23:35 -0200

    Jquery working with pseudoelements

--- javascript/jquery/assets/js/script.js
+++ javascript/jquery/assets/js/script.js
@@ -1,5 +1,5 @@
 var aposInicializado = function () {
-	var totalItens = $('.preco');
+	var totalItens = $('.preco:visible');
 	var valorTotal = 0;
 	console.log(totalItens);
 
@@ -9,36 +9,78 @@ var aposInicializado = function () {
 		valorTotal += preco;
 	}
 
-	$('#valor-total').text(valorTotal);
-	$('#quantidade-de-itens').text(totalItens.length);
+	$('.valor-total').text(valorTotal);
+	$('.quantidade-de-itens').text(totalItens.length);
+
+	var umaPropaganda = function(){
+		var propagandas = ["O que acha de comprar uma motocicleta?",
+		"O que acha de comprar uma lancha?",
+		"O que acha de comprar uma bicicleta?",
+		"O que acha de comprar uma carro?"
+		];
+
+		var posicao = Math.floor(propagandas.length *Math.random());
+		var texto = propagandas[posicao];
+		var tr =$("<tr>").addClass("propaganda").append($("<td>"));
+		tr.find("td").attr("colspan", 6).text(texto);
+		return tr;
+	}
+
+	$(this).find("tr:nth-child(3n), tr:last").each(function() { //subindo pela arvore a partir do enesimo elemento
+		umaPropaganda().insertAfter($(this));
+	});
 }
 
 var removeItem = function(event) {
-
 	event.preventDefault(); //setando valor padrao de comportamento
 
-	var quantidadeComoString = $("#quantidade-de-itens").text();
+	var quantidadeComoString = $(".quantidade-de-itens").text();
 	var atual = parseInt(quantidadeComoString);
 	var novaQuantidade = atual - 1; 
 	
-	$("#quantidade-de-itens").text(novaQuantidade); 
+	$(".quantidade-de-itens").text(novaQuantidade); 
 
 	var self = $(this);
 	self.closest('tr').hide(); //sobe a hirarquia na arvore de DOM até o elemento especificado
 
-	var valorTotal = parseFloat($('#valor-total').text());
+	var valorTotal = parseFloat($('.valor-total').text());
 	var preco = parseFloat(self.closest('tr').find('.preco').text());
 	var precoFinal = valorTotal - preco;
-	$('#valor-total').text(precoFinal);
+	$('.valor-total').text(precoFinal);
 }
 
 var undo = function() {
-	$('tr:visible').removeClass('recuperado'); //setando por default a retirada da classe de mercação
+	var carrinho = $(this).closest('.carrinho');
+	carrinho.find('tr:visible').removeClass('recuperado'); //setando por default a retirada da classe de mercação
 
-	var trs = $('tr:hidden'); //pegar todas as trs com propriedade de hide
-	trs.show().addClass('recuperado')
+	var trs = carrinho.find('tr:hidden'); //pegar todas as trs com propriedade de hide
+	trs.show().addClass('recuperado');
+
 };
 
+var daDestaque = function() {
+	$(this).find('.remove-item').fadeIn();
+	$(this).addClass('hovering');
+}
+
+var tiraDestaque = function () {
+	$(this).find('.remove-item').fadeOut();
+	$(this).removeClass('hovering');
+}
+
+var alternaPropaganda = function (event) {
+	event.preventDefault();
+	$('.propaganda').fadeToggle();
+	$('.alternaPropaganda').fadeToggle();
+}
+
+
 $(aposInicializado);
-$('#undo').click(undo);
-$('.remover').click(removeItem);
+$('.undo').click(undo);
+$('.remove-item').click(removeItem);
+$('.alternaPropaganda').click(alternaPropaganda);
+$('tr').hover(daDestaque, tiraDestaque);
+
+// $('tbody tr').on("mouseenter", .hovering); //utilizando funçoes de evento
+// $('tbody tr').on("mouseleave", .hovering);
+
