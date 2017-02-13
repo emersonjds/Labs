34c74d40c5f039c666aac9801cee918df5306e88
Emerson Jr <emerson_jdss@hotmail.com>
2017-02-13 00:36:11 -0200

    Working with tuple and dictionary

---
+++ python/dictionary.py
@@ -0,0 +1,7 @@
+tipo_convites = ['vip', 'normal', 'meia', 'cortesia']; 
+#Desta forma o array declarado ẽ mutavel, a lista tem que ser refeita dependendo do modelo de negocio
+
+tipo_convites = ('vip', 'normal', 'meia', 'cortesia');
+#Ja uma lista declarada com tupple nao permite acesso ao metodo append nem ao metodo remove
+#o comportamento se equipara a uma constante
+
