23955d83a6abb5ceb8a107d09ddce6fa487bf730
Emerson Jr <emerson_jdss@hotmail.com>
2017-02-14 01:02:54 -0200

    Atribute Operators

---
+++ javascript/xti-js/js/attributeOperators.js
@@ -0,0 +1,24 @@
+/*
+Attribute Operators
+*/
+
+x = 6;
+
+x = x + 3;
+
+console.log(x);
+
+x += 3;
+console.log(x);
+
+x -= 3;
+console.log(x);
+
+x *= 3;
+console.log(x);
+
+x /= 3;
+console.log(x);
+
+x %= 3;
+console.log(x);
