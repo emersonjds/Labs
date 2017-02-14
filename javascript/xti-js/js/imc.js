d693ef54f00e3299567a0670131c9585f0da8b92
Emerson Jr <emerson_jdss@hotmail.com>
2017-02-14 10:09:57 -0200

    First Exercises

---
+++ javascript/xti-js/js/imc.js
@@ -0,0 +1,11 @@
+
+
+console.log(peso);
+
+function calcularIMC(e) {
+	var formulario = document.getElementById(formulario);
+
+	e.preventDefault();
+	peso = formulario.peso.value();
+	console.log(peso);
+}
