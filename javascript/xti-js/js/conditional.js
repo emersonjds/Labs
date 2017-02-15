a7d35490fee09182e70c3d487e773402cc19b654
Emerson Jr <emerson_jdss@hotmail.com>
2017-02-15 02:08:43 -0200

    Conditional Structures

---
+++ javascript/xti-js/js/conditional.js
@@ -0,0 +1,17 @@
+/*
+	Conditional Structures
+ */
+
+var idade = 30;
+
+if (idade <= 11) 
+	console.log("criança");
+else if (idade >= 18)
+	console.log("adulto");
+
+var passou = true;
+
+if (passou == true)
+	console.log("aprovado")
+else 
+	console.log("reprovado")
