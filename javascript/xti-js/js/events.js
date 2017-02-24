a8d02ff27c61156efea43dcd8418003d283fca84
Emerson Jr <emerson_jdss@hotmail.com>
2017-02-23 23:44:46 -0300

    Understanding eventListeners

--- javascript/xti-js/js/events.js
+++ javascript/xti-js/js/events.js
@@ -1,4 +1,14 @@
 
+
+//DOM NIVEL 0
+window.onload = function () {
+	var botao = document.getElementById("botao");
+	botao.addEventListener("click", funcao, false);
+	//primeiro parametro evento, segundo funcao, e terceiro se quer tratar o evento com propagacao
+}
+
+
+
 function funcao() {
 	alert("Ola");
 }
