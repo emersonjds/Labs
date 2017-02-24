4616a0e648ccdc2d3ecce7fc0b232847014fdbdb
Emerson Jr <emerson_jdss@hotmail.com>
2017-02-24 02:18:42 -0300

    Learn events

--- javascript/xti-js/js/events.js
+++ javascript/xti-js/js/events.js
@@ -5,10 +5,20 @@ window.onload = function () {
 	var botao = document.getElementById("botao");
 	botao.addEventListener("click", funcao, false);
 	//primeiro parametro evento, segundo funcao, e terceiro se quer tratar o evento com propagacao
+	botao.addEventListener("click", funcao1, false);
+
+	botao .onclick = function(evt) {
+		var oEvento = evt ? evt : window.event;
+		alert("x =" + oEvento.screenX + " Y = " + oEvento.screenY);
+	}
 }
 
-
+//com a utilização do addEventListener é possivel adicionar 2 eventos a uma mesma ação
 
 function funcao() {
 	alert("Ola");
+}
+
+function funcao1() {
+	alert("Ola 2");
 }
