b6e7f81c4371563a8cced74b8b33db85494479bc
Emerson Jr <emerson_jdss@hotmail.com>
2017-02-26 00:00:01 -0300

    Call function method

--- javascript/xti-js/js/functionMethods.js
+++ javascript/xti-js/js/functionMethods.js
@@ -17,10 +17,27 @@ function apartamentoVenda(local, quantidade, vendidos) {
 zonaSulDescricao = new apartamentoDescricao("Zona Sul", 4, 'sim');
 zonaSulVendas = new apartamentoVenda("Zona Sul", 12, 7);
 
-alert(zonaSulDescricao.local + " " + zonaSulDescricao.quarto + " " + zonaSulDescricao.garagem + '\n' + 
-	zonaSulVendas.local + " " + zonaSulVendas.quantidade + " " + zonaSulVendas.vendidos);
+// alert(zonaSulDescricao.local + " " + zonaSulDescricao.quarto + " " + zonaSulDescricao.garagem + '\n' + 
+// 	zonaSulVendas.local + " " + zonaSulVendas.quantidade + " " + zonaSulVendas.vendidos);
 
 
+function montadora(fabricacao, marca, ano, cor) {
+	this.fabricacao = fabricacao;
+	this.marca = marca;
+	this.ano = ano;
+	this.cor = cor;
+}
+
+function Carro(marca, ano, modelo) {
+	this.marca = marca;
+	this.ano = ano;
+	this.modelo = modelo;
+	montadora.call(this, fabricacao, marca, ano, cor);
+}
+
+carroZero = new Carro('Nissan', '1989', 'Tiida', 'Bahia', 'vermelho');
+
+console.log(carroZero);
 
 
 
