80a089eb3c7a83feebc4d6f06a9d58584eb583f5
Emerson Jr <emerson_jdss@hotmail.com>
2017-03-12 01:41:42 -0300

    Working with addEventListener and events shortcut

--- javascript/js/js/script.js
+++ javascript/js/js/script.js
@@ -35,6 +35,56 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
+var adicionarPaciente = document.querySelector("#adicionar-paciente");
+
+console.log(linha);
+
+
+adicionarPaciente.addEventListener("click", function(e){
+    e.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nomeDigitado = form.nome.value;
+    var pesoDigitado = form.peso.value;
+    var alturaDigitada = form.altura.value;
+    var taxaGordura = form.gordura.value;
+
+    var linha = document.createElement("tr");
+
+    var nomeTr = document.createElement("td");
+    var pesoTr = document.createElement("td");
+    var alturaTr = document.createElement("td");
+    var taxaGorduraTr = document.createElement("td");
+    var imcTr = document.createElement("td");
+
+    nomeTr.textContent = nomeDigitado;
+    pesoTr.textContent = pesoDigitado;
+    alturaTr.textContent = alturaDigitada;
+    taxaGorduraTr.textContent = taxaGordura;
+    imcTr.textContent = 0 ;
+    
+    linha.appendChild(nomeTr);
+    linha.appendChild(pesoTr);
+    linha.appendChild(alturaTr);
+    linha.appendChild(taxaGorduraTr);
+    linha.appendChild(imcTr);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(linha);
+
+    limpaCampos();
+});
+
+
+function limpaCampos() {
+    var nomeDigitado = " ";
+    var pesoDigitado = " ";
+    var alturaDigitada = " ";
+    var taxaGordura = " ";
+}
+
 
 
 
