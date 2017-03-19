da791a9853107e053e4ae8b43646dc6121206327
Emerson Jr <emerson_jdss@hotmail.com>
2017-03-19 00:07:58 -0300

    Final Course

--- javascript/js/js/buscarPaciente.js
+++ javascript/js/js/buscarPaciente.js
@@ -6,12 +6,18 @@ botaoAdicinar.addEventListener("click", function () {
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function () { //caso haja a conexao externa e os dados sejam trazidos, mostra os dados
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
-        
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+
+        if (xhr.status == 200) { //valida se a requisicão chegou na api
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status); //status de erro
+            console.log(xhr.responseText); 
+        }
     });
 
     xhr.send(); //enviar requisição
