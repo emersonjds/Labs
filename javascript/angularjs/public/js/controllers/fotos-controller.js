5cc243dbc613aced1d0b6b8d5a767227ba5b92a0
Emerson Jr <emerson_jdss@hotmail.com>
2017-03-27 23:51:39 -0300

    Integration with backend

--- javascript/angularjs/public/js/controllers/fotos-controller.js
+++ javascript/angularjs/public/js/controllers/fotos-controller.js
@@ -1,7 +1,26 @@
-angular.module('alurapic').controller('FotosController', function($scope) { //primeiro parametro nome do Controller e o segundo a funcao                                                                  que o define
-     $scope.foto = {
-        titulo : 'Leao',
-        url : 'http://lionsbrusquebercofiacao.org.br/wp-content/uploads/2015/12/leao.png'
-    }
+angular.module('alurapic').controller('FotosController', function ($scope, $http) { //primeiro parametro nome do Controller e o segundo a funcao                                                                  que o define
+    $scope.fotos = [];
+
+    $http.get('v1/fotos')
+        .success(function () {
+            $scope.fotos = fotos;
+        }).error(function(err){
+            console.log(err);
+    });
+
+    // var promise = $http.get('v1/fotos');
+
+    // promise.then(function(retorno) {
+    //     $scope.fotos = retorno.data; 
+    // }).catch(function(err){
+    //     console.log(err);
+    // });
+
 });
+
+
+//$scope -> atraves dele a controller consegue se comunicar com a view passando seu escopo interno
+//$http -> rsponsavel pela execução de requests no angular
+//promise -> promessa de que vai buscar os dados e caso haja sucesso retorna os dados com o metodo then
+    //caso contrario chama um cath para informar o erro 
 
