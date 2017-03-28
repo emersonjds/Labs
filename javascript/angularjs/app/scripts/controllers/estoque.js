e3f1fabd04bfadafcf6bbccc023db19c75b6545b
Emerson Jr <emerson_jdss@hotmail.com>
2017-03-27 22:36:09 -0300

    Configurando a arquitetura da aplicação

--- javascript/angularjs/app/scripts/controllers/estoque.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	var EstoqueController = function($scope) {
-
-	};
-
-	EstoqueController.inject = ['$scope'];
-
-	angular.module('myApp').controller('EstoqueController', EstoqueController);
-})();
--- javascript/angularjs/app/scripts/controllers/home.js
+++
@@ -1,8 +0,0 @@
-(function (){
-	var HomeController = function($scope) {
-	};
-
-	HomeController.$inject = ['$scope'];
-
-	angular.module('myApp').controller('HomeController', HomeController);
-})();
