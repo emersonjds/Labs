8942c15cd9a9a27cf6b7251a1d0867320f70bf87
Emerson Jr <emerson_jdss@hotmail.com>
2017-03-27 23:02:42 -0300

    Understanding MVVW

---
+++ javascript/angularjs/public/js/controllers/fotos-controller.js
@@ -0,0 +1,7 @@
+angular.module('alurapic').controller('FotosController', function($scope) { //primeiro parametro nome do Controller e o segundo a funcao                                                                  que o define
+     $scope.foto = {
+        titulo : 'Leao',
+        url : 'http://lionsbrusquebercofiacao.org.br/wp-content/uploads/2015/12/leao.png'
+    }
+});
+
