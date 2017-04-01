f5830efa122f2d3b305c73780d291d1fd37f5024
Emerson Jr <emerson_jdss@hotmail.com>
2017-04-01 16:24:32 -0300

    solved bug version angular

--- javascript/angularjs/http.js
+++ javascript/angularjs/http.js
@@ -3,7 +3,7 @@ var router = require('./router');
 var app = router(3412);
 
 var operadoras = [
-    {nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
+    {nome: "Oi", codigo: 14, categoria: "Celular", preco: 1},
     {nome: "Vivo", codigo: 11, categoria: "Celular", preco: 2},
     {nome: "Claro", codigo: 42, categoria: "Celular", preco: 3},
 ];
