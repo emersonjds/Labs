117fabfc37ba1eaf0eee0b504e33e2b0b17619a7
Emerson Jr <emerson_jdss@hotmail.com>
2017-06-09 15:33:23 -0300

    data of the component for view

--- javascript/angular2- @Loiane/primeiro/src/app/app.module.ts
+++ javascript/angular2- @Loiane/primeiro/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FirstComponent} from './first-component/first-component'
+import { FirstComponent } from './first-component/first-component'
 
 @NgModule({
   declarations: [
--- javascript/angular2- @Loiane/primeiro/src/app/first-component/first-component.ts
+++ javascript/angular2- @Loiane/primeiro/src/app/first-component/first-component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core'
   templateUrl: 'first-component.html'
 })
 
-class FirstComponent {
-
+export class FirstComponent {
+  titulo: string = 'dado da model pra view'
 }
