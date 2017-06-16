34ddcb74498b60be26c20a39856ec498c5652ce2
Emerson Jr <emerson_jdss@hotmail.com>
2017-06-16 00:21:52 -0300

    Refactor code Two way Data bind

--- javascript/angular2- @Loiane/data-binding/src/app/data-binding/data-binding.component.ts
+++ javascript/angular2- @Loiane/data-binding/src/app/data-binding/data-binding.component.ts
@@ -13,6 +13,7 @@ export class DataBindingComponent implements OnInit {
   valorAtual: string = ''
   valorSalvo: string = ''
   isMouseOver: boolean = false
+  nome: string = 'abc'
 
   getValor() {
     return 1
---
+++ javascript/angular2- @Loiane/data-binding/src/app/meu-form/meu-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-form',
+  templateUrl: './meu-form.component.html',
+  styleUrls: ['./meu-form.component.css']
+})
+export class MeuFormComponent implements OnInit {
+
+  pessoa: any = {
+    nome: 'Emerson',
+    idade: 27
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
