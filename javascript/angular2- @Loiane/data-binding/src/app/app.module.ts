49fcc4078e0647795a8581e6aa85393edc744d6d
Emerson Jr <emerson_jdss@hotmail.com>
2017-06-15 21:01:43 -0300

    refactor code

--- javascript/angular2- @Loiane/data-binding/src/app/app.module.ts
+++ javascript/angular2- @Loiane/data-binding/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataBindingComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
