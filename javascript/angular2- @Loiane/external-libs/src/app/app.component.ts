f533eef412299ef3a9bcd6000bcb3e55b1c86ce4
Emerson Jr <emerson_jdss@hotmail.com>
2017-06-20 17:15:01 -0300

    working with lodash

--- javascript/angular2- @Loiane/external-libs/src/app/app.component.ts
+++ javascript/angular2- @Loiane/external-libs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  list = _.map([1,2,3], (n) => `#${n}` )
 }
