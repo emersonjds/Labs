2a8ae9ae4cc34fbe2e79316644973883921de5f7
Emerson Jr <emerson_jdss@hotmail.com>
2017-06-26 23:54:07 -0300

    Solved fixed bug parameter error

--- javascript/angular2- @Loiane/directives/src/app/shared/yellow-bg.directive.spec.ts
+++ javascript/angular2- @Loiane/directives/src/app/shared/yellow-bg.directive.spec.ts
@@ -1,8 +1,2 @@
 import { YellowBgDirective } from './yellow-bg.directive';
 
-describe('YellowBgDirective', () => {
-  it('should create an instance', () => {
-    const directive = new YellowBgDirective();
-    expect(directive).toBeTruthy();
-  });
-});
