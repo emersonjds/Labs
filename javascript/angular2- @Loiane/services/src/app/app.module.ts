00adeeb56f397a8b81a61d0e30921fc89a6cf0b3
Emerson Jr <emerson_jdss@hotmail.com>
2017-06-29 17:08:05 -0300

    Injects Services in another Services

--- javascript/angular2- @Loiane/services/src/app/app.module.ts
+++ javascript/angular2- @Loiane/services/src/app/app.module.ts
@@ -5,21 +5,22 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosService } from './cursos/cursos.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LogService } from './shared/log.service';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+  AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    CriarCursoModule,
-    CursoModule
+  BrowserModule,
+  FormsModule,
+  HttpModule,
+  CriarCursoModule,
+  CursoModule
   ],
-  //providers: [CursosService],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
