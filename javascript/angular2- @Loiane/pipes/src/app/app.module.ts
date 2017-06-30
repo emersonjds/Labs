80d9c6e2590285c4e7899733637d0a02f0006b0e
Emerson Jr <emerson_jdss@hotmail.com>
2017-06-30 00:01:43 -0300

    Create another pipes

--- javascript/angular2- @Loiane/pipes/src/app/app.module.ts
+++ javascript/angular2- @Loiane/pipes/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CamelCasePipe } from './camel-case.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PipeExamplesComponent
+    PipeExamplesComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule
