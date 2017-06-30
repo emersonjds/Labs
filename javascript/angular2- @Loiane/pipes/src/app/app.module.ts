6cece6bdc2a8c965c6e4552d44609ff8bc1c8a6a
Emerson Jr <emerson_jdss@hotmail.com>
2017-06-30 01:48:58 -0300

    Create Service for a Pipe

--- javascript/angular2- @Loiane/pipes/src/app/app.module.ts
+++ javascript/angular2- @Loiane/pipes/src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { SettingsService } from './settings.service';
+
 import { PipeExamplesComponent } from './pipe-examples/pipe-example.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CamelCasePipe } from './camel-case.pipe';
@@ -15,7 +17,20 @@ import { CamelCasePipe } from './camel-case.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService) => {
+        settingsService.getLocale()
+      }
+    }
+    // {
+    //   provide: LOCALE_ID,
+    //   useValue: 'pt-BR'
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
