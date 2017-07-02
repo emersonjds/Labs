acc3ea7cb3262789f8ee1a9bf3c3de047d944985
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-02 18:13:00 -0300

    working with params in routes

--- javascript/angular2- @Loiane/routing/src/app/app.module.ts
+++ javascript/angular2- @Loiane/routing/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { MaterializeModule } from 'angular2-materialize';
 import { CursosComponent } from './cursos/cursos.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CursosComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
--- javascript/angular2- @Loiane/routing/src/app/app.routing.ts
+++ javascript/angular2- @Loiane/routing/src/app/app.routing.ts
@@ -1,14 +1,17 @@
-import { ModuleWithProviders } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
 
+import { ModuleWithProviders } from '@angular/core';
 import { CursosComponent } from './cursos/cursos.component'
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 const APP_ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'cursos', component: CursosComponent }
+  { path: 'cursos', component: CursosComponent },
+  { path: 'cursos/:id', component: CursoDetalheComponent } // : siginifica o nome do parametro passado na rota
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES); // rota para raiz da aplicação
