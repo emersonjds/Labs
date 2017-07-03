130947b435250c4fe3650445397b463572037153
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-02 22:45:04 -0300

    Listen change of paramns

--- javascript/angular2- @Loiane/routing/src/app/curso-detalhe/curso-detalhe.component.ts
+++ javascript/angular2- @Loiane/routing/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/RX';
 
 @Component({
-  selector: 'app-curso-detalhe',
-  templateUrl: './curso-detalhe.component.html',
-  styleUrls: ['./curso-detalhe.component.css']
+	selector: 'app-curso-detalhe',
+	templateUrl: './curso-detalhe.component.html',
+	styleUrls: ['./curso-detalhe.component.css']
 })
 export class CursoDetalheComponent implements OnInit {
 
-  id: string;
+	id: string;
+	incricao : Subscription;
 
-  constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
-  }
+	constructor(private route: ActivatedRoute) {
+		// this.id = this.route.snapshot.params['id'];
+		// console.log(this.route);
+	}
 
-  ngOnInit() {}
+	ngOnInit() {
+		this.incricao = this.route.params.subscribe((params: any) => {
+			this.id = params['id'];
+		})	
+	}
 
+	ngOnDestroy() {
+		this.incricao.unsubscribe();
+	}
 }
