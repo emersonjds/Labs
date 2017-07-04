8b696ddd23248c378a91480aafd358f50daf4901
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-04 11:15:09 -0300

    Working with queryUrl

--- javascript/angular2- @Loiane/routing/src/app/cursos/cursos.component.ts
+++ javascript/angular2- @Loiane/routing/src/app/cursos/cursos.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { CursosService } from './cursos.service';
 
+import { Subscription } from 'rxjs/RX';
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
@@ -11,11 +14,36 @@ import { CursosService } from './cursos.service';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private _cursosService: CursosService) { }
+  constructor(
+    private _cursosService: CursosService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
+
+  proximaPagina() {
+    //this.pagina++;
+    this._router.navigate(['/cursos'],
+    {
+      queryParams: {'pagina': ++this.pagina}
+    }
+    )
+  }
 
   ngOnInit() {
     this.cursos = this._cursosService.getCursos();
+
+    this.inscricao = this._route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina']
+      }
+    )
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 
 }
