f8d5cae921e0165efce1f73add1e430c39d30584
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-10 22:17:05 -0300

    Fixed bug files

--- javascript/c-course/caelumpic-server/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http')
-    ,app = require('./config/express')
-    ,db = require('./config/database');
-
-app.listen(3000, function() {
-    console.log('Servidor escutando na porta: ' + this.address().port);
-});
