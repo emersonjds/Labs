67a8f6f35ef846cd418e5c65320efa74ac01cd8e
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-17 00:01:04 -0300

    parse data

--- javascript/json/main.js
+++ javascript/json/main.js
@@ -1,1 +1,7 @@
-var data = { "favColor": "Orange", "favSeason": "Fall" }
+var start = '{ "favColor": "Orange", "favSeason": "Fall" }';
+
+var data = JSON.parse(start) //convert to Object
+console.log(data)
+
+var string = JSON.stringify(data) //Convert to string
+console.log(string)
