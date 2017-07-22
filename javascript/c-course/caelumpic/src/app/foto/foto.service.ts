e7bcf23e40d469bda69a80bdb67b38516fee5e52
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-22 14:38:57 -0300

    Refactor service code

--- javascript/c-course/caelumpic/src/app/foto/foto.service.ts
+++ javascript/c-course/caelumpic/src/app/foto/foto.service.ts
@@ -1,21 +1,15 @@
-import { Http, Headers } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
 import { FotoComponent } from './foto.component';
+import { Observable } from 'rxjs';
+import { Headers } from '@angular/http;
 
 export class FotoService {
+    
     http: Http;
     headers: Headers;
     url: string = 'http://localhost:3000/v1/fotos';
-    constructor(http: Http) {
-        this.http = http;
-        this.headers = new Headers();
-        this.headers.append('Content-Type', 'application/json');
-    }
-    lista() {
-        return this.http.get(this.url)
-            .map(res => res.json());
-    }
-    cadastra(foto: FotoComponent) {
-        return this.http.post(this.url, JSON.stringify(foto),
-            { headers: this.headers });
+
+    constructor(private _http: Http) {
+
     }
 }
