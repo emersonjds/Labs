432afe02e9bd2c1dc3c84e1a5cb151d3ff11b68d
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-24 20:40:12 -0300

    configure req and res

--- javascript/c-nodeJS/cdc.js
+++ javascript/c-nodeJS/cdc.js
@@ -1,7 +1,14 @@
+
+
 var http = require('http')
 
-var server = http.createServer()
+var server = http.createServer((req, res) => {
+    res.end('Testando server')
+})
+
+server.listen(3000, 'localhost');
 
 console.log('rodando');
 
-server.listen('localhost');
+
+
