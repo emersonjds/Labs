ffc8e3c966eca08789beb2d046cb5b2a4fa51806
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-24 21:52:03 -0300

    Configure .gitignore and package json

--- javascript/c-nodeJS/cdc.js
+++ javascript/c-nodeJS/cdc.js
@@ -1,13 +1,16 @@
 
 
 var http = require('http')
+var server = require('express')
 
-var server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        "Content-type": "text/html"
-    })
-    res.end('Testando server')
+server.get("/", (req, res) => {
+    res.send('Testando server')
 })
+
+server.get("/lista", (req, res) => {
+    res.send('Lista de alunos')
+})
+
 
 server.listen(3000, 'localhost', () => {
     console.log('Escutando na porta 3000')
