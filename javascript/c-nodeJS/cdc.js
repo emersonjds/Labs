7cdded74e1b9a04f1ac83dc00307e4c9ccea635e
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-24 21:31:57 -0300

    Configure Request Headers

--- javascript/c-nodeJS/cdc.js
+++ javascript/c-nodeJS/cdc.js
@@ -3,12 +3,18 @@
 var http = require('http')
 
 var server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-type": "text/html"
+    })
     res.end('Testando server')
 })
 
-server.listen(3000, 'localhost');
+server.listen(3000, 'localhost', () => {
+    console.log('Escutando na porta 3000')
+})
 
-console.log('rodando');
+
+
 
 
 
