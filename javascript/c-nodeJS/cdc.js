98bbdbd9631f648dd50f68a7fb7b33df5eef8746
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-24 22:37:25 -0300

    Read file with assyncronous functions

--- javascript/c-nodeJS/cdc.js
+++ javascript/c-nodeJS/cdc.js
@@ -1,13 +1,21 @@
 var express = require('express')
 var server = express()
+var fs = require('fs')
 
 server.get("/", (req, res) => {
     res.send('Testando server')
 })
 
-server.get("/lista", (req, res) => {
+server.get("/produtos/lista", (req, res) => {
+    fs.read("lista.html", (data) => {
+        res.send('Lista de alunos')
+    })
+})
+
+server.get("/produtos/cadastro", (req, res) => {
     res.send('Lista de alunos')
 })
+
 
 server.listen(3000, 'localhost', () => {
     console.log('Escutando na porta 3000')
