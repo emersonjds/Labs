5b0029d48c8717136a9bede06e5b5e9984d58685
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-25 20:17:35 -0300

    Config view pages

--- javascript/c-nodeJS/cdc.js
+++ javascript/c-nodeJS/cdc.js
@@ -2,21 +2,23 @@ var express = require('express')
 var server = express()
 server.set("view engine", "ejs");
 
-var fs = require('fs')
 
 server.get("/", (req, res) => {
     res.send('Testando server')
 })
 
+//assyncronous call
 server.get("/produtos/lista", (req, res) => {
-
     var livros = []
-    res.render("produtos/lista", { livros: [
-        {titulo: 'Javascript', preco: 50, descricao: 'Javascript'}
-    ] })
+   
+    res.render("produtos/lista", {
+        livros: [
+            { titulo: "Livro 1", preco: 5, descricao: 'Livro 1'}
+        ]
+    })
 })
 
-server.use(express.static("/public")) // faz a chamada para trazer os arquivos estaticos
+server.use(express.static("./public")) // faz a chamada para trazer os arquivos estaticos
 
 
 server.listen(3000, 'localhost', () => {
