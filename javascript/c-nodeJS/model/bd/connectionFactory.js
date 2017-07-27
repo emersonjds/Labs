0755a6cf29c37c768756dcbbe903d6498093140c
Emerson Jr <emerson_jdss@hotmail.com>
2017-07-27 19:42:20 -0300

    configure environment variable

--- javascript/c-nodeJS/model/bd/connectionFactory.js
+++ javascript/c-nodeJS/model/bd/connectionFactory.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql") //driver mySql
 
 function getConnetion() {
-    return mysql.createConnection({
-        hostname: "localhost",
-        user: "root",
-        password: "",
-        database: "casadocodigo",
-        port: 3306
+    
+    config = require("../../config/config")
+
+    //variavel de ambiente do node
+    //****process.env
+    
+    //cria um pool de conexao ao inves de criar uma conexao a cada requisicao
+    return mysql.createPool({
+        connectionLimit: 10,
+        hostname: config.MYSQL_DB_ADDRESS,
+        user: config.MYSQL_DB_USER,
+        password: config.MYSQL_DB_PASSWORD,
+        database: config.MYSQL_DB_NAME,
+        port: config.MYSQL_DB_PORT
     })
 }
 
