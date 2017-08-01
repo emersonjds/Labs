aaa060d615299c70ecb0d6b965a19434059fd26c
Emerson Jr <emerson.junior.silva@accenture.com>
2017-08-01 00:52:12 -0300

    Understanding states of web browser in ajax requests

--- javascript/json/main.js
+++ javascript/json/main.js
@@ -7,19 +7,34 @@ console.log(objeto.Jane.degree)
 
 window.onload = function () {
 
+  document.getElementById("message2").innerHTML = JSON.stringify(objeto.Jim)
+
   var ajax = new XMLHttpRequest()
 
   ajax.open('GET', './data.json', true);
 
   ajax.onload = () => {
-    var data = JSON.stringify(ajax.responseText) // 
+    var data = JSON.parse(ajax.responseText) // 
+    console.log(data)
     document.getElementById("message").innerHTML = data.userId
   }
 
   ajax.send()
 
+  var xhr = new XMLHttpRequest()
 
-  //document.getElementById("message").innerHTML = JSON.stringify(objeto.Jim)
+  xhr.open('GET', './people.json', true)
+  xhr.onload = () => {
+
+  }
+  xhr.send()
+
+
+  xhr.onreadystatechange = () => {
+    console.log(xhr.readyState)
+    console.log(xhr.status)
+    console.log(xhr.statusText)
+  }
 }
 
 
