339a241e1f0e19ff1a6cda1e0920bac8b06f296c
Emerson Jr <emerson.junior.silva@accenture.com>
2017-08-01 00:09:20 -0300

    Working with news notations

--- javascript/json/main.js
+++ javascript/json/main.js
@@ -1,6 +1,12 @@
-var dados = '{"Jane": {"age":"29", "degree":"MFA"}, "Jim": {"age":"29", "degree":"MFA"}}';
+var dados = '{"Jane": {"age":"29", "degree":{ "AAS":"Virginia Military Institute", "BA": "UVA" }}, "Jim": {"age":"29", "degree":"MFA"}}';
 
-var objeto = JSON.parse(dados);
+var objeto = JSON.parse(dados); // parse to object
 
-console.log(objeto.Jane.age)
+console.log(objeto.Jane.degree)
+
+
+window.onload = function(){
+  document.getElementById("message").innerHTML = JSON.stringify(objeto.Jim)
+}
+
 
