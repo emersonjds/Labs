2add23ccc0a3103942b14a1aee4c81269e4a49dd
Emerson Jr <emerson.junior.silva@accenture.com>
2017-08-02 00:47:33 -0300

    Refactor code

--- javascript/json/jsonArrays.js
+++ javascript/json/jsonArrays.js
@@ -9,8 +9,21 @@ req.onload = () => {
   if (req.status === 200) {
     var dados = JSON.parse(req.responseText)
     
-   for(var i = 0; i < dados.length; i++) {
-     console.log(dados.modelos[i]);
-   }
+    for(i = 0; i < dados.modelos.length; i++) {
+      console.log(dados.modelos[i].nome)
+      document.getElementById("modelos").innerHTML = dados.modelos[i].nome
+    }
+
+    var novosDados = ""
+
+    for (i = 0; i < dados.modelos.length; i++) {
+      var x = i + 1;
+      novosDados += 'numero do modelo ' + x + " :" 
+      novosDados += dados.modelos[i].nome 
+      novosDados += dados.modelos[i].ano + "<br>"
+
+      console.log(novosDados)
+    }
+    document.getElementById("modelos").innerHTML = novosDados
   }
 }
