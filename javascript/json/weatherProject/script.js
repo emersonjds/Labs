5c5cdbed5209dbb5353e7df57067d7fe218354fe
Emerson Jr <emerson_jdss@hotmail.com>
2017-08-16 22:19:17 -0300

    Config path for API

--- javascript/json/weatherProject/script.js
+++ javascript/json/weatherProject/script.js
@@ -15,9 +15,11 @@ weatherConditions.onload = () => {
     if (weatherConditions.status === 200) {
         cObj = JSON.parse(weatherConditions.responseText);
         console.log(cObj);
+        document.getElementById('cidade').innerHTML = cObj.current_observation.display_location.full;
+        document.getElementById('condicoes').innerHTML = cObj.current_observation.weather;
+        document.getElementById('temp').innerHTML = cObj.current_observation.temp_f + " Farenheit"
     }
 }
-
 
 weatherForecast.open('GET', 'http://api.wunderground.com/api/b8272d3f8bd0120c/forecast/q/32789.json', true);
 weatherForecast.responseType = 'text';
@@ -27,6 +29,8 @@ weatherForecast.onload = () => {
     if (weatherForecast.status === 200) {
         fObj = JSON.parse(weatherForecast.responseText);
         console.log(fObj);
+
+        document.getElementById('deic').innerHTML = fObj.forecast.txt_forecast.forecastday["0"].fcttext;
     }
 }
 
