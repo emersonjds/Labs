c4aa3f07370d6c04602fdc2368ad252a59a5e6ff
Emerson Jr <emerson_jdss@hotmail.com>
2017-08-20 19:39:17 -0300

    Config app

--- javascript/u-courses/angular2/cadastro-contatos/app/app.component.ts
+++ javascript/u-courses/angular2/cadastro-contatos/app/app.component.ts
@@ -2,7 +2,13 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Primeira pagina</h1>'
+  template: `
+    <h1>Primeira pagina</h1>'
+
+    <br/>
+
+    <button class="btn btn-primary">Testando botao</button>
+  `
 })
 export class AppComponent {
   constructor() {
