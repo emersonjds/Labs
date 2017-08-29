e57aef71488de76d2b820872298daecaf6539b0d
Emerson Silva <emerson_jdss@hotmail.com>
2017-08-28 23:15:02 -0300

    config a navbar

--- javascript/u-courses/angular2/cadastro-contatos/app/app.component.ts
+++ javascript/u-courses/angular2/cadastro-contatos/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-    <h1>Primeira pagina</h1>'
-
-    <br/>
-
-    <router-outlet></router-outlet>
-
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
