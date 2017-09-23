5be7f743f431af79606f8b1580116b5676a8d135
Emerson Silva <emerson_jdss@hotmail.com>
2017-09-23 13:04:32 -0300

    Error treatment

--- javascript/maujor-book-js/cap-3/errors.js
+++ javascript/maujor-book-js/cap-3/errors.js
@@ -1,7 +1,16 @@
 try {
     /// possivel exception
-} catch(e) {
+} catch (e) {
     console.log(e)
 } finally () {
     // script qualquer
+}
+
+try {
+    var x = u;
+    var y = x + 2;
+    console.log(y)
+} catch (e) {
+    console.log(e.message + "\n" + e.name + "\n" + e.toString())
+    //atributos do objeto Error -> message, name 
 }
