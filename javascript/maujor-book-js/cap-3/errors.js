d375a8c8a916e288e095c0f198d7e249f14c0c30
Emerson Silva <emerson_jdss@hotmail.com>
2017-09-23 13:11:13 -0300

    treatment with default throw error use

--- javascript/maujor-book-js/cap-3/errors.js
+++ javascript/maujor-book-js/cap-3/errors.js
@@ -14,3 +14,24 @@ try {
     console.log(e.message + "\n" + e.name + "\n" + e.toString())
     //atributos do objeto Error -> message, name 
 }
+
+//Trow treatment
+
+var z = "Entre com um numero maior do que 0 e menor do que 10"
+
+var x = 0;
+try {
+    if (x < 0)
+        throw ("erro 1")
+    if (x > 10)
+        throw ("erro 2")
+    if (isNaN(parseInt(x)))
+        throw ("erro 3")
+} catch (error) {
+    if (error == "erro 1")
+        console.log("O numero deve ser maior que 0")
+    if (error == "erro 2")
+        console.log("O numero deve ser menor que 11")
+    if (error == "erro 3")
+        console.log("Entre comente com numeto maior que e menor que 11")
+}
