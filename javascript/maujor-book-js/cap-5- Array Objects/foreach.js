304f32b749a750339863ded316324305dc0ddea0
Emerson Silva <emerson_jdss@hotmail.com>
2017-09-24 18:52:13 -0300

    Starts with foreach

---
+++ javascript/maujor-book-js/cap-5- Array Objects/foreach.js
@@ -0,0 +1,9 @@
+//FOREACH
+// Percorre cada elemento do array retornando um callback
+var arr = [3, 4, 5, "a", "b"]
+arr.forEach((dado) => {
+    if (dado % 2 == 0) {
+        console.log(dado + 10)
+    }
+    return dado;
+})
