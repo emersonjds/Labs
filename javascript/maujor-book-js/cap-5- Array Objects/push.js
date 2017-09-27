be4eb3b6090dc986e38ef7b6a621df047ce21bd6
Emerson Silva <emerson_jdss@hotmail.com>
2017-09-26 23:47:46 -0300

    push method

---
+++ javascript/maujor-book-js/cap-5- Array Objects/push.js
@@ -0,0 +1,6 @@
+//push(arg1, arg2 ... arg10)
+//Acrescenta os argumentos no final de um array e retorna a nova quantidade de elementos do array
+
+var array = [1, 2, 3, 4, 5]
+var a = array.push("b", "c", "d", "e")
+console.log(a) // retorna 9 
