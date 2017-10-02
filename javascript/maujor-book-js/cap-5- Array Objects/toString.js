ce76e151dbe8fb342746f3c765995c59f13cfccf
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-01 21:45:58 -0300

    toString array

---
+++ javascript/maujor-book-js/cap-5- Array Objects/toString.js
@@ -0,0 +1,5 @@
+//O metoto toString() converte cada elemento do array em uma string e retorna uma lista de elementos separados por , tem o mesmo efeito do metodo join() quando usado sem argumento
+//O metodo toLocaleString() é uma versao localizada do metodo anterior, fazendo a conversao para string e usando separadores de acordo com as caracteristicas locais do dispositivo interpretador , nao pratica nao ha diferença entre os metodos
+
+var arr = [16, 7, 18, 19]
+arr.toString() // retorna "16,7,18,19"
