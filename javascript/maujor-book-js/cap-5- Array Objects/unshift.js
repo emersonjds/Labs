9ab1f92cbfab8c9161edf7854d0a66bbbe338126
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-01 21:47:51 -0300

    unshift

---
+++ javascript/maujor-book-js/cap-5- Array Objects/unshift.js
@@ -0,0 +1,7 @@
+//Esse metodo insere elementos no inicio de um array e retorna uma nova quantidade de elementos do array (array.length)
+
+unshift(args)
+
+var arr = [1,2,"a", "b"]
+var a = arr.unshift(2,4,9)
+console.log(a) // Resulta em 7 
