64d05cf32b5edb7b31009d8f1dc65c692e1d34a7
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-02 23:57:16 -0300

    Function

---
+++ javascript/maujor-book-js/cap-6 - Functions/function.js
@@ -0,0 +1,15 @@
+//Criação de funcao
+
+//declarativa
+function nome() {
+    //bloco de codigo    
+}
+
+//utilizando objeto construtor
+var minhaFuncao = new function(){}//bloco de codigo dentro do paranteses
+
+//sintaxe literal ou pure function
+var funcaoLiteral = function() {
+    //bloco de codigo
+}
+
