59ffdf5a68803e997e48a152745bd0eb8534229f
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-08 14:49:37 -0300

    Objects returns and arrays

---
+++ javascript/maujor-book-js/cap-6 - Functions/returnArrayObject.js
@@ -0,0 +1,9 @@
+
+//Return array object
+function calculaRetagulo(b, h) {
+    var area = b*h;
+    var perimentro = (b+h) * 2
+    return [area, perimetro]
+}
+
+calculaRetagulo(10,20) // return array []
