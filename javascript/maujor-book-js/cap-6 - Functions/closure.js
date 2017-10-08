22c04558e3435f7ccf3c37200c56c72404b0362c
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-08 16:23:26 -0300

    Closure escapes

--- javascript/maujor-book-js/cap-6 - Functions/closure.js
+++ javascript/maujor-book-js/cap-6 - Functions/closure.js
@@ -24,3 +24,59 @@ function funcaoExterna() {
 }
 funcaoExterna() // retorna Função externa e Função Interna
 
+// ----------------------------------- // 
+//Exemplo 3 - Escape Closure
+
+var global;
+
+function funcaoExterna() {
+    console.log(`Função externa`)
+    function funcaoInterna() {
+        console.log(`Função interna`)
+    }
+    global = funcaoInterna; // referenciando a closure
+}
+
+funcaoExterna() // funcao Externa
+global() // funcao interna
+
+// ----------------------------------- // 
+//Exemplo 4 - Escape Closure with return
+
+function funcaoExterna() {
+    console.log('Funcao externa')
+    function funcaoInterna() {
+        console.log('Funcao Interna')
+    }
+    return funcaoInterna()
+}
+
+var reference = funcaoExterna()
+
+// Understanding closures with loops
+
+window.onload() = function () {
+    var botao = document.getElementsByName('button');
+    for (var i = 0; i < botao.length; i++) {
+        botao[i].onclick = () => {
+            console.log('Voce clicou')
+        }
+    }
+}
+
+window.onload = () => {
+    var botao = document.getElementsByName('button');
+
+    auxiliar(j => {
+        return () => {
+            console.log(`voce clicou no botao ${j}`)
+        }
+    })
+
+    for (var i = 0; i < botao.length; i++) {
+        botao[i].click = () => {
+            auxiliar(i) //scape da closure 
+            console.log(`voce clicou no botao ${i}`)
+        }
+    }
+}
