8d4116e887679ffaa42ddf712b13599f78afb34a
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-09 20:56:35 -0300

    parseInt

---
+++ javascript/maujor-book-js/cap-6 - Functions/global functions/parseInt.js
@@ -0,0 +1,7 @@
+//Converte a string passada em um numero inteiro, admite um argumento opcional , que é a base na qual o numero devera ser retornado.Caso o argumento nao seja passado a conversão é feita na base 10.
+
+parseInt(int[,base])
+
+function numeroParseado(x) {
+    eval(`var num = parseInt(x); console.log(num)`)
+}
