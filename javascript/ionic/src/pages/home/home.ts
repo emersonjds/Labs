a62bc5b62f149d880082a79c6280071f103d0589
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-08 22:21:22 -0300

    start project

--- javascript/ionic/src/pages/home/home.ts
+++ javascript/ionic/src/pages/home/home.ts
@@ -7,8 +7,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public carros;
 
+  constructor(public navCtrl: NavController) {
+    this.carros = [
+     {nome: 'Azera v6', preco:85000},
+     {nome: 'BMW 325i', preco:97000},
+     {nome: 'Porshe Cayenne', preco:250000},
+     {nome: 'Veloster', preco:49000},
+     {nome: 'Porshe Panammera', preco:190000},
+     {nome: 'ix35', preco:85000},
+     {nome: 'Gol g5', preco:20000},
+     {nome: 'Corsa Classic', preco:19000},
+     {nome: 'Gol 2000', preco:7000},
+     {nome: 'xj6 2012', preco:20000},
+    ]
   }
 
 }
