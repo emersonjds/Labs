a6218a4f7b7823cd5e61e62171578989a236fca0
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-08 21:58:23 -0300

    install dependencies

--- javascript/ionic/src/pages/home/home.ts
+++ javascript/ionic/src/pages/home/home.ts
@@ -7,7 +7,8 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-   constructor(public navCtrl: NavController) {
-   
+  constructor(public navCtrl: NavController) {
+
   }
+
 }
