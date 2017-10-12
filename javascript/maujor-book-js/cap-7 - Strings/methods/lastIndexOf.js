87ebdecca47da72c4d6645968fd7c3ed5f6405bc
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-12 17:20:56 -0300

    lastIndexOf

---
+++ javascript/maujor-book-js/cap-7 - Strings/methods/lastIndexOf.js
@@ -0,0 +1,8 @@
+// O metodo lastIndexOf retorna o indice da ultima ocorrencia da string, definida no parametro inicial . Esse parametro é a substring de busca que supostamente esta continda na string base em que se faz a procura. Se definirmos um caracter como stringProcurada o metodo retorna o indice desse caractere. Havendo mais de 1 caractere, o indice retornado é o do ultimo na string.
+
+// O metodo tambem é sensivel a CamelCase
+
+var str = "Hello planet earth, you are a great planet.";
+var n = str.lastIndexOf("planet"); // 36
+
+
