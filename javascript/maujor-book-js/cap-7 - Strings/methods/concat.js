7cc4af06bc06c311ff94eb3ba4459be241c852f7
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-12 16:31:14 -0300

    Concat

---
+++ javascript/maujor-book-js/cap-7 - Strings/methods/concat.js
@@ -0,0 +1,9 @@
+//O metodo concat() concatena duas ou mais strings efinidas nos parametros stringsN
+
+// A string resultante é fornada pela junção das strings ncatenadas. Esse método é não destrutivo, ou seja, as strings a serem reunidas são preservadas e uma nova string é criada.
+
+var string1 = "Casa";
+var string2 = " é um bom lgar para";
+
+var stringResultante = string1.concat(string)
+console.log(stringResultante);
