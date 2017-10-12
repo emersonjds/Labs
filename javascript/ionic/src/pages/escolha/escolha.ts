7096dab250fdd66361686574e95b059e715e1d04
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-12 19:58:11 -0300

    Refactor ion-toggle

--- javascript/ionic/src/pages/escolha/escolha.ts
+++ javascript/ionic/src/pages/escolha/escolha.ts
@@ -8,14 +8,27 @@ import { NavParams } from 'ionic-angular'
 export class EscolhaPageComponent {
     public carro;
     public acessorios;
+    private _precoTotal: number;
 
     constructor(public navParams: NavParams) {
         this.carro = this.navParams.get('carroSelecionado');
+        this._precoTotal = this.carro.preco;
+
         this.acessorios = [
             { nome: 'Freios ABS', preco: 800 },
             { nome: 'Ar Condicionado', preco: 300 },
             { nome: 'Banco de couro', preco: 300 },
-            { nome: 'Teto Solar', preco: 600 }
+            { nome: 'Teto Solar', preco: 600 },
+            { nome: 'MP3 Player', preco: 300 }
         ]
     }
+
+    get precoTotal() { //getter 
+        return this._precoTotal;
+    }
+
+    atualizaTotal(ativo: boolean, acessorio) {
+        ativo ? this._precoTotal += acessorio.preco : this._precoTotal -= acessorio.preco;
+    }
+
 } 
