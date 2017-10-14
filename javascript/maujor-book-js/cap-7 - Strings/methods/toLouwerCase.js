62e8655271fa5d9f88ed6b4325814302156d1c42
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-14 18:21:54 -0300

    To upper e to lower

---
+++ javascript/maujor-book-js/cap-7 - Strings/methods/toLouwerCase.js
@@ -0,0 +1,5 @@
+//este metodo retorna uma string em letra minuscula
+
+var x = 'EMERSON JUNIOR DANIEL SILVA';
+
+console.log(x.toLocaleLowerCase()) // emerson junior daniel silva
---
+++ javascript/maujor-book-js/cap-7 - Strings/methods/toUpperCase.js
@@ -0,0 +1,4 @@
+//Retorna uma string em letra maiuscula
+
+var x = 'emerson junior'
+console.log(x.toLocaleUpperCase()) // EMERSON JUNIOR
