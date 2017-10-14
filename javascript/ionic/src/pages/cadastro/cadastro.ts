805060ccf2980be25f4debd2bd46d696905c749b
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-14 20:54:38 -0300

    config send data to api

--- javascript/ionic/src/pages/cadastro/cadastro.ts
+++ javascript/ionic/src/pages/cadastro/cadastro.ts
@@ -1,6 +1,7 @@
 import { Carro } from './../../domain/carro/carro';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 @Component({
   templateUrl: 'cadastro.html',
@@ -14,16 +15,18 @@ export class CadastroPage {
   public email: string;
   public date: string = new Date().toISOString();// o componente devolve em formato de string a data
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private _http: Http) {
     this.carro = navParams.get('carro')
     this.precoTotal = navParams.get('precoTotal')
   }
 
   agenda() {
-    console.log(this.nome)
-    console.log(this.endereco)
-    console.log(this.email)
-    console.log(this.date)
+    let api = `https://aluracar.herokuapp.com/salvarpedido?carro=${this.carro.nome}&nome=${this.nome}&preco=${this.precoTotal}&endereco=${this.endereco}&email=${this.email}&dataAgendamento=${this.date}`;
+    this._http
+      .get(api)
+      .toPromise()
+      .then(() => console.log('Sucesso'))
+      .catch(err => console.log(err.message))
   }
 
 }
