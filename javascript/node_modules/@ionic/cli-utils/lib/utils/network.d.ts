cb9682fb45af86e918d50532557fb42f790b6d65
Emerson Silva <emerson_jdss@hotmail.com>
2017-10-16 23:40:22 -0200

    first commit project

--- javascript/node_modules/@ionic/cli-utils/lib/utils/network.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function getAvailableIPAddress(): any[];
-export declare function findClosestOpenPort(host: string, port: number): Promise<number>;
-export declare function isPortTaken(host: string, port: number): Promise<boolean>;
