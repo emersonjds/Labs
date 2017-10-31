38190df33fa3bffc05aa1300f9bb8ecc80c40133
Silva <emerson.junior.silva@accenture.com>
2017-10-31 21:42:54 -0200

    Inputs and outputs properties

--- javascript/plural-angular-4/APM-Start/src/app/products/products-list.component.ts
+++ javascript/plural-angular-4/APM-Start/src/app/products/products-list.component.ts
@@ -1,74 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { IProduct } from '../products/products';
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "../products/products";
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './products-list.component.html',
-    styleUrls: [
-        './products-list.component.css'
-    ]
+  selector: "pm-products",
+  templateUrl: "./products-list.component.html",
+  styleUrls: ["./products-list.component.css"]
 })
 export class ProductsListComponent implements OnInit {
-    pageTitle: string = 'Product List';
-    imgWidth: number = 50;
-    imgMargin: number = 2;
-    showImage: boolean = false;
-    productFiltered: string = 'cart';
+  pageTitle: string = "Product List";
+  imgWidth: number = 50;
+  imgMargin: number = 2;
+  showImage: boolean = false;
+  productFiltered: string = "cart";
 
-    _listFilter: string;
+  _listFilter: string;
 
-    //ggas
-    get listFilter() {
-        return this._listFilter;
+  //ggas
+  get listFilter() {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
+  }
+
+  filteredProducts: IProduct[];
+
+  products: IProduct[] = [
+    {
+      productId: 2,
+      productName: "Garden Cart",
+      productCode: "GDN-0023",
+      releaseDate: "March 18, 2016",
+      description: "15 galoon capacity",
+      price: 32.99,
+      starRating: 3.8,
+      imageUrl:
+        "http://d2trtkcohkrm90.cloudfront.net/images/emoji/apple/ios-10/256/balloon.png"
+    },
+    {
+      productId: 3,
+      productName: "Bic Pen",
+      productCode: "BIC-0023",
+      releaseDate: "March 01, 2016",
+      description: "1 capacity",
+      price: 2.0,
+      starRating: 4.9,
+      imageUrl:
+        "https://www.pascogifts.com/files/cache/square/files/migrated-bic-attriant-d0d8.jpg"
     }
+  ];
 
-    set listFilter(value: string) {
-        this._listFilter = value;
-        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-    }
+  //starts with initialize compontn
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = "cart";
+  }
 
-    filteredProducts: IProduct[];
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase(); //code starts by converting the filter criteria to lowercase.
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": 'Garden Cart',
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 galoon capacity",
-            "price": 32.99,
-            "starRating": 3.8,
-            "imageUrl": "http://d2trtkcohkrm90.cloudfront.net/images/emoji/apple/ios-10/256/balloon.png"
-        },
-        {
-            "productId": 3,
-            "productName": 'Bic Pen',
-            "productCode": "BIC-0023",
-            "releaseDate": "March 01, 2016",
-            "description": "1 capacity",
-            "price": 2.00,
-            "starRating": 4.9,
-            "imageUrl": "https://www.pascogifts.com/files/cache/square/files/migrated-bic-attriant-d0d8.jpg"
-        }
-    ]
+  toogleImage(): void {
+    this.showImage = !this.showImage;
+  }
 
-    //starts with initialize compontn
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
+  ngOnInit(): void {
+    console.log("START ON INIT");
+  }
 
-    performFilter(filterBy: string): IProduct[] {
-        filterBy = filterBy.toLocaleLowerCase(); //code starts by converting the filter criteria to lowercase.
-        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
-    }
-
-
-    toogleImage(): void {
-        this.showImage = !this.showImage;
-    }
-
-    ngOnInit(): void {
-        console.log('START ON INIT')
-    }
+  onNotify(message: string): void {
+    console.log(message);
+  }
 }
--- javascript/plural-angular-4/APM-Start/src/app/shared/start.component.ts
+++ javascript/plural-angular-4/APM-Start/src/app/shared/start.component.ts
@@ -1,15 +1,27 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter
+} from "@angular/core";
 
 @Component({
-    selector: 'pm-star',
-    templateUrl: './star.component.html',
-    styleUrls: ['./star.component.css']
+  selector: "pm-star",
+  templateUrl: "./star.component.html",
+  styleUrls: ["./star.component.css"]
 })
 export class StarComponent implements OnChanges {
-    @Input() rating: number = 4;
-    starWidth: number;
+  @Input() rating: number = 4;
+  @Output() notify: EventEmitter<string> = new EventEmitter<string>();
 
-    ngOnChanges(): void {
-        this.starWidth = this.rating * 86 / 5;
-    }
+  starWidth: number;
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 86 / 5;
+  }
+
+  click() {
+    console.log(`The rating ${this.rating} was clicked`);
+    this.notify.emit("EMITINDO UM EVENTO");
+  }
 }
