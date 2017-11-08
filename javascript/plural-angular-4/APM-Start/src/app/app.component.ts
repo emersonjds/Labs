76373f92c849cbfd02ad9129fbe6094a8ea13942
Emerson Jr <emerson_jdss@hotmail.com>
2017-11-07 22:41:47 -0200

    named the directive

--- javascript/plural-angular-4/APM-Start/src/app/app.component.ts
+++ javascript/plural-angular-4/APM-Start/src/app/app.component.ts
@@ -2,27 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root ',
-  template: `
-
-  <div>
-    <nav class="navbar navbar-inverse">
-      <div class="container-fluid">
-        <a class="navbar-brand"> {{pageTitle}}</a>
-        <ul class="nav navbar-nav">
-          <li><a [routerLink]="['/welcome']">Home</a></li>
-          <li><a [routerLink]="['/products']">Product Lista</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-
-  <div class="container">
-    <router-outlet></router-outlet>
-  </div>
-
-
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  pageTitle: string = 'Studies Again and Again';
+  pageTitle: string = 'Project';
 }
