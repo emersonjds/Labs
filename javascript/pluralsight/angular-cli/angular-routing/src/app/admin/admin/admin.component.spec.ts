aaa91e2443fb643a9ca8fc114936bedc45c8e9e3
Emerson Silva <emerson_jdss@hotmail.com>
2017-11-12 20:09:12 -0200

    unit tests

--- javascript/pluralsight/angular-cli/angular-routing/src/app/admin/admin/admin.component.spec.ts
+++ javascript/pluralsight/angular-cli/angular-routing/src/app/admin/admin/admin.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
@@ -8,9 +9,10 @@ describe('AdminComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      declarations: [AdminComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
