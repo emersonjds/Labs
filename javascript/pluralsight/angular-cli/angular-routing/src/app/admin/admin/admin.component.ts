c40d5bab790505e4320f29d7f129298b1b7c3caa
Emerson Silva <emerson_jdss@hotmail.com>
2017-11-12 21:06:50 -0200

    working with Karma

--- javascript/pluralsight/angular-cli/angular-routing/src/app/admin/admin/admin.component.ts
+++ javascript/pluralsight/angular-cli/angular-routing/src/app/admin/admin/admin.component.ts
@@ -7,10 +7,27 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class AdminComponent implements OnInit {
+  users: any[];
+  // tslint:disable-next-line:no-inferrable-types
+  hasPermission: boolean = false;
 
   constructor() { }
-
   ngOnInit() {
+    if (this.hasPermission) {
+      this.getUsers()
+        .then(users => this.users = users)
+        .catch(e => console.log(e.message));
+    } else {
+      this.users = [];
+    }
   }
+
+  async getUsers() {
+    return [
+      { name: 'jhon', email: 'john@angular.com' },
+      { name: 'coolean', email: 'coolean@angular.com' }
+    ];
+  }
+
 
 }
