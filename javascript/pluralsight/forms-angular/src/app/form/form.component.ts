5af9290528ab76bdfd16736c69e371f6ae0492ad
Emerson Jr <emerson_jdss@hotmail.com>
2017-11-19 21:48:59 -0200

    understanding another attributes of html 5 in differents browsers

--- javascript/pluralsight/forms-angular/src/app/form/form.component.ts
+++ javascript/pluralsight/forms-angular/src/app/form/form.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public languages: Array<string> = [
+    'portuguese',
+    'english',
+    'spanish'
+  ];
+
   constructor() { }
 
   ngOnInit() {
