15cf1f88b334c46eec0c7c87094ae1a7939dca59
Emerson Jr <emerson_jdss@hotmail.com>
2017-11-19 21:14:30 -0200

    config validations

--- javascript/pluralsight/forms-angular/src/app/app.module.ts
+++ javascript/pluralsight/forms-angular/src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterializeModule } from 'angular2-materialize';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent
-  ],
-  imports: [
-    BrowserModule,
-    MaterializeModule
-  ],
+  declarations: [AppComponent, FormComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
