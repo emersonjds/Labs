de7b0f927c4649d43d40be30b45fe16e0c74bf1e
Emerson Jr <emerson_jdss@hotmail.com>
2017-11-20 21:19:17 -0200

    data binding in input fields

--- javascript/pluralsight/forms-angular/src/app/form/form.component.ts
+++ javascript/pluralsight/forms-angular/src/app/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee';
 
 @Component({
   selector: 'app-form',
@@ -6,16 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.sass']
 })
 export class FormComponent implements OnInit {
+  public languages: Array<string> = ['portuguese', 'english', 'spanish'];
 
-  public languages: Array<string> = [
-    'portuguese',
-    'english',
-    'spanish'
-  ];
+  model = new Employee('Emerson', 'Silva');
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  nameToUpperCase(value: string) {
+    if (value.length > 0) {
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    } else {
+      this.model.firstName = value;
+    }
   }
-
 }
---
+++ javascript/pluralsight/forms-angular/src/app/models/employee.ts
@@ -0,0 +1,6 @@
+export class Employee {
+ constructor(
+  public firstName: string,
+  public lastName: string
+ ) {}
+}
