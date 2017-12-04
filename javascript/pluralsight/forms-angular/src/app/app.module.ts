a52c67e48cc73bed86ad589419ac34eca5ad04af
Emerson Silva <emerson_jdss@hotmail.com>
2017-12-03 23:15:13 -0200

    understanding external modules

--- javascript/pluralsight/forms-angular/src/app/app.module.ts
+++ javascript/pluralsight/forms-angular/src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule} from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { FormService } from '../services/form-post.service';
@@ -10,11 +10,19 @@ import { FormService } from '../services/form-post.service';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 import { NG2Component } from './components/ng2-forms';
+import { DatepickerModule } from 'ng2-bootstrap';
+import { TimepickerModule } from 'ng2-bootstrap';
+import { ButtonsModule } from 'ng2-bootstrap';
+import { BsDatepickerModule } from 'ngx-bootstrap';
+
 
 @NgModule({
   declarations: [AppComponent, FormComponent, NG2Component],
-  imports: [BrowserModule, FormsModule, HttpModule, RouterModule],
+  imports: [BrowserModule,
+    FormsModule, HttpModule,
+    RouterModule, DatepickerModule,
+    BsDatepickerModule, TimepickerModule, ButtonsModule],
   providers: [FormService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- javascript/pluralsight/forms-angular/src/app/components/ng2-forms.ts
+++ javascript/pluralsight/forms-angular/src/app/components/ng2-forms.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
     templateUrl: './ng2-forms.html',
     styleUrls: ['./ng2-forms.scss']
 })
-export class NG2Component {}
+export class NG2Component {
+    public startDate= new Date('Jan 1 2017');
+    minDate = new Date(2017, 5, 10);
+  maxDate = new Date(2018, 9, 15);
+ 
+  bsValue: Date = new Date();
+  bsRangeValue: any = [new Date(2017, 7, 4), new Date(2017, 7, 20)];
+
+  startTime = new Date('Jan 1 2017 3:00 PM');
+  singleModel: any;
+  taxtType = 'W2';
+
+}
