b081790dca5b1424b8a6500745a62c90cf9aa034
Emerson Silva <emerson_jdss@hotmail.com>
2017-12-31 18:43:49 -0200

    create concecpts

--- javascript/pluralsight/angular-fundamentals/src/app/eventlist/eventlist.component.ts
+++ javascript/pluralsight/angular-fundamentals/src/app/eventlist/eventlist.component.ts
@@ -8,18 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventlistComponent implements OnInit {
 
-  event1 = {
-    id: 1,
-    name: 'Angular Connect',
-    date: '12/12/2018',
-    time: '10:00 am',
-    price: 599.99,
-    location: {
-      address: '1055 DT',
-      city: 'London',
-      country: 'England'
-    }
-  };
+  events = [
+    {
+      id: 1,
+      name: 'Angular Connect',
+      date: '12/12/2018',
+      time: '10:00 am',
+      price: 599.99,
+      location: {
+        address: '1055 DT',
+        city: 'London',
+        country: 'England'
+      }
+    },
+    {
+      id: 2,
+      name: 'Angular Connect',
+      date: '12/12/2018',
+      time: '10:00 am',
+      price: 599.99,
+      location: {
+        address: '1055 DT',
+        city: 'London',
+        country: 'England'
+      }
+    },
+
+  ]
 
   constructor() { }
 
