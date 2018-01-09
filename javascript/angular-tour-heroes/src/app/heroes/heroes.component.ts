1ba35296013fa607d73efefd502ccc44717f47ed
Emerson Jr <emerson_jdss@hotmail.com>
2018-01-09 14:11:57 -0200

    first hero component

---
+++ javascript/angular-tour-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,21 @@
+import { Hero } from './../hero';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windostorm'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
