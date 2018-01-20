33ecee5d4469ab5d9effc32a34431795ced9d56c
Emerson Silva <emerson_jdss@hotmail.com>
2018-01-19 22:09:04 -0200

    details

--- javascript/pluralsight/angular-fundamentals/src/app/eventlist/event-details/event-details.component.ts
+++ javascript/pluralsight/angular-fundamentals/src/app/eventlist/event-details/event-details.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
 import { ActivatedRoute } from '@angular/router';
 
-
 @Component({
   templateUrl: './event-details.component.html'
 })
