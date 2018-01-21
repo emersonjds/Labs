157d2b8d39e28b075a6e8b07065319b754735804
Emerson Silva <emerson_jdss@hotmail.com>
2018-01-20 22:26:41 -0200

    refactor code

---
+++ javascript/vanilla/jsforB/01-class/script01.js
@@ -0,0 +1,8 @@
+
+function jsFunction() {
+    document.getElementById("content").innerHTML = "That's how we roll";
+}
+
+function initialText() {
+    document.getElementById("content").innerHTML = "This is the current text";
+}
