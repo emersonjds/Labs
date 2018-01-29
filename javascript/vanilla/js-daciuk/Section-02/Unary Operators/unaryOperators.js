72866e323d4990d43b2abad2910dba733ef5786e
Emerson Silva <emerson_jdss@hotmail.com>
2018-01-29 21:47:21 -0200

    refactor unary operator

--- javascript/vanilla/js-daciuk/Section-02/Unary Operators/unaryOperators.js
+++ javascript/vanilla/js-daciuk/Section-02/Unary Operators/unaryOperators.js
@@ -3,3 +3,19 @@ retorna 3 , pois o valor foi parseado de string para numerico
 
 +'emerson'
 retorna NaN pois nao foi possivel converter a cadeia de strings
+// NaN é avaliado como tipo numerico pelo javascript
+
+'emer' + 'son';
+// concatenação de valores
+
+'3' + 3
+contatena em '33' string
+
+3 + '3'
+// concatena em '33' string, o javascript entende que sempre que um numero se contatena a uma string nao é uma operação matematica e sim uma unicidade de valor em string
+
+ var x = 3;
+ -x
+//  resultado -3
+
+// o valor de x nao é alterado , ele apenas recebe o valor negativo 
