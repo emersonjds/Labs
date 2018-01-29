23af695d99d4e35d6103caa77dd5db2bcef8dbc2
Emerson Silva <emerson_jdss@hotmail.com>
2018-01-29 21:32:04 -0200

    logical tests

---
+++ javascript/vanilla/js-daciuk/Section-02/Logic Operators/logic.js
@@ -0,0 +1,19 @@
+var x = 3;
+var y = 7;
+
+x === 3;
+true 
+
+x === 3 && y === 7
+true
+
+x != '3' 
+// x é diferente de 3 string == falso por que esta sendo testado somente o valor e nao o tipo
+
+x !== '3'
+// true por que x é igual a 3 number e nao '3' string, o valor e o tipo estao sendo testados
+
+x === 3 || y === 3
+// true por que x ou y podem ter valores verdadeiros, no caso x
+// A expressao ou so da falso caso as duas sentenças sejam falsas
+
