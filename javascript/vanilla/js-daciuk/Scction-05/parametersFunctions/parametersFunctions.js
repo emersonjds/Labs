a07919b4756631da2488597be60f5bc7f298c549
Emerson Silva <emerson_jdss@hotmail.com>
2018-02-04 21:03:15 -0200

    functions parameters of high class

---
+++ javascript/vanilla/js-daciuk/Scction-05/parametersFunctions/parametersFunctions.js
@@ -0,0 +1,30 @@
+var arr = [1,2,3]
+
+function arrayarr(arg){
+    return arg;
+}
+
+arrayarr(arr); // return [1,2,3]
+
+function myFunction2(args) {
+    return args[1];
+}
+
+myFunction2(arr);
+
+myFunction2([1,2,3,4,5]); // return 2, pois é o valor contido no indice de chamada interna de retorno da função
+
+
+var obj = {
+    propriedade: 'Emerson',
+    curso: 'JAavascript Ninja',
+    ninja: true
+}
+
+function myFunction(arg) {
+    return arg;
+}
+
+myFunction(obj);
+myFunction(obj).propriedade; //return 'Emerson'
+myFunction(obj).ninja; //return true
