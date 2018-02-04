6c7873f76f45583230c3b4326bb44f2fe765e82c
Emerson Silva <emerson_jdss@hotmail.com>
2018-02-04 15:53:54 -0200

    refactor code

--- javascript/vanilla/js-daciuk/Section-04/challenge-04/challenge-04.js
+++ javascript/vanilla/js-daciuk/Section-04/challenge-04/challenge-04.js
@@ -126,12 +126,13 @@ citado acima, no lugar de "pessoas".
 */
 carro.adicionaPessoas = function (numeroPessoas) {
     var totalPessoas = carro.quantidadePessoas + numeroPessoas;
+    var quantasPessoasCabem = carro.assentos - carro.quantidadePessoas();
+    var pluralOuSingular = quantasPessoasCabem === 1 ? 'pessoa' : 'pessoas';
+
     if (carro.quantidadePessoas() === carro.assentos && totalPessoas >= carro.assentos) {
         return 'O carro ja esta lotado!';
     }
-    if(totalPesosas > carro.assentos) {
-        var quantasPessoasCabem = carro.assentos - carro.quantidadePessoas();
-        var pluralOuSingular = quantasPessoasCabem === 1 ? 'pessoa' : 'pessoas';
+    if (totalPesosas > carro.assentos) {
         return 'Só cabem mais ' + quantasPessoasCabem + ' ' + pluralOuSingular;
     }
     carro.quantidadePessoas += numeroPessoas;
