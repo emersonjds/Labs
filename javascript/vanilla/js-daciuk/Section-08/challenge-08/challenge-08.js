ebc2519d1943e7a8114757794ac67130a9101b93
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-10 14:43:07 -0300

    challenge 08

--- javascript/vanilla/js-daciuk/Section-08/challenge-08/challenge-08.js
+++ javascript/vanilla/js-daciuk/Section-08/challenge-08/challenge-08.js
@@ -3,38 +3,43 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+var sum = function calculateSum(x, y) {
+    return x + y;
+}
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+console.log(`A soma de 10 + 20 é ao resultado ${sum(10, 20)}`);
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+console.log(`"O nome da função que faz a soma é ${sum.name}"`);
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+function showName() {
+    return `Emerson Junior Daniel Silva`;
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+
+var varShowName = showName();
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+console.log(`A função ${showName.name} retorna ${varShowName}`);
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,7 +53,22 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+
+function calculator(operator) {
+    return function (x, y) {
+        if (operator === "+" ||
+            operator === "-" ||
+            operator === "*" ||
+            operator === "/" ||
+            operator === "%") {
+            debugger;
+            var calculo = parseInt(x + ' ' + operator + ' ' + y);
+            debugger;
+            return `Resultado da operação: ${x} ${operator} ${y} = ${calculo}.`
+        }
+        return "Operação inválida."
+    }
+}
 
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
