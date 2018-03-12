57f87f4700db4b3d048ef9759c03e12f6c03900f
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-11 21:17:51 -0300

    understanding functional programing with closures

--- javascript/vanilla/js-daciuk/Section-09/scopeFunctions.js
+++ javascript/vanilla/js-daciuk/Section-09/scopeFunctions.js
@@ -1,9 +1,10 @@
 function myFunction() {
-    v1 = 10; // esse escopo é valido na função na hora da execução
-    v2 = 20;
     function sum() {
         return v1 + v2;
         // sum é uma closure ( consegue acessar parametros externos a ela)
     }
-    return sum(); //execução da função
+    v1 = 10; // esse escopo é valido na função na hora da execução
+    v2 = 20;
+    return sum(); //execução da função,neste momemtno o JS se encarrega de montar a função , passando os valores 
 }
+
