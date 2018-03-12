bf71b748adb30fa6fcd0cf08b29e84ab781b18c4
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-11 21:57:22 -0300

    right declaration function inside another function to avoid Hoisting

--- javascript/vanilla/js-daciuk/Section-09/hoisting/hoisting.js
+++ javascript/vanilla/js-daciuk/Section-09/hoisting/hoisting.js
@@ -14,6 +14,21 @@ function myFunc() {
     var sum = function sum() {
         return n1 + n2;
     }
-} // nao funciona pois a função criada nao é do tipo literal
+} // nao funciona pois a função criada nao é do tipo literal, retorna undefined 
 
 console.log(myFunction());
+console.log(myFunc());
+
+// -------------------------------------------------------
+// Forma correta de declara funções dentro de funções para evitar Hoisting
+function xpto() {
+    var n1 = 10; //variaveis sempre no começo
+    var n2 = 20;
+
+    function sumNumbers() { // função literal utilizando closure para acesso as variaveis do escopo externo local
+        return v1 + v2;
+    }
+
+    return sum();
+    // execucão da função
+}
