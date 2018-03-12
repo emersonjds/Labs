77fb5958c99f7a007774cb5b35ad84c347d5980e
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-11 23:24:06 -0300

    IIFE

---
+++ javascript/vanilla/js-daciuk/Section-09/IIFE/iife.js
@@ -0,0 +1,23 @@
+// Função autoexecutavel  IIFE 
+function sum() {
+    return 1 + 2;
+}
+
+console.log(sum());
+
+var sum2 = function () {
+    return 3 + 2;
+}
+
+console.log(sum2());
+
+var sum3 = function otherSum() {
+    return 5 + 8;
+}
+
+console.log(sum3());
+
+// IIFE 
+(function () {
+    console.log(1 + 2);
+})(); 
