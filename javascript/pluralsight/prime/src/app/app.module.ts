b2da2cd3226f3505c597c48f85c2c5c70f8fee70
Emerson Jr <emerson_jdss@hotmail.com>
2018-03-13 11:22:58 -0300

    add a new component

--- javascript/pluralsight/prime/src/app/app.module.ts
+++ javascript/pluralsight/prime/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule
