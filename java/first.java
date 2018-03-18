c40c2a82276a79c137dbc19d18bfc4cf193c6147
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-18 14:42:49 -0300

    first steps

--- java/first.java
+++ java/first.java
@@ -1,5 +1,5 @@
 class MeuPrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.println("O primeiro e muitos");
+        System.out.println("O primeiro de muitos códigos escritos em " + args[0] + " pelo " + args[1] + "!");
     }
 }
