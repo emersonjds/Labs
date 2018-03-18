be1dd704e542577b4626c598e81fa76d786f1f77
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-18 00:15:36 -0300

    change script

--- javascript/typescript/app/app.js
+++ javascript/typescript/app/app.js
@@ -3,9 +3,7 @@ function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath Starting a new game ...';
 }
-
 // document.getElementById('startGame').addEventListener('click', startGame);
-
-document.getElementById('startGame').addEventListener('click', () => {
+document.querySelector('startGame').addEventListener('click', function () {
     console.log('clicado');
 });
