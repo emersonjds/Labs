ef5524a38a03b736f881ad24786eda316a9c15ba
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-20 23:48:50 -0300

    change syso

--- java/first.java
+++ java/first.java
@@ -1,5 +1,6 @@
 class MeuPrimeiroPrograma {
     public static void main(String[] args) {
         System.out.println("O primeiro de muitos códigos escritos em " + args[0] + " pelo " + args[1] + "!");
+        System.out.println("came back to Java");
     }
 }
