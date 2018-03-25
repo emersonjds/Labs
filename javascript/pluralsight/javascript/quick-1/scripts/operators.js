92899570b432e4d3bd6935990e293558ae6d4c6c
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-24 23:01:58 -0300

    logical operators

---
+++ javascript/pluralsight/javascript/quick-1/scripts/operators.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    console.log('Sum', 2 + 2); // sum 
+    console.log('Subtraction', 3 + 1); // subtraction
+    console.log('Division', 4 / 2); // division
+    console.log('Mulitplication', 3 * 2); // multiplication 
+    console.log('Mod', 5 % 3); //mode
+}
