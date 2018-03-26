a79c7155d9d192d9a763f3c8ded497311f40a319
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-25 21:26:33 -0300

    more of consoles

--- javascript/pluralsight/javascript/quick-1/scripts/console.js
+++ javascript/pluralsight/javascript/quick-1/scripts/console.js
@@ -1,5 +1,9 @@
 //Working with output messages in console
 window.onload = function () {
     console.log('working with browser console');
+
+    console.warn('Cause a warn in console');
+
+    console.error('Call the error to browser');
 }
 
