29e97dea06c4cc273955cc33ba5872b8dea3eb99
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-25 21:57:40 -0300

    variables concept

--- javascript/pluralsight/javascript/quick-1/scripts/variables.js
+++ javascript/pluralsight/javascript/quick-1/scripts/variables.js
@@ -1,3 +1,9 @@
 window.onload = function () {
-    
+    // a store for information, witch can use in the middle code
+    var a = 10;
+    var b = 20;
+
+    typeof (a + b);
+
+    console.log(a + b);
 }
