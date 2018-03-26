ab76bc568550d938c83070e2a7026fa7573a479f
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-25 23:15:53 -0300

    flow comparison

---
+++ javascript/pluralsight/javascript/quick-1/scripts/flow_comparison.js
@@ -0,0 +1,13 @@
+randomNumber = Math.round(Math.random() * 15);
+
+if (randomNumber % 3 === 0 && randomNumber !== 0) {
+    alert('Fizz');
+} else if (randomNumber % 5 === 0 && randomNumber !== 0) {
+    alert('Buzz');
+} else if (randomNumber % 3 === 0 || randomNumber % 5 === 0 && randomNumber !== 0) {
+    alert('FizzBuzz');
+} else if (randomNumber === 0) {
+    console.log(randomNumber)
+} else {
+    console.log(randomNumber)
+}
