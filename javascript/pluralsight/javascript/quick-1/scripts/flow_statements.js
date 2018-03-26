818a508e452c703a26c3c0e00c39eac10eb7e4cf
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-25 22:32:40 -0300

    flow satements

---
+++ javascript/pluralsight/javascript/quick-1/scripts/flow_statements.js
@@ -0,0 +1,7 @@
+var answer = prompt(`What's is the capital New York ? `);
+
+if (answer === 'Albany') {
+    alert('You are correct');
+} else {
+    alert('Sorry, the correct answer is Albany');
+}
