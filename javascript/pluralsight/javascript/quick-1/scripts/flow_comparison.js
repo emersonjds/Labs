23af22589c2b98efbfce519fa99a5eb2403bd336
Emerson Silva <emerson_jdss@hotmail.com>
2018-03-25 23:24:10 -0300

    refactor code

--- javascript/pluralsight/javascript/quick-1/scripts/flow_comparison.js
+++ javascript/pluralsight/javascript/quick-1/scripts/flow_comparison.js
@@ -4,10 +4,9 @@ if (randomNumber % 3 === 0 && randomNumber !== 0) {
     alert('Fizz');
 } else if (randomNumber % 5 === 0 && randomNumber !== 0) {
     alert('Buzz');
-} else if (randomNumber % 3 === 0 || randomNumber % 5 === 0 && randomNumber !== 0) {
+} else if (randomNumber % 3 === 0 && randomNumber % 5 === 0 && randomNumber !== 0) {
     alert('FizzBuzz');
-} else if (randomNumber === 0) {
-    console.log(randomNumber)
-} else {
+}
+else {
     console.log(randomNumber)
 }
