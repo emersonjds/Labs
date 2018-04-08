c6eb55c7dc74ca8a7ee37fd45b64945d267ced83
Emerson Silva <emerson_jdss@hotmail.com>
2018-04-07 21:16:21 -0300

    variables

--- javascript/u-courses/vanilla/Section-01/variables/variable.js
+++ javascript/u-courses/vanilla/Section-01/variables/variable.js
@@ -1,5 +1,7 @@
-// nome simbolico para recptaculo que recebe um valor seguido da palavra reservada var
-
+// nome simbolico recebe um valor seguido da palavra reservada var
 var x = 10;
-
 console.log(x);
+x = 1;
+console.log(x); // int 
+
+var y = 1.2; // float
