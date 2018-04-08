d3bd4a73b78494f24945224694a670840aedeeb7
Emerson Silva <emerson_jdss@hotmail.com>
2018-04-07 21:38:58 -0300

    refactor operators

--- javascript/u-courses/vanilla/Section-01/variables/operators.js
+++ javascript/u-courses/vanilla/Section-01/variables/operators.js
@@ -12,5 +12,14 @@ soma // return 20
 
 soma + 1 // return 21
 
+// Increment 
 ++soma // pre increment 22
 soma++ //pos increment 22 ... 23 
+
+--soma // return 22
+soma-- //return 21
+
+soma += 20; // return 40
+soma -= 10; // return 30
+soma *= 2; // return 60
+soma /= 2; // return 30
