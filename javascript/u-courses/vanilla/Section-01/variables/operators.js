ec9f5a424d3a5a198dbcfca326ffbc59ecaebf11
Emerson Silva <emerson_jdss@hotmail.com>
2018-04-07 21:51:21 -0300

    refactor relational operators

--- javascript/u-courses/vanilla/Section-01/variables/operators.js
+++ javascript/u-courses/vanilla/Section-01/variables/operators.js
@@ -24,10 +24,25 @@ soma -= 10; // return 30
 soma *= 2; // return 60
 soma /= 2; // return 30
 
-== equal to 
-1 == 1 return true , //verify value
-1 === 1 // return true , verify value and type
-1 === "1" // false
-
+== //equal to 
+1 == 1; //return true //verify value
+1 === 1; // return true , verify value and type
+1 === "1"; // false
 != //diferent 
 !== //iferente mas do mesmo tipo
+
+'emerson' == 'emerson'; // return true
+var nome = 'Emerson';
+nome // return Emerson
+
+nome == 'Maria' // return false
+var pessoa2 = 'Maria';
+nome == Maria // return false Emerson != Maria
+
+var um = '1';
+
+1 == um; // return true , o javascript converte a string em numero para fazer a comparacão
+
+1 === um // return false por que string é diferente de numerico
+
+1 !== um // return true por que o tipo da variavel é diferente do tipo do numero 1
