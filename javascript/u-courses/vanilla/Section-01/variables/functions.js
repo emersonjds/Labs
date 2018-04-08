3283b56af1d35607998b31f0f24dd1c175312cea
Emerson Silva <emerson_jdss@hotmail.com>
2018-04-07 22:00:02 -0300

    functions

---
+++ javascript/u-courses/vanilla/Section-01/variables/functions.js
@@ -0,0 +1,13 @@
+function nome() { }
+
+var x = 1;
+
+function soma() {
+    x = x + 1;
+}
+
+x // return 1;
+soma() // return undefined ja que nao ha retorno na funç
+x // return 2; o valor foi acrescenteado pela função
+soma();
+x // return 3
