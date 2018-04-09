ca9c26e5ad173a71ec9d51ad2f23798b55d4d180
Emerson Silva <emerson_jdss@hotmail.com>
2018-04-08 22:16:33 -0300

    logical operators

---
+++ javascript/u-courses/vanilla/Section-02/logicalOperators.js
@@ -0,0 +1,23 @@
+// && (AND) verify both values are true and return, if not return false
+var x = 3;
+var y = 7;
+
+x === 3; // return true, verify value and type
+
+y === 5; // return false, verify value and type
+
+x === 3 && y === 7; // return true, verify value and type
+
+x === 3 && y === 4; // return false, if any variable has a diferent value the && operator return false, just return true with both variable   are true 
+
+// || (OR) verify of any value is true, if yes return true, just return false with both values false
+
+x === 3 || y === 3
+
+// !(NOT) inverts a value
+x === 3; // return true
+!x === 3; // return falsea
+
+x != 3; //return false
+x !== 3; // return false 
+
