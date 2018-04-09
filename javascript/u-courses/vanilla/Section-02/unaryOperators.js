39f044afbfd00fc208fa6b68ded2efe95a5cbde2
Emerson Silva <emerson_jdss@hotmail.com>
2018-04-08 22:23:03 -0300

    unary operators

---
+++ javascript/u-courses/vanilla/Section-02/unaryOperators.js
@@ -0,0 +1,21 @@
+// + & - 
+
+// +
+3 + 3; // return 6
++ 3; // convert a next value in a Number, case a number don't return  
++ '3'; // convert string in a number 
++ 'emerson'; // return NaN
+
+'emerson' + ' junior'; // return emerson junior , concat values when strings 
+
+'3' + 3; // return '33' string
+
+3 + '3'; // return '33' string
+
+// Always concat with string, return a new string 
+
+// -----------------------------------------------------
+
+var x = 3;
+- x // return -3
+
