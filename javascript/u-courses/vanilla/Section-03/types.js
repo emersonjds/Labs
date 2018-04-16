9e0e1e23d12e0b1adb13d7615fcbbd2deda829da
Emerson Jr <emerson_jdss@hotmail.com>
2018-04-15 23:20:38 -0300

    understanding objects again

--- javascript/u-courses/vanilla/Section-03/types.js
+++ javascript/u-courses/vanilla/Section-03/types.js
@@ -6,9 +6,33 @@ var name = 'Emeson'
 // - boolean
 var correct = boolean;
 // - null e undefined
-var data = null;
+var data = null || undefined;
 
 // -----------------------------------------------
 
 // Object Types
+{ propriedade: 'valor' }
 
+var objeto = { propriedade: 'valor', propriedade2: 10, propriedade3: true };
+objeto.propriedade // return valor
+
+var pessoa = {
+    nome: 'Emerson',
+    idade: 28,
+    peso: 85,
+    altura: 1.66
+}
+
+pessoa.nome // return 'Emerson'
+pessoa.idade // return 28
+pessoa.altura // 1.66
+
+var carro = {
+    cor: 'vermelho',
+    motor: '2.0',
+    portas: 4
+}
+
+carro.cor // return vermelho
+carro.motor // return '2.0'
+carro.portas // 4
