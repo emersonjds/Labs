d698f2a7ecc40097d5d4d4fbd7930688fbcc7a0a
Emerson Jr <emerson_jdss@hotmail.com>
2018-04-20 23:22:20 -0300

    falsy values

--- javascript/u-courses/vanilla/Section-04/falsyValues.js
+++ javascript/u-courses/vanilla/Section-04/falsyValues.js
@@ -1,7 +1,15 @@
-//Falsy
+//Falsy Values in JS
 - undefined
 - null
 - NaN
   0
 - 0
-- 'ou'
+- ''
+
+var teste;
+
+`` ? teste = true : teste = false; // return false
+`0` ? teste = true : teste = false; // return true any string with value it's true
+undefined ? teste = true : teste = false; // return false
+null ? teste = true : teste = false; // return false
+{} ? teste = true : teste = false; // return true
