c15ceb3bf7b3c7221fe72806dbf629d9db94fb09
Emerson Jr <emerson_jdss@hotmail.com>
2018-04-25 16:29:21 -0300

    config reactive form

--- javascript/angular5/reactive-form/src/app/app.component.ts
+++ javascript/angular5/reactive-form/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  formulario: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      nome: null,
+      email: null
+    });
+  }
 }
--- javascript/angular5/reactive-form/src/app/app.module.ts
+++ javascript/angular5/reactive-form/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
