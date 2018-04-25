85b723c779886b17e2f3478fb62a37b3d2504ede
Emerson Jr <emerson_jdss@hotmail.com>
2018-04-25 17:59:55 -0300

    refactor form

--- javascript/angular5/reactive-form/src/app/app.component.ts
+++ javascript/angular5/reactive-form/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnChanges {
   title = 'app';
 
   formulario: FormGroup;
@@ -19,4 +19,13 @@ export class AppComponent implements OnInit {
       email: [null]
     });
   }
+
+  ngOnChanges() {
+    this.showValues();
+  }
+
+  showValues() {
+    console.log(this.formulario.value);
+  }
+  
 }
