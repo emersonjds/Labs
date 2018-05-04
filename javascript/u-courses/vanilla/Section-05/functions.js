61587110135fdbf94253a6041db4bc1ce04872f8
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-04 00:10:03 -0300

    testes with return of functions

--- javascript/u-courses/vanilla/Section-05/functions.js
+++ javascript/u-courses/vanilla/Section-05/functions.js
@@ -1,3 +1,16 @@
 (() => {
     return `IIFE`;
 })();
+
+function returnFunction(number) {
+    return function sum(x, y) {
+        return x + y * number;
+    }
+}
+returnFunction()(10,20)
+
+function multiply(x,y) {
+    return x * y;
+}
+
+function calc(multiply(10,28));
