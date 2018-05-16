e42caac49c2cf7539706d3504d8f7c630d84f2c7
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-15 22:57:31 -0300

    loop while

---
+++ javascript/u-courses/vanilla/Section-06/loopWhile.js
@@ -0,0 +1,16 @@
+while (true) {
+    // execute anything 
+}
+
+counter = 10;
+
+while (counter !== 0) {
+    console.log(counter--);
+}
+// when value to arrive 0, the instructions stop, because 0 has a falsy value 
+
+var counter = 10;
+
+while (counter--) {
+    console.log(counter);
+}
