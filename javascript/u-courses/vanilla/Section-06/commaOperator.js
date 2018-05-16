44ac98bf6b9bc814ff63cf2948891aa7913e161d
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-15 22:40:00 -0300

    comma operator

---
+++ javascript/u-courses/vanilla/Section-06/commaOperator.js
@@ -0,0 +1,9 @@
+var a, b = 2, c;
+
+function myFunction() {
+    var x = 0;
+    return (x += 1, x);
+}
+
+let b = 1, c = 4; 
+// With reserved word let or var, the variables received the values in sequence, but the ideal is use always var or let in each variable declareted
