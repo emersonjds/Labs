749942d7e95bd1f6a7ecd40a9ae286b2e1fa8439
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-15 22:45:46 -0300

    switch case operator

---
+++ javascript/u-courses/vanilla/Section-06/switchCase.js
@@ -0,0 +1,12 @@
+function myFunc(x) {
+    switch (x) {
+        case 1:
+            console.log(`x é igual a 1`);
+            break;
+        case 2:
+            console.log(`x é igual a 2`);
+            break;
+        default:
+            console.log(`x não é nem 1 nem 2`);
+    }
+}
