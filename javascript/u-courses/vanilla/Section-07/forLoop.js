01086e40198a07c0c7260f1d42ba8452fa6afdf6
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-22 19:30:18 -0300

    for loop

---
+++ javascript/u-courses/vanilla/Section-07/forLoop.js
@@ -0,0 +1,17 @@
+// Loop for pattern 
+// for(init, condition, final-expression)
+
+var num = 0;
+
+while (num < 20) {
+    console.log(num++);
+}
+
+for (var num = 0; num < 20; num++) {
+    console.log(num)
+}
+
+for (var num = 2, arroz = 3; num < 5, num++ ) {
+    console.log(num);
+    console.log(arroz + num);
+}
