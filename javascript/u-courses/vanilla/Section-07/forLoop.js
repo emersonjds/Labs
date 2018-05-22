f189cd2695c6db02eca191f43a7d8b1fc56c3738
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-22 19:32:42 -0300

    refactor function

--- javascript/u-courses/vanilla/Section-07/forLoop.js
+++ javascript/u-courses/vanilla/Section-07/forLoop.js
@@ -8,10 +8,10 @@ while (num < 20) {
 }
 
 for (var num = 0; num < 20; num++) {
-    console.log(num)
+    console.log(num);
 }
 
-for (var num = 2, arroz = 3; num < 5, num++ ) {
+for (var num = 2, arroz = 3; num < 5; num++) {
     console.log(num);
     console.log(arroz + num);
 }
