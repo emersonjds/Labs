9bc88723178f825117f4bbc7229ae92d108fdbb5
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-24 14:53:08 -0300

    learn new properties

--- python/python_alura/python_introduction/app.py
+++ python/python_alura/python_introduction/app.py
@@ -1,3 +1,12 @@
-convites, convites_ano_passado = 12, 20;
+convites, convites_ano_passado = 12, 20
 convites
 convites_ano_passado
+
+salario = 7500.50
+novos_convites = 10L
+
+perfil_aspas_duplas = "Perfil 1"
+perfil_aspas_simpels = 'Perfil 2'
+
+convidado = 'Emerson Junior Daniel Silva'
+convidado[0,14] # this call it is a slice method 
