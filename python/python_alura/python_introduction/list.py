5e630b356ea62a5d8f104921793e47b3257d65b5
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-25 15:15:50 -0300

    add list.py

---
+++ python/python_alura/python_introduction/list.py
@@ -0,0 +1,9 @@
+convite_1 = 'Emerson Silva'
+convite_2 = 'Lucas Silva'
+convite_3 = 'Ricardo Ledo'
+
+convites = ['Emerson Silva', 'Lucas Silva', 'Ricardo Ledo']
+
+convites[0]  # //access to position determineted
+convites[0: 2]  # return Emerson Silva , Lucas Silva with slice operator
+convites[1:]  # return Lucas Silva , Ricardo Ledo
