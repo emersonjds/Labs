fe090bcf8bb529ef108469fb4ad6815b5a52fb93
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-27 13:23:08 -0300

    named Functions

---
+++ javascript/u-courses/vanilla/Section-08/namedFunctions.js
@@ -0,0 +1,21 @@
+var func = function() {} // anonimous function ; wrong
+
+var func = function func() {} //rigth mode ; this facilities a debug of function 
+
+var func = function() {
+    return 'hi';
+}
+
+func(); // hi
+func.name; // func
+
+var func = function myFunction() {}
+
+func.name; // return myFunction
+
+// Literal Function
+function myFunction2 () {
+    return 'myFunction 2'
+}
+
+myFunction2.name // return myFunction2
