ba4be963041d9aac93899b515c1b663c03196f4f
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-27 13:52:50 -0300

    function return of another function

---
+++ javascript/u-courses/vanilla/Section-08/functionalProgramming2.js
@@ -0,0 +1,17 @@
+// return Function with another functions
+
+function adder(x) {
+    return function (y) {
+        return x + y;
+    }
+}
+
+var add2 = adder(2);
+console.log(add2(3)) // return 5
+
+function adder(x) {
+    function anotherFunction(y) {
+        return x + y
+    }
+    return anotherFunction;
+}
