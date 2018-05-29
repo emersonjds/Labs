159e2928a068fe133fecca16539df69cee57e793
Emerson Jr <emerson_jdss@hotmail.com>
2018-05-28 23:01:52 -0300

    correct error logic

--- javascript/u-courses/vanilla/Section-08/challenge-08.js
+++ javascript/u-courses/vanilla/Section-08/challenge-08.js
@@ -38,7 +38,7 @@ function showName() {
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
 
-var showName = showName;
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
@@ -46,7 +46,7 @@ atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
 
-console.log(`A função ${showName.name} retorna ${showName()}`);
+console.log(`A função ${varShowName.name} retorna ${varShowName()}`);
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
