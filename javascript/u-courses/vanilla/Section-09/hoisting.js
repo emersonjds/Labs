651cae3df71a83e75e36bbe4446d7da50835fc78
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-03 14:18:40 -0300

    hoisting example valid

--- javascript/u-courses/vanilla/Section-09/hoisting.js
+++ javascript/u-courses/vanilla/Section-09/hoisting.js
@@ -6,7 +6,7 @@ function myFunction() {
         return number1 + number2;
     }
 }
-console.log( myFunction() ); 
+console.log(myFunction());
 // this function returns undefined because the literal function are atribuited for a variable, in this case the hoisting of variables works of diferent form
 
 function myFunction() {
@@ -17,5 +17,14 @@ function myFunction() {
         number1 + number2;
     }
 }
-console.log( myFunction() );
+console.log(myFunction());
 // In this case the functions work because the hoisting mechanism is works with a literal function , that stay available in all scope of function
+
+function myFunction() {
+    function sum() { // hoisting mechanism
+        return n1 + n2; // closure mechanism
+    }
+    var n1 = 1;
+    var n2 = 2;
+    return sum();
+}
