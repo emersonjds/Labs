3172c8122b727e93632606fd5c988a2fafab0832
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-02 22:47:50 -0300

    understanding hoisting

---
+++ javascript/u-courses/vanilla/Section-09/hoisting.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var number1 = 1;
+    var number2 = 2;
+    return sum();
+    var sum = function sum() {
+        return number1 + number2;
+    }
+}
+console.log( myFunction() ); 
+// this function returns undefined because the literal function are atribuited for a variable, in this case the hoisting of variables works of diferent form
+
+function myFunction() {
+    var number1 = 1;
+    var number2 = 2;
+    return sum();
+    function sum() {
+        number1 + number2;
+    }
+}
+console.log( myFunction() );
+// In this case the functions work because the hoisting mechanism is works with a literal function , that stay available in all scope of function
