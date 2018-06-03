7181e4e21a52354a65c75098220c01a066cebb58
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-02 22:01:13 -0300

    scoped functions and closures

--- javascript/u-courses/vanilla/Section-09/scopedFunctions.js
+++ javascript/u-courses/vanilla/Section-09/scopedFunctions.js
@@ -1,6 +1,30 @@
 function myFunction() {
+    var number1 = 1;
+    var number2 = 2;
     function sum() {
-        return 1 + 2;
+        return number1 + number2;
     }
+    return sum(); // sum it´s a closure beacuse access variables external to her
+}
+
+console.log(myFunction()); // return 3
+console.log( sum() ); //return function is not defined because this scope is intern of function 
+
+function myFunction() {
+    function sum() {
+        number1 + number2;
+    }
+    var number1 = 1;
+    var number2 = 2;
+    return sum(); // return 3
+}
+
+function myFunction() {
+    var number1 = 1;
+    var number2 = 2;
     return sum();
+
+    function sum() {
+        return number1 + number2;
+    }
 }
