41b3bc586777bab9651cdaf9105387933d2ec23b
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-05 17:17:14 -0300

    first steps with js

---
+++ javascript/alura/introducao-javascript/js/script.js
@@ -0,0 +1,7 @@
+
+console.log('documento carregado');
+
+var titulo = document.querySelector('.titulo');
+titulo.innerHTML = 'Maximo Nutrição';
+console.log('requisições de chamadas pre ordendas')
+
