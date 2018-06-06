0947dc076ef39df16957be66576ccde869170f5b
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-06 12:25:14 -0300

    starts the first calc

--- javascript/alura/introducao-javascript/js/script.js
+++ javascript/alura/introducao-javascript/js/script.js
@@ -5,3 +5,11 @@ var titulo = document.querySelector('.titulo');
 titulo.innerHTML = 'Maximo Nutrição';
 console.log('requisições de chamadas pre ordendas')
 
+var paciente = document.querySelector('#primeiro-paciente');
+var pesoPaciente = paciente.querySelector('.info-peso').textContent;
+var alturaPaciente = paciente.querySelector('.info-altura').textContent;
+
+var imc = pesoPaciente / (alturaPaciente * alturaPaciente);
+var infoIMC = document.querySelector('.info-imc');
+
+infoIMC.textContent = imc
