62752d4877945388b50345cb96005b1b1cb263c2
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-07 22:33:04 -0300

    validations

--- javascript/alura/introducao-javascript/js/script.js
+++ javascript/alura/introducao-javascript/js/script.js
@@ -3,13 +3,16 @@ console.log('documento carregado');
 
 var titulo = document.querySelector('.titulo');
 titulo.innerHTML = 'Maximo Nutrição';
-console.log('requisições de chamadas pre ordendas')
+console.log('requisições de chamadas pre ordendas');
 
 var paciente = document.querySelector('#primeiro-paciente');
 var pesoPaciente = paciente.querySelector('.info-peso').textContent;
 var alturaPaciente = paciente.querySelector('.info-altura').textContent;
 
+if (pesoPaciente < 0)
+    console.log('peso invalido');
+
 var imc = pesoPaciente / (alturaPaciente * alturaPaciente);
 var infoIMC = document.querySelector('.info-imc');
 
-infoIMC.textContent = imc
+infoIMC.textContent = imc;
