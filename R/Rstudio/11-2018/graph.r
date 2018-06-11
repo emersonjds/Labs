2f23d146086ba0d8aa422878c78f142e5ad68605
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-11 20:26:50 -0300

    using logical operators

--- R/Rstudio/11-2018/graph.r
+++ R/Rstudio/11-2018/graph.r
@@ -17,3 +17,12 @@ boxplot(Banco_1$idade ~Banco_1$sexo)
 a <- c(0,1,1,1,2,3,5,4)
 a
 boxplot(a)
+
+selecao <-Banco_1[,c("id", "situacao")]
+selecao
+selecao <-Banco_1[Banco_1$sexo=="masculino",]
+selecao
+selecao <-Banco_1[Banco_1$sexo=="masculino" && Banco_1$idade >= 20,]
+selecao
+selecao <-Banco_1[Banco_1$sexo=="masculino" && Banco_1$idade >= 20,]
+selecao
