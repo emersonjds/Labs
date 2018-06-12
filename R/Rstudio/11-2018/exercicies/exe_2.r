9862b03ae87efddc3ff3f15c4f68f0babdbf8125
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-11 22:21:27 -0300

    work with graphs

--- R/Rstudio/11-2018/exercicies/exe_2.r
+++ R/Rstudio/11-2018/exercicies/exe_2.r
@@ -4,3 +4,6 @@ grap_s = barplot(table(Bank$sex), col=c("blue", "red"))
 grap_ = barplot(table(Bank$sex), col=c("blue", "red"))
 
 grap_sexrace = barplot(table(Bank$sexrace), col=c("green", "orange", "gray"))
+
+graph_jobcat = barplot(table(Bank$jobcat), col=c("purple", "yellow", "lightblue"))
+
