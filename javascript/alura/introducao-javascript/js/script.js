764af694354d9e5a0e6999b4098b744b3493d646
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-18 14:33:54 -0300

    refactor

--- javascript/alura/introducao-javascript/js/script.js
+++ javascript/alura/introducao-javascript/js/script.js
@@ -6,14 +6,23 @@ titulo.innerHTML = 'Maximo Nutrição';
 console.log('requisições de chamadas pre ordendas');
 
 var paciente = document.querySelector('#primeiro-paciente');
-var pesoPaciente = paciente.querySelector('.info-peso').textContent;
-var alturaPaciente = paciente.querySelector('.info-altura').textContent;
+
+var pesoPaciente = paciente.querySelector('.info-peso');
+var peso = pesoPaciente.textContent;
+
+var alturaPaciente = paciente.querySelector('.info-altura');
+var altura = alturaPaciente.textContent;
+
+var infoIMC = document.querySelector('.info-imc');
+
+var imc = pesoPaciente / (alturaPaciente * alturaPaciente);
+
+infoIMC.textContent = imc;
 
 if (pesoPaciente < 0 || pesoPaciente > 1000)
     console.log('peso invalido')
 
 
-var imc = pesoPaciente / (alturaPaciente * alturaPaciente);
-var infoIMC = document.querySelector('.info-imc');
 
-infoIMC.textContent = imc;
+
+
