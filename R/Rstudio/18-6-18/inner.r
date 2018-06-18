1521c52d8c9cec011fe325b6f454ae2474affd96
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-18 19:55:29 -0300

    Sql library

--- R/Rstudio/18-6-18/inner.r
+++ R/Rstudio/18-6-18/inner.r
@@ -1,4 +1,25 @@
+#all = full join
+#all.x = left join
+#all.y = right join 
+
 attach(Apend_A)
-chave <- merge(Apend_A, Apend_B, by=c("id"), all = TRUE)
-chave
+fulljoin <- merge(Apend_A, Apend_B, by=c("id"), all = TRUE) #all = full join
+fulljoin
+
+leftjoin <- merge(Apend_A, Apend_B, by=c("id"), all.x = TRUE) #left join
+leftjoin
+
+rightjoin <- merge(Apend_A, Apend_B, by=c("id"), all.y = TRUE) #left join
+rightjoin
+
+#sql library
+library(sqldf)
+selecionados <- sqldf("select * from Apend_A where idade >= 12")
+selecionados
+
+sql = sqldf("select * from Apend_A
+            inner join Apend_B on
+            Apend_A.id = Apend_B.id")
+sql
+
 
