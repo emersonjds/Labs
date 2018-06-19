f5ca5ea38ef1d143c309d5855d8c7e4b3d13787a
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-19 15:27:19 -0300

    refactor code to any pacients

--- javascript/alura/introducao-javascript/js/script.js
+++ javascript/alura/introducao-javascript/js/script.js
@@ -5,35 +5,41 @@ let titulo = document.querySelector('.titulo');
 titulo.innerHTML = 'Maximo Nutrição';
 console.log('requisições de chamadas pre ordendas');
 
-let paciente = document.querySelector('#primeiro-paciente');
+// let paciente = document.querySelector('#primeiro-paciente');
+let pacientes = document.querySelectorAll('.paciente');
 
-let pesoPaciente = paciente.querySelector('.info-peso');
-let peso = pesoPaciente.textContent;
+for (var i = 0; i <= pacientes.length; i++) {
+    let paciente = paciente[i];
+    let pesoPaciente = paciente.querySelector('.info-peso');
+    let peso = pesoPaciente.textContent;
 
-let alturaPaciente = paciente.querySelector('.info-altura');
-let altura = alturaPaciente.textContent;
+    let alturaPaciente = paciente.querySelector('.info-altura');
+    let altura = alturaPaciente.textContent;
 
-let alturaValida = true;
-let pesoValido = true;
+    let alturaValida = true;
+    let pesoValido = true;
 
-let infoIMC = document.querySelector('.info-imc');
+    let infoIMC = document.querySelector('.info-imc');
 
-if (peso <= 0 || peso >= 1000) {
-    console.log('peso invalido');
-    pesoValido = false;
-    infoIMC.textContent = 'Peso Invalido';
+    if (peso <= 0 || peso >= 1000) {
+        console.log('peso invalido');
+        pesoValido = false;
+        infoIMC.textContent = 'Peso Invalido';
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log('altura invalida');
+        alturaValida = false;
+        infoIMC.textContent = 'Altura Invalida';
+    }
+
+    if (pesoValido && alturaValida) {
+        var imc = peso / (altura * altura);
+        infoIMC.textContent = imc.toFixed(2); // arredonda as casas decimais para 2 digitos 
+    }
 }
 
-if (altura <= 0 || altura >= 3.00) {
-    console.log('altura invalida');
-    alturaValida = false;
-    infoIMC.textContent = 'Altura Invalida';
-}
 
-if (pesoValido && alturaValida) {
-    var imc = peso / (altura * altura);
-    infoIMC.textContent = imc;
-}
 
 
 
