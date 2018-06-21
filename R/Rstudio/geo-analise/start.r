e22d9e32da7dfa8bb870fb838f1ad4924130e592
Emerson Jr <emerson_jdss@hotmail.com>
2018-06-20 22:00:25 -0300

    refactoring again

--- R/Rstudio/geo-analise/start.r
+++ R/Rstudio/geo-analise/start.r
@@ -30,3 +30,47 @@ install.packages(sqldf)
 list.files()
 dir()
 install.packages()
+
+sqrt(9) # raiz quadrada
+
+prod(2,2)
+
+c(1:5) #cria conjuntos, concatena
+prod(c(1:5)) #produto 
+log(3, 10) #log 3 base 10
+
+View(iris)
+
+3 != 4
+cont <- 0;
+while (FALSE) {
+  print('fiap')
+  if (cont == 10) {
+    break()
+  }
+  cont++;
+}
+
+for (i in 1:10) {
+  x[i] <- rmorm(i)
+}
+
+abs(3-9)
+factorial(3)
+
+demo(graphics)
+
+valores <- c(22, 23, 24, 25, 26)
+valores
+valores[1] <- 100
+matrix(data = valores, nrow = 2, ncol = 5)
+
+rep(x = 2,10)
+
+rep(1:3, each = 5)
+temp <- runif(n = 200, min = 80, max = 100)
+temp
+
+hist(temp)
+
+runif()
