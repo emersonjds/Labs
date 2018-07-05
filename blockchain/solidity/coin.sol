c74ded7c8b2fd28d9daa6e507d2348f268837e9c
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-05 13:23:42 -0300

    refactor code

--- blockchain/solidity/coin.sol
+++ blockchain/solidity/coin.sol
@@ -1,19 +1,20 @@
 pragma solidity ^0.4.21;
 
 contract Coin {
+   
     address public minter;
     mapping (address => uint) public balances;
 
-    event Sent(address from, address to , uint amount);
+    event Sent(address from, address to, uint amount);
 
     function Coin() public {
         minter = msg.sender;
     }
 
-    function mint(address receiver, uint mount) public {
+    function mint(address receiver, uint amount) public {
         if (msg.sender != minter) return;
         balances[receiver] += amount;
-    } 
+    }
 
     function send(address receiver, uint amount) public {
         if (balances[msg.sender] < amount) return;
