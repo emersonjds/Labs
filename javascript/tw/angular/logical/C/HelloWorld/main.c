62363a3122129e12abb7d80be84885a954712ed7
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-12 23:51:34 -0300

    refactor c code

--- javascript/tw/angular/logical/C/HelloWorld/main.c
+++ javascript/tw/angular/logical/C/HelloWorld/main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int idade;
+    printf("Usuario digite a sua idade: ");
+    scanf("%d", &idade);
+    printf("Usuario voce tem %d anos.", idade);
+    // interpolation with d letter
     return 0;
 }
