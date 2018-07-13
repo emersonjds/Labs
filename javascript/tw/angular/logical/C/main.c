827c5eb39ce1eefeaf89eb3e0caf79027a762eb1
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-12 23:11:26 -0300

    variables in c

--- javascript/tw/angular/logical/C/main.c
+++ javascript/tw/angular/logical/C/main.c
@@ -3,19 +3,7 @@
 
 int main ()
 {
-  /*
-   declarações: todas as variáveis utilizadas
-   precisam ser declaradas
-  */
-  int idade;
-
-  /* programa */
-  printf("Quantos anos voce tem? \n");
-  scanf("%d", &idade);
-
-  printf("%d? Puxa, voce parece que tem so %d anos!\n", idade, idade * 2);
-
-  /* fim do programa */
-  system ("pause");
-  return 0;
+  int numero;
+  int numero2 = 14;
+  char nome[256];
 }
