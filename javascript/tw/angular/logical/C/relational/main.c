709f8b7dd3bcb25595dd5aa93831a788bea0b539
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-13 23:40:46 -0300

    relational operators

--- javascript/tw/angular/logical/C/relational/main.c
+++ javascript/tw/angular/logical/C/relational/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-
+    int numero;
+    printf("Digite um numero ");
+    scanf("%d", &numero);
+    printf("O numero digitado %d e maior que 10? %d \n", numero, numero > 10);
+    printf("O numero digitado %d e maior igual 10? %d \n", numero, numero >= 10);
+    printf("O numero digitado %d e menor que 10? %d \n", numero, numero < 10);
+    printf("O numero digitado %d e menor igual a 10? %d \n", numero, numero <= 10);
+    printf("O numero digitado %d e igual a 10? %d \n", numero, numero == 10);
     return 0;
 }
