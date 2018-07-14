9a33ceb6eaa52e058b9f3cd0cf6cbc8d32417fd0
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-13 23:22:06 -0300

    add new project

---
+++ javascript/tw/angular/logical/C/operators/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero, numero2;
+    printf("Digite o valor do primeiro numero ");
+    scanf("%d", &numero);
+    printf("Digite o valor para o segundo numero ");
+    scanf("%d", &numero2);
+    printf("%d + %d = %d \n", numero, numero2, numero + numero2);
+    printf("%d - %d = %d \n", numero, numero2, numero - numero2);
+    printf("%d x %d = %d \n", numero, numero2, numero * numero2);
+    printf("%d / %d = %d \n", numero, numero2, numero / numero2);
+    return 0;
+}
