be52df51b1b8b7fc7fa1ba9f08b74a200127f771
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-15 19:08:51 -0300

    for structure for test

---
+++ javascript/tw/angular/logical/C/for/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number;
+    printf("Digite o numero que deseja obter a tabuada");
+    scanf("%d", &number);
+    for (int i = 0; i < 10; i++)
+    {
+        printf(" %d x %d = %d ", number, i, number * i);
+    }
+    return 0;
+}
