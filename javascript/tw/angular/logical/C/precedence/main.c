75b1e79fd5b28d35bcacab358ed2ab99c9e4c3b6
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-15 16:49:49 -0300

    resolved exercise

--- javascript/tw/angular/logical/C/precedence/main.c
+++ javascript/tw/angular/logical/C/precedence/main.c
@@ -6,5 +6,7 @@ int main()
     printf("%d", (4 + 5) * 9);
     printf("%d", (20 == 20 || 20 == 5) && 10 > 10);
     //&& operator has procedente of another operators logicals
+    int var = 2 + 3 * 4 / 5 - 6;
+    printf("%d", var);
     return 0;
 }
