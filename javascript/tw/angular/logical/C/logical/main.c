1a173d4d40994d688bf243adb6a8b7c0cf7ff13f
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-15 00:03:50 -0300

    another changes with logical operators

--- javascript/tw/angular/logical/C/logical/main.c
+++ javascript/tw/angular/logical/C/logical/main.c
@@ -3,6 +3,15 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int numero1, numero2;
+    printf("Digite o valor de numero 1: ");
+    scanf("%d", &numero1);
+    printf("Digite o valor de numero 2: ");
+    scanf("%d", &numero2);
+    printf("%d > 10 e %d > 20 ?! %d \n", numero1, numero2, numero1 > 10 && numero2 > 20);
+    //with returns 0 equals false
+    printf("%d > 10 ou %d > 20 ?! %d \n", numero1, numero2, numero1 > 10 || numero2 > 20);
+    printf("nao %d > 10 e nao %d > 20 ?! %d \n", numero1, numero2, !(numero1 > 10) && !(numero2 > 20));
+    printf("nao %d > 10 e nao %d > 20 ?! %d \n", numero1, numero2, !(numero1 > 10) || !(numero2 > 20));
     return 0;
 }
