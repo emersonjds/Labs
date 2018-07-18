f190b79b575adfbfe14efb2e5274d036975406df
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-17 23:54:49 -0300

    add a new class

---
+++ javascript/tw/angular/oo/main.java
@@ -0,0 +1,16 @@
+public static void main(String[] args) {
+    public class Dog {
+        String breed;
+        int age;
+        String color;
+     
+        void barking() {
+        }
+     
+        void hungry() {
+        }
+     
+        void sleeping() {
+        }
+     }
+}
