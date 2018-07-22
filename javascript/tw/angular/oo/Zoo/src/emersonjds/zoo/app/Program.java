cf38688494b6997446ae0c579e6655393aef30c9
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-22 20:38:26 -0300

    working with instances

---
+++ javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/app/Program.java
@@ -0,0 +1,15 @@
+package emersonjds.zoo.app;
+
+import emersonjds.zoo.classes.Animal;
+
+// import emersonjds.zoo.classes.*; import all packages of the package
+// always use a unique import for itself data
+
+public class Program {
+
+	public static void main(String[] args) {
+		Animal cachorro = new Animal();
+		cachorro.setName("Toto");
+	}
+
+}
