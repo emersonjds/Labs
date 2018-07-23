b6851bdc18480a73f95df0881aa8d0988771a90c
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-22 22:52:59 -0300

    refactor class

--- javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Dentist.java
+++ javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Dentist.java
@@ -4,6 +4,7 @@ public class Dentist {
 	private String name;
 	private String midleName;
 	private String speciality;
+	private int croSubscription;
 	private boolean active = true;
 	
 	public Boolean isActive() {
@@ -12,6 +13,14 @@ public class Dentist {
 		} else {
 			return false;
 		}
+	}
+	
+	public int getCroSubscription() {
+		return croSubscription;
+	}
+
+	public void setCroSubscription(int croSubscription) {
+		this.croSubscription = croSubscription;
 	}
 
 	public String getName() {
@@ -41,4 +50,25 @@ public class Dentist {
 	public void setActive(boolean active) {
 		this.active = active;
 	}
+
+	public void attendPatient() {
+		System.out.println("O " + this.name + "esta atendendo um paciente no momento ");
+	}
+
+	public void markConsult() {
+		System.out.println("Consulta agendada com sucesso");
+	}
+
+	public void remarkConsult() {
+		System.out.println("Consulta remaracada com sucesso");
+	}
+
+	public void cancelConsult() {
+		System.out.println("Consulta foi cancelada com sucesso ");
+	}
+
+	public void travelToCongress() {
+		System.out.println("No momento estou em viagem");
+	}
+	
 }	
