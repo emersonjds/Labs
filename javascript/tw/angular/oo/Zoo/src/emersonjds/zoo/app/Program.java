152ada8b3d9b893242a6c16d52b782c63647a217
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-24 22:37:38 -0300

    add a new call

--- javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/app/Program.java
+++ javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/app/Program.java
@@ -1,6 +1,7 @@
 package emersonjds.zoo.app;
 
 import emersonjds.zoo.classes.Animal;
+import emersonjds.zoo.classes.Cat;
 import emersonjds.zoo.classes.Dog;
 
 // import emersonjds.zoo.classes.*; import all packages of the package
@@ -8,6 +9,7 @@ import emersonjds.zoo.classes.Dog;
 public class Program {
 	public static void main(String[] args) {
 		Dog cachorro = new Dog("Toto", 2, "Cachorro");
+		Cat cat = new Cat("Bob", 12, "gato");
 		System.out.println(
 			"Ola seu animal ï¿½ " + cachorro.getSpecie() + 
 			" o nome do animal ï¿½ " + cachorro.getNome() + 
@@ -17,5 +19,7 @@ public class Program {
 		} else {
 			System.out.println("Animal nao ï¿½ adulto");
 		}
+		
+		System.out.println("O nome do gato é " + cat.getNome());
 	}
 }
