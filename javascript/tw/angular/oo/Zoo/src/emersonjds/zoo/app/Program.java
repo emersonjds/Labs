8f4c68babb613b4bb554b6fe4cfe29c962d52586
Emerson Jr <emerson_jdss@hotmail.com>
2018-07-26 17:45:50 -0300

    refactor code

--- javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/app/Program.java
+++ javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/app/Program.java
@@ -1,30 +1,25 @@
 package emersonjds.zoo.app;
 
-import emersonjds.zoo.classes.Animal;
 import emersonjds.zoo.classes.Cat;
 import emersonjds.zoo.classes.Dog;
 
-// import emersonjds.zoo.classes.*; import all packages of the package
-// always use a unique import for itself data
 public class Program {
-	public static void main(String[] args) {
-		Dog cachorro = new Dog("Toto", 2, "Cachorro");
-		Cat cat = new Cat("Bob", 12, "gato");
-		System.out.println(
-			"Ola seu animal � " + cachorro.getSpecie() + 
-			" o nome do animal � " + cachorro.getNome() + 
-			" e ele tem " + cachorro.getAge() + " anos");
-		if(cachorro.isAdult()) {
-			System.out.println("Animal adulto");
-		} else {
-			System.out.println("Animal nao � adulto");
-		}
-		
-		System.out.println("O nome do gato � " + cat.getNome());
+    public static void main(String[] args) {
+        Dog cachorro = new Dog("Toto", 2, "Cachorro");
+        Cat cat = new Cat("Bob", 12, "gato");
+        System.out.println("Ola seu animal é " + cachorro.getSpecie() +  " o nome do animal é " + cachorro.getNome() +
+                 " e ele tem " + cachorro.getAge() + " anos");
+        if (cachorro.isAdult()) {
+            System.out.println("Animal adulto");
+        } else {
+            System.out.println("Animal nao � adulto");
+        }
 
-		System.out.println("=================");
-		System.out.println("Barulho do cachorro");
-		System.out.println("=================");
-		System.out.println("Barulho do gato");
-	}
+        System.out.println("O nome do gato é " + cat.getNome());
+
+        System.out.println("=================");
+        System.out.println("Barulho do cachorro");
+        System.out.println("=================");
+        System.out.println("Barulho do gato");
+    }
 }
