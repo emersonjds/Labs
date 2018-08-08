0bf51117a75ae8cc0fcd1c83b0afe3c4b1767e41
Emerson Jr <emerson_jdss@hotmail.com>
2018-08-07 23:44:57 -0300

    generate a new ArrayList

--- javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Zoologico.java
+++ javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Zoologico.java
@@ -1,27 +1,32 @@
 package emersonjds.zoo.classes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Zoologico
  */
 public class Zoologico {
-    // composition class, a zoo is compost of a list of the animals
+  // composition class, a zoo is compost of a list of the animals
+  private String name;
+  private List<Animal> animais;
 
-    private String name;
-    private List<Animal> animais;
+  public Zoologico() {
+    // when the class is initialize a new ArrayList of the Animal is create
+    animais = new ArrayList<Animal>();
+  }
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+  /**
+   * @return the name
+   */
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+  /**
+   * @param name the name to set
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 }
