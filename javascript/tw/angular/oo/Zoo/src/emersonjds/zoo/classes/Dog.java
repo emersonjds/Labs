1273703729405c8604f2f780be6392a7a3b21178
Emerson Jr <emerson_jdss@hotmail.com>
2018-08-07 23:58:40 -0300

    refactor code

--- javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Dog.java
+++ javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Dog.java
@@ -2,33 +2,33 @@ package emersonjds.zoo.classes;
 
 public class Dog extends Animal {
 
-	public Dog (String name, int age, String species) {
-		super(name, age, species);
-	}
-	
-	public Dog (String name) {
-		super(name);
-	}
-	
-	public Dog (String name, int age) {
-		super(name, age, "Rex");
-	}
+  public Dog(String name, int age, String species) {
+    super(name, age, species);
+  }
 
-	@Override
-	public void soundEmmiter() {
-		if(isLive) {
-			System.out.println("Au Au");
-		} else {
-			System.out.println("The animal is died");
-		}
-	}
-	
-	@Override
-	public Boolean isAdult () {
-		if(isLive) {
-			return age >= 2;
-		} else {
-			return false;
-		}
-	}
+  public Dog(String name) {
+    super(name);
+  }
+
+  public Dog(String name, int age) {
+    super(name, age, "Rex");
+  }
+
+  @Override
+  public void soundEmmiter() {
+    if (isLive) {
+      System.out.println("Au Au");
+    } else {
+      System.out.println("The animal is died");
+    }
+  }
+
+  @Override
+  public Boolean isAdult() {
+    if (isLive) {
+      return age >= 2;
+    } else {
+      return false;
+    }
+  }
 }
