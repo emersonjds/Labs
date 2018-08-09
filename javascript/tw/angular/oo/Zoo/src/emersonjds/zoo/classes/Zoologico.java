5669746579e4bcdd71a12b97150a7bb63fb69562
Emerson Jr <emerson_jdss@hotmail.com>
2018-08-09 00:00:14 -0300

    add a new type

--- javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Zoologico.java
+++ javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Zoologico.java
@@ -10,6 +10,7 @@ public class Zoologico {
   // composition class, a zoo is compost of a list of the animals
   private String name;
   private List<Animal> animais;
+  private type: String;
 
   public Zoologico() {
     // when the class is initialize a new ArrayList of the Animal is create
