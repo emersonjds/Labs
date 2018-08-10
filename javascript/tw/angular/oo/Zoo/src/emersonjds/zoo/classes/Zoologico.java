1511522fe81db3d47112656ba985209efa1a95b4
Emerson Jr <emerson_jdss@hotmail.com>
2018-08-10 00:17:20 -0300

    remove unused variable

--- javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Zoologico.java
+++ javascript/tw/angular/oo/Zoo/src/emersonjds/zoo/classes/Zoologico.java
@@ -10,7 +10,6 @@ public class Zoologico {
   // composition class, a zoo is compost of a list of the animals
   private String name;
   private List<Animal> animais;
-  private String types;
 
   public Zoologico() {
     // when the class is initialize a new ArrayList of the Animal is create
