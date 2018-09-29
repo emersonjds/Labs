f4dc456ec93b3c5e81402d801703868f6ff3ae5e
Emerson Jr <emerson_jdss@hotmail.com>
2018-09-29 13:33:12 -0300

    filter

--- nodejs/wendell-curso/database.js
+++ nodejs/wendell-curso/database.js
@@ -31,6 +31,18 @@ class Database {
     return dados;
   }
 
+  async remover(id) {
+    const dados = await this.obterDados();
+    // usamos a função nativa do Javascript para filtrar itens de uma lista 
+    // para cada item da lista chamara uma função e retorna uma nova lista baseada
+    // nas respostas com TRUE
+
+    // existem duas formas de mandar uma funcao 
+    // podemos chamar function nomeFuncao() {}
+    // ou podemos passar um parametro seguido para simular o corpo 
+    const dadosFiltrados = dados.filter(item => item.id === id)
+  }
+
 }
 
 //para usar o contexto de async/ await
@@ -39,9 +51,9 @@ class Database {
 // na pratica é a mesma funçao MAIN
 // com uma sintaxe diferente
 // para o vscode nao se perder na busca da função no hoisting
-;(async function main() {
+; (async function main() {
   const database = new Database();
-  await database.cadastrar({id: 1, name: 'War Machine'});
+  await database.cadastrar({ id: 1, name: 'War Machine' });
   const dados = await database.listar();
   console.log('xablau', dados)
 })()
