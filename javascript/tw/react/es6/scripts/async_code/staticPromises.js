9c00e52eac3d5e5ec0d21de993baa6d7150479d2
Emerson Jr <emerson_jdss@hotmail.com>
2018-11-13 15:04:41 -0200

    static Promises

---
+++ javascript/tw/react/es6/scripts/async_code/staticPromises.js
@@ -0,0 +1,30 @@
+// Também temos alguns métodos estáticos da API de Promise.
+
+// resolve()
+// Caso a gente queira criar uma Promise e a gente já tenha o valor retornado, não faz muito sentido instanciarmos uma Promise e chamar o “resolve()” dentro da função de callback.
+
+// Para facilitar, podemos executar a função “resolve()” diretamente, sem a necessidade de instanciar uma nova Promise.
+
+var myPromise = Promise.resolve(77);
+
+// Isso pode nos ajudar muito. Caso você queira garantir que um valor retornado seja uma Promise, use essa função.
+
+var myValue = myFunction();
+var myPromise = Promise.resolve(myValue);
+
+// Temos a função “myFunction()” como exemplo. Caso você não tenha certeza do retorno, ou quer apenas garantir que uma função receba uma Promise como parâmetro, use o “Promise.resolve()” para isso.
+
+//  -----------------------------------------------------------------
+// Promise Race 
+// Essa função recebe uma lista de Promises e executa o “then()” quando a primeira Promise for resolvida ou retornar um erro.
+var p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(500)
+  }, 2000)
+})
+
+var p2 = Promise.resolve(77);
+
+Promise.race([p1, p2]).then((value) => {
+  console.log(value)
+})
