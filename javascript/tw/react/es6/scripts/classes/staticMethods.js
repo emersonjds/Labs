2bfacf1462f868f9f87e192efcd65a6835b72e6e
Emerson Jr <emerson_jdss@hotmail.com>
2018-11-13 13:03:14 -0200

    understanding static methods

---
+++ javascript/tw/react/es6/scripts/classes/staticMethods.js
@@ -0,0 +1,14 @@
+class Numeros {
+  static double(number) {
+    return number * 2;
+  }
+
+  static triple(number) {
+    return number * 3
+  }
+}
+
+Com a utilização de metodos estaticos nao é necessario que a classe seja instanciada, caso a mesma seja instanciada os metodos estaticos perdem seus valores e nao podem ser utilizados, desta forma o ideal é que se use a classe chamando diretamente o metodo sem a instancia da mesma, metodos estaticos sao metodos que serao usados por muitas outras classes.
+
+Numeros.double(10) // return 20
+Numeros.triple(10) // return 30
