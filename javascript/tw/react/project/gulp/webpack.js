396c75c25500a5a79f6bb40aa397d9bf00310cf7
Emerson Jr <emerson_jdss@hotmail.com>
2018-11-14 18:05:22 -0200

    config webpack module

---
+++ javascript/tw/react/project/gulp/webpack.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+webpack = require('webpack');
+
+gulp.task("webpack", function (callback) {
+  webpack({
+    entry: `./scripts/app.js`, // arquivo principal
+    output: { // saida
+      path: "./dist/js",
+      filename: `app.js`
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel',
+          query: {
+            presets: ['react', 'es2015', 'stage-0']
+          }
+        }
+      ]
+    }
+  })
+}, function (err, stats) {
+  if(err) throw new gutil.PluginError("webpack", err);
+  console.log("[webpack]", stats.toString({
+    //output options
+  }));
+  callback;
+});
