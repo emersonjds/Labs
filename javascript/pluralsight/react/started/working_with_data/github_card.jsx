01e2d32ad616d318a4333fbd3ae2b870d9ecfcf0
Emerson Silva <emerson_jdss@hotmail.com>
2018-11-17 17:01:59 -0200

    ref element

--- javascript/pluralsight/react/started/working_with_data/github_card.jsx
+++ javascript/pluralsight/react/started/working_with_data/github_card.jsx
@@ -12,24 +12,6 @@ const Card = (props) => {
   )
 }
 
-let data = [
-  {
-    name: "Emerson Silva",
-    avatar_url: "https://avatars2.githubusercontent.com/u/12503997?s=460&v=4",
-    company: "Facebook"
-  },
-  {
-    name: "Emerson Silva",
-    avatar_url: "https://avatars2.githubusercontent.com/u/12503997?s=460&v=4",
-    company: "Facebook"
-  },
-  {
-    name: "Emerson Silva",
-    avatar_url: "https://avatars2.githubusercontent.com/u/12503997?s=460&v=4",
-    company: "Facebook"
-  }
-]
-
 const CardList = (props) => {
   return (
     <div>
@@ -39,11 +21,48 @@ const CardList = (props) => {
 }
 
 class Form extends React.Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    //referencia ao campo de input
+    console.log('Event: Form Event', this.userNamenput.value)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input 
+        ref={(input) => this.userNamenput = input}
+        type="text" 
+        placeholder="Github User" />
+        <button type="submit"> Add Card </button>
+      </form>
+    )
+  }
+
+}
+
+class App extends React.Component {
+
+  state = {
+    cards : [
+      {
+        name: "Emerson Silva",
+        avatar_url: "https://avatars2.githubusercontent.com/u/12503997?s=460&v=4",
+        company: "Facebook"
+      },
+      {
+        name: "Emerson Silva",
+        avatar_url: "https://avatars2.githubusercontent.com/u/12503997?s=460&v=4",
+        company: "Facebook"
+      }
+    ]
+  }
+
   render() {
     return (
       <div>
-        <input style={{}} type="text"/>
-        <CardList cards={data}/>
+        <Form />
+        <CardList cards={this.state.cards}/>
       </div>
     )
   }
@@ -51,4 +70,4 @@ class Form extends React.Component {
 
 //create a reference for array or list in declaration of component
 // ReactDOM.render(<CardList cards={data} />, mountNode);
-ReactDOM.render(<Form />, mountNode);
+ReactDOM.render(<App />, mountNode);
