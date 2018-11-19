5fd3a35cce28c539de6829ce2466dc1be9356ffe
Emerson Silva <emerson_jdss@hotmail.com>
2018-11-18 23:17:52 -0200

    mini todo list

--- javascript/tw/react/react-pokedex/src/App.js
+++ javascript/tw/react/react-pokedex/src/App.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PokeListItem, { pokeListItem } from './Components/pokeListItem';
+// import PokeListItem, { pokeListItem } from './Components/pokeListItem';
 import { MyListItem, MyList } from './myList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img className="pokeball-back" id="pokeball-back"
+        {/* <img className="pokeball-back" id="pokeball-back"
           src="//hanashiro.github.io/pokedex/images/pokeball.svg"
         />
         <input type="text" id="pokeFilter" placeholder="digite o nome buscado" />
         <h2> Teste </h2>
 
-        <PokeListItem />
+        <PokeListItem /> */}
 
-        <br />
+        {/* <br /> */}
 
-        <MyListItem text="ABC" color="blue" onClick={console.log.bind(console, 'item clicado')} />
+        {/* <MyListItem text="ABC" color="blue" onClick={console.log.bind(console, 'item clicado')} /> */}
 
-        <br />
+        {/* <br /> */}
 
-        <MyList>
-          <span>ABC</span>
-          <span>DEF</span>
-        </MyList>
+        <MyList></MyList>
 
-        
       </div>
     );
   }
--- javascript/tw/react/react-pokedex/src/myList.js
+++ javascript/tw/react/react-pokedex/src/myList.js
@@ -14,32 +14,56 @@ export class MyListItem extends React.Component {
     Channel.emit('listItem:click');
   }
 
+  removeItem = (index) => {
+    Channel.emit('listItem:remove', index)
+  }
+
   render() {
     var props = this.props,
       state = this.state,
       style = { "color": props.color };
     return (
-      <li onClick={this.click} style={style} >{props.text}  - {state.totalClicks}</li>
+      <li onClick={this.click} style={style} >{props.text}  - {state.totalClicks}
+        <button onClick={this.removeItem.bind(this, props.index)}> X </button>
+      </li>
     )
   }
 }
 
 export class MyList extends React.Component {
   state = {
-    totalClicks: 0
+    totalClicks: 0,
+    items: []
   }
 
   componentDidMount = () => {
     Channel.on('listItem:click', this.childClick);
+    Channel.on('listItem:remove', this.removeItem)
   }
 
   componentWillUnmount = () => {
     Channel.removeListener('listItem:click', this.childClick);
+    Channel.removeListener('listItem:remove', this.removeItem)
   }
 
   childClick = () => {
-    var totalClicks = ++ this.state.totalClicks;
-    this.setState({totalClicks})
+    var totalClicks = ++this.state.totalClicks;
+    this.setState({ totalClicks })
+  }
+
+  addItem = (event) => {
+    var items = this.state.items,
+    text = this.refs.itemText.value || `Ìtem ${items.length}`;
+
+    this.refs.itemText.value = '';
+    items.push({ text });
+    this.setState({ items })
+  }
+
+  removeItem = (index) => {
+    var items = this.state.items;
+    items.splice(index, 1);
+    this.setState({items})
   }
 
   render() {
@@ -48,21 +72,24 @@ export class MyList extends React.Component {
 
     return (
       <div>
-        <h3>Total de Itens: {props.children.length}</h3>
+        <h3>Total de Itens: {state.items.length}</h3>
         <h3>Total de Cliques: {state.totalClicks}</h3>
+        <input type="text" ref="itemText"/>
+        <button onClick={this.addItem.bind(this)}>Adicionar</button>
         <ul>
           {
             // this.props.children.map((child, index) => {
             //   return <li>{child}</li>
             // })
             //children neste caso é o span adeclarado dentro do componente
-            this.props.children.map((child, index) => {
-              return <MyListItem 
-                onClick={this.childClick.bind(this)}
+            state.items.map((item, index) => {
+              return <MyListItem
+                // onClick={this.childClick.bind(this)}
                 color="red"
                 // neste caso estamos acessando o texto dentro da tag span que é definido como props deste componente filho
-                text={child.props.children}
-                key={index} />
+                text={item.text}
+                key={index} 
+                index={index}/>
             })
           }
         </ul>
