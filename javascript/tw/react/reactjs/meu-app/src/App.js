dd7ca3e74663b4877831b657360497c6bf7d4b07
Emerson Silva <emerson_jdss@hotmail.com>
2018-11-18 23:37:25 -0200

    refactor fix with eventEmmitter

--- javascript/tw/react/reactjs/meu-app/src/App.js
+++ javascript/tw/react/reactjs/meu-app/src/App.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TextCounter from './TextCounter';
+import { MyListItem, MyList } from './myList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <br/>
-        <TextCounter title="Studies"/>
-        <br/>
+
+        {/* <br /> */}
+
+        {/* <MyListItem text="ABC" color="blue" onClick={console.log.bind(console, 'item clicado')} /> */}
+
+        {/* <br /> */}
+
+        <MyList></MyList>
+
       </div>
     );
   }
