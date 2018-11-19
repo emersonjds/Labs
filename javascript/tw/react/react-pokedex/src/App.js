dc81bd4c1ee9a063081b21acf3fb36e07ae2f8ab
Emerson Silva <emerson_jdss@hotmail.com>
2018-11-18 23:21:15 -0200

    reorder files

--- javascript/tw/react/react-pokedex/src/App.js
+++ javascript/tw/react/react-pokedex/src/App.js
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import PokeListItem, { pokeListItem } from './Components/pokeListItem';
-import { MyListItem, MyList } from './myList';
+import PokeListItem, { pokeListItem } from './Components/pokeListItem';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <img className="pokeball-back" id="pokeball-back"
+        <img className="pokeball-back" id="pokeball-back"
           src="//hanashiro.github.io/pokedex/images/pokeball.svg"
         />
         <input type="text" id="pokeFilter" placeholder="digite o nome buscado" />
         <h2> Teste </h2>
 
-        <PokeListItem /> */}
-
-        {/* <br /> */}
-
-        {/* <MyListItem text="ABC" color="blue" onClick={console.log.bind(console, 'item clicado')} /> */}
-
-        {/* <br /> */}
-
-        <MyList></MyList>
-
+        <PokeListItem />
       </div>
     );
   }
