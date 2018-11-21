2536b9b602912317f71f077e900233c2b577baf8
Emerson Silva <emerson_jdss@hotmail.com>
2018-11-21 21:08:11 -0200

    global variables

---
+++ javascript/rocketseat/starter/js/scripts/variables.js
@@ -0,0 +1,13 @@
+var nome = 'Emerson';
+var age = 23;
+var average = 80.5;
+var human = true;
+var students = ['Emerson', 'Maria', 'José'];
+var person = {
+    name: nome,
+    age: age
+}
+
+console.log('person', person)
+console.log('student', students[1])
+console.log('name person', person.name)
