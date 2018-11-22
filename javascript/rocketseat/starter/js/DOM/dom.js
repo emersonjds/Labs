34dd42631d5fd38777171f5507db1a81621dc3b0
Emerson Silva <emerson_jdss@hotmail.com>
2018-11-21 23:58:03 -0200

    dom manipulation

--- javascript/rocketseat/starter/js/DOM/dom.js
+++ javascript/rocketseat/starter/js/DOM/dom.js
@@ -1,3 +1,16 @@
 function showAlert() {
     alert('Botao clicado')
 }
+
+// var inputElement = document.querySelector('#nome')
+
+// busca pela propriedade da tag 
+var inputElement = document.querySelector('input[name=nome]')
+
+var btnElement = document.querySelector('.botao');
+
+btnElement.onclick = function() {
+    var text = inputElement.value;
+    alert(text)
+}
+console.log(inputElement)
