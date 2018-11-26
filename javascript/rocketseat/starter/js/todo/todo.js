19ed9c0d7cac9a16d99e5a1b6aec8f9c991b82b5
Emerson Jr <emerson_jdss@hotmail.com>
2018-11-26 17:05:59 -0200

    add delete button

--- javascript/rocketseat/starter/js/todo/todo.js
+++ javascript/rocketseat/starter/js/todo/todo.js
@@ -8,12 +8,31 @@ var todos = [
 ];
 
 function renderTodos() {
+  listElement.innerHTML = '';
+  
   for (todo of todos) {
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
+    var deleteLink = document.createElement('a');
+    var textLink = document.createTextNode(' X');
+
+    deleteLink.appendChild(textLink)
+    deleteLink.setAttribute('href', '#');
+
     todoElement.appendChild(todoText);
-    listElement.appendChild(todoElement)
+    todoElement.appendChild(deleteLink);
+    listElement.appendChild(todoElement);
+
   }
 }
+
+function addTodo() {
+  var todoText = inputElement.value;
+  todos.push(todoText);
+  inputElement.value = '';
+  renderTodos();
+}
+
+buttonElement.onclick = addTodo; // receite function 
 
 renderTodos();
