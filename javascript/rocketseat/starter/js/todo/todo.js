2fcfb7dd2924729e8a0be0c80400eac096728902
Emerson Jr <emerson_jdss@hotmail.com>
2018-11-26 15:59:08 -0200

    generate todos list

---
+++ javascript/rocketseat/starter/js/todo/todo.js
@@ -0,0 +1,19 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+  'Fazer Café',
+  'Acessar comunidade',
+];
+
+function renderTodos() {
+  for (todo of todos) {
+    var todoElement = document.createElement('li');
+    var todoText = document.createTextNode(todo);
+    todoElement.appendChild(todoText);
+    listElement.appendChild(todoElement)
+  }
+}
+
+renderTodos();
