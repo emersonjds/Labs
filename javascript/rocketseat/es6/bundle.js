0ef544d0bab6a1ef034f9ce3902f7a8e12a92840
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-01 20:23:58 -0200

    literal templates

--- javascript/rocketseat/es6/bundle.js
+++ javascript/rocketseat/es6/bundle.js
@@ -219,6 +219,11 @@ function showName(_ref) {
 showName(usuario);
 "use strict";
 
+var nome = 'Emerson';
+var idade = 29;
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade));
+"use strict";
+
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
---
+++ javascript/rocketseat/es6/scripts/emplate_literals.js
@@ -0,0 +1,4 @@
+const nome = 'Emerson';
+const idade = 29;
+
+console.log(`Meu nome é ${nome} e tenho ${idade}`);
