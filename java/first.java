013334c7e217781890ded316408e99de5dcd9df6
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-01 15:25:06 -0200

    organize folders and files

--- java/first.java
+++
@@ -1,6 +0,0 @@
-class MeuPrimeiroPrograma {
-    public static void main(String[] args) {
-        System.out.println("O primeiro de muitos códigos escritos em " + args[0] + " pelo " + args[1] + "!");
-        System.out.println("came back to Java");
-    }
-}
