7805733e564823e5e13bd80b9d4845dbd9140386
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-01 17:36:43 -0200

    arrow functions

---
+++ javascript/rocketseat/es6/scripts/arrow_functions.js
@@ -0,0 +1,8 @@
+const arr = [1, 2, 3, 6, 9, 12];
+
+const newArray = arr.map(data => data * 2);
+const newArray = arr.map(data => 'teste');
+const newArray = arr.map(data => [1, 3, 4, 52]);
+// Para retornar um objeto nestes moedes é necessario que ele seja embrulhado em um parenteses assim como no JSX
+const newArray = arr.map(data => ({name: 'Emeson '}) );
+
