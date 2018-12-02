ff5c7e4bc67334a198baf3049522dbdb2508c8d1
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-02 17:03:44 -0200

    challenge axios + async await

--- javascript/rocketseat/es6/src/main.js
+++ javascript/rocketseat/es6/src/main.js
@@ -1,17 +1,73 @@
-import axios from 'axios';
+import axios from 'axios'
 
-class API {
-  static async getUserInfo(username) {
-    try {
-      const response = await axios.get(`https://api.github.com/users/${username}`)
-      console.log(response);
-    } catch (error) { 
-      console.warn('Erro na API');
-    }
+// função delay
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+function umPorSegundo() {
+  delay().then(() => {
+    console.log('1s');
+    delay().then(() => {
+      console.log('2s');
+      delay().then(() => {
+        console.log('3s');
+      });
+    })
+  });
+}
+
+const umPorSegundoV2 = async () => {
+  const response = await umPorSegundo();
+  console.log(response);
+}
+umPorSegundoV2();
+
+// -------------------------------------------------------------------------------
+function getUserFromGithub(user) {
+  axios.get(`https://api.github.com/users/${user}`)
+    .then(response => {
+      console.log(response.data);
+    }).catch(err => {
+      console.log('Usuario nao existe')
+    })
+}
+
+const getUserGithub = async (username) => {
+  try {
+    const response = await getUserFromGithub(username)
+    console.log(response);
+  } catch (error) {
+    console.warn('Erro na API');
   }
 }
 
-API.getUserInfo('emersonjds');
+getUserGithub('emersonjds');
+getUserGithub('emersonasdfsda');
 
-// ao utilizar async await como o retorno nao tem o catch para manipulação direta como numa promise o ideal é utilizar o try / catch para caso algum erro aconteça na request
-// O axios por padrao retorna uma promise para ser trabalhada, por isso async e await funcionam normalmente com ele 
+// -------------------------------------------------------------------------------
+
+class Github {
+  static async getRepositories(repo) {
+    try {
+      const response = await axios.get(`https://api.github.com/repos/${repo}`);
+
+      console.log(response.data);
+    } catch (err) {
+      console.log("Repositório não existe");
+    }
+  }
+}
+Github.getRepositories("rocketseat/rocketseat.com.br");
+Github.getRepositories("rocketseat/dslkvmskv");
+
+// --------------------------------------------------------------------------
+
+const buscaUsuario = async usuario => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${user}`);
+    console.log(response.data);
+  } catch (err) {
+    console.log("Usuário não existe");
+  }
+};
+
+buscaUsuario("emersonjds");
