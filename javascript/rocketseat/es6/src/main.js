9ff04de3e05532b03f0200ad195605e30a82ee35
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-02 20:05:42 -0200

    end of app

--- javascript/rocketseat/es6/src/main.js
+++ javascript/rocketseat/es6/src/main.js
@@ -24,20 +24,26 @@ class App {
       return;
     }
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
 
-    console.log(response);
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
+      console.log(response);
+      const { name, description, html_url, owner: { avatar_url } } = response.data;
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
+      this.inputEl.value = '';
+      this.render();
+    } catch (e) {
+      alert('O repositorio nao existe', e.message);
+    }
 
-    const { name, description, html_url, owner: { avatar_url } } = response.data;
+    this.setLoading(false);
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url
-    })
-    this.inputEl.value = '';
-    this.render();
   }
 
   render() {
@@ -66,6 +72,18 @@ class App {
 
       this.listEl.appendChild(listItemEl);
     })
+  }
+
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando...'));
+      loading.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove;
+    }
   }
 }
 
