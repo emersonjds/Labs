8885b0cc4b66c661dc9fbe54a789686b28b8d56a
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-02 16:35:19 -0200

    axios + async / await

---
+++ javascript/rocketseat/es6/src/main.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+class API {
+  static async getUserInfo(username) {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${username}`)
+      console.log(response);
+    } catch (error) { 
+      console.warn('Erro na API');
+    }
+  }
+}
+
+API.getUserInfo('emersonjds');
+
+// ao utilizar async await como o retorno nao tem o catch para manipulação direta como numa promise o ideal é utilizar o try / catch para caso algum erro aconteça na request
+// O axios por padrao retorna uma promise para ser trabalhada, por isso async e await funcionam normalmente com ele 
