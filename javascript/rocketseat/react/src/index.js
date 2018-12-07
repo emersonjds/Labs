e7d96c9cca870c74ba8496181e8db76da8037deb
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-06 22:09:46 -0200

    understanding Fragments

--- javascript/rocketseat/react/src/index.js
+++ javascript/rocketseat/react/src/index.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
+
+class Button extends Component {
+  render() {
+    return <a href="#">Enviar </a>
+  }
+}
 
 class App extends Component {
   render() {
-    return <h1>Hello</h1>
+    return (
+      <Fragment>
+        <h1>Hello</h1>
+        <Button></Button>
+      </Fragment>
+    )
   }
 }
 
 render(<App />, document.getElementById('app'));
+
+// Para que nao haja quebra de layout existe uma outra propriedade chamada Fragment que pode ser utilizada ao inves da div na renderização do componente
