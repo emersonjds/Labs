016c74133a84985832e7e9984a024b7e14b4bfce
Emerson Silva <emerson_jdss@hotmail.com>
2018-12-12 09:09:41 -0200

    config eslint

--- javascript/rocketseat/git-compare/src/pages/Main/index.js
+++ javascript/rocketseat/git-compare/src/pages/Main/index.js
@@ -1,13 +1,24 @@
-import React from "react";
-import { Container, Form } from "./styles";
-import CompareList from "../../components/CompareList";
-const Main = () => (
-  <Container>
-    <Form>
-      <input type="text" placeholder="usuario/repositorio" />
-      <button type="submit">OK</button>
-    </Form>
-    <CompareList />
-  </Container>
-);
-export default Main;
+import React, { Component } from 'react';
+import { Container, Form } from './styles';
+import CompareList from '../../components/CompareList';
+
+export default class Main extends Component {
+  state = {
+    repositories: [],
+  };
+
+  render() {
+    const { repositories } = this.state;
+    return (
+      <Container>
+        <Form>
+          <input type="text" placeholder="usuario/repositorio" />
+          <button type="submit">OK</button>
+        </Form>
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+}
+
+// api.github.com / repos / facebook / react;
