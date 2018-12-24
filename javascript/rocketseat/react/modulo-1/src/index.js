16cfa240946b2fcb9cd280e22bc2a6e80a12ce94
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-24 19:57:54 -0200

    states and immutability

--- javascript/rocketseat/react/modulo-1/src/index.js
+++ javascript/rocketseat/react/modulo-1/src/index.js
@@ -4,28 +4,31 @@ import { PropTypes } from "prop-types";
 
 class Button extends Component {
   render() {
-    return (
-      <a href="#" onCilck={this.props.onClick}>
-        {this.props.children}
-      </a>
-    );
+    return <button onClick={this.props.onClick}>{this.props.children}</button>;
   }
   // return <a href="#">{this.props.title}</a>;
+
+  static defaultProps = {
+    children: "Salvar"
+  };
+
+  static propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.string
+  };
 }
 
-Button.defaultProps = {
-  children: "Salvar"
-};
-
-Button.propTypes = {
-  onClick: PropTypes.func.isRequired,
-  children: PropTypes.string
-};
-
 class App extends Component {
-  handleClick() {
-    alert("Botao clicado");
-  }
+  state = {
+    counter: 0
+  };
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
   render() {
     return (
       <Fragment>
