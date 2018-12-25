1da8880f1c478bc5746345d3c9928d42c096dbb5
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-25 13:58:40 -0200

    config initial webpack

---
+++ javascript/rocketseat/blog-project/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "scss-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
