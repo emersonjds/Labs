43c1400d2259f18ec40d8b42e95cda34b683a3ff
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-26 22:16:58 -0200

    config eslint + editorconfig

--- javascript/rocketseat/react/app02/src/App.js
+++ javascript/rocketseat/react/app02/src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from "react";
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Teste</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <h1>Teste</h1>
+  </div>
+);
 
 export default App;
