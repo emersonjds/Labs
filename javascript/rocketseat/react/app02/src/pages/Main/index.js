d5e052f94cdf47f4f92ab5e6f9eec3738de34403
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-27 19:28:53 -0200

    config first steps to fetch api data

--- javascript/rocketseat/react/app02/src/pages/Main/index.js
+++ javascript/rocketseat/react/app02/src/pages/Main/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Form, Container } from './style';
 import CompareList from '../../components/CompareList';
 
-const Main = () => (
-  <Container>
-    <h1>Git Compare</h1>
-    {/* <img src="#"> */}
+export default class Main extends Component {
+  state = {};
 
-    <Form>
-      <input type="text" placeholder="usuario/repositorio" />
-      <button type="submit">Ok</button>
-    </Form>
-    <CompareList />
-  </Container>
-);
+  render() {
+    return (
+      <Container>
+        <h1>Git Compare</h1>
+        {/* <img src="#"> */}
 
-export default Main;
+        <Form>
+          <input type="text" placeholder="usuario/repositorio" />
+          <button type="submit">Ok</button>
+        </Form>
+        <CompareList />
+      </Container>
+    );
+  }
+}
