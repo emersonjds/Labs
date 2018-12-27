79abb3177f38db7019e4a8e137786b5736fdb24a
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-26 22:36:57 -0200

    config styled global component

--- javascript/rocketseat/react/app02/src/App.js
+++ javascript/rocketseat/react/app02/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import GlobalStyle from './styles/global';
 
 const App = () => (
-  <div className="App">
-    <h1>Teste</h1>
-  </div>
+  <Fragment>
+    <GlobalStyle />
+    <div className="App" />
+  </Fragment>
 );
 
 export default App;
