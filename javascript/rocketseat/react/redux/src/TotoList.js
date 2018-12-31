0ecc2633ddb1d179bafaa59b5f528f1a8ebad038
Emerson Jr <emerson_jdss@hotmail.com>
2018-12-31 16:14:26 -0200

    config Actions and dispatchers for reducers

--- javascript/rocketseat/react/redux/src/TotoList.js
+++ javascript/rocketseat/react/redux/src/TotoList.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
 //essa prop conecta o componente com alguma informação do reducer do redux
 import { connect } from "react-redux";
 
-const TodoList = props => (
-  <ul>
-    {props.todos.map(data => (
-      <li key={data.id}>{data.text}</li>
-    ))}
-  </ul>
+const TodoList = ({ todos, addTodo }) => (
+  <Fragment>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+    <button onClick={() => addTodo("Novo Todo")}>Adicionar Todo</button>
+  </Fragment>
 );
 
 //mapeia estados do redux em propriedades para o componente
 const mapStateToProps = state => ({
   // o state neste caso representa todas as informações contidas no Store do redux, e o todos é o reducer de todos que foi criado
   todos: state.todos
+});
+
+// o dispatch sao as actions que serao disparadas do componente para as Actions em si
+const mapDispatchToProps = dispatch => ({
+  addTodo: text =>
+    dispatch({
+      type: "ADD_TODO",
+      payload: {
+        text
+      }
+    })
 });
 
 TodoList.propTypes = {
@@ -28,4 +42,7 @@ TodoList.propTypes = {
 };
 
 // High Orders Function (Componente)
-export default connect(mapStateToProps)(TodoList);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
