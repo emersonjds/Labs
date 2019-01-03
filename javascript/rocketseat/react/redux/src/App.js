eabdc57a6e33d160fcd0a0d0c7514b33b0259e0b
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-02 23:38:09 -0200

    config reactron

--- javascript/rocketseat/react/redux/src/App.js
+++ javascript/rocketseat/react/redux/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
+import "./config/reactotron";
 import store from "./store";
 import TodoList from "./TotoList";
 
--- javascript/rocketseat/react/redux/src/store/index.js
+++ javascript/rocketseat/react/redux/src/store/index.js
@@ -1,8 +1,15 @@
 // inicializa o redux na aplicação
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import reducers from "./reducers";
 
 // neste ponto sao passado os reducers
-const store = createStore(reducers);
+// const store = createStore(reducers);
+
+const createAppropriateStore =
+  process.env.NODE_ENV === "development"
+    ? console.tron.createStore
+    : createStore;
+
+const store = createAppropriateStore(reducers, compose(applyMiddleware(...[])));
 
 export default store;
