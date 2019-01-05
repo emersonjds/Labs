55133db7f9df70663f2d2373595a02c1ccfacf2e
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-04 23:43:35 -0200

    config main page

--- javascript/rocketseat/react/app-redux/src/pages/Main/index.js
+++ javascript/rocketseat/react/app-redux/src/pages/Main/index.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 
-const Main = () => <h1>Teste</h1>;
+export default class Main extends Component {
+  state = {
+    repositoryInput: ""
+  };
+  handleAddRepository = () => {};
+  render() {
+    return (
+      <Fragment>
+        <form onSubmit={this.handleAddRepository}>
+          <input
+            placeholder="usuario/value"
+            value={this.state.repositoryInput}
+            onChange={e =>
+              this.setState({
+                repositoryInput: e.target.value
+              })
+            }
+          />
+          <button type="submit">Adicionar</button>
+        </form>
 
-export default Main;
+        <ul>
+          <li>
+            <p>
+              <strong>Facebook/react</strong>
+            </p>
+            <a href="#">Acessar github</a>
+          </li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
