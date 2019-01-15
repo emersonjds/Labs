b04a42b12a5832584a3f5c2641764ea2962d341b
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-15 16:45:27 -0200

    verify duplicated repository

--- javascript/rocketseat/react/app-redux/src/store/sagas/favorites.js
+++ javascript/rocketseat/react/app-redux/src/store/sagas/favorites.js
@@ -1,19 +1,27 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import api from '../../services/api';
 import { Creators as FavoriteActions } from '../ducks/favorites';
 
 export function* addFavorite(action) {
   try {
+    // Promise
     const { data } = yield call(api.get, `/repos/${action.payload.repository}`);
-    // compose object
-    const repositoryData = {
-      id: data.id,
-      name: data.full_name,
-      description: data.description,
-      url: data.html_url,
-    };
-    yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+
+    const isDuplicated = yield select(state => state.favorites.data.find(favorite => favorite.id === data.id));
+
+    if (isDuplicated) {
+      yield put(FavoriteActions.addFavoriteFailure('repositorio duplicado'));
+    } else {
+      // compose object
+      const repositoryData = {
+        id: data.id,
+        name: data.full_name,
+        description: data.description,
+        url: data.html_url,
+      };
+      yield put(FavoriteActions.addFavoriteSuccess(repositoryData));
+    }
   } catch (err) {
-    yield put(FavoriteActions.addFavoriteFailure('erro ao adicionar repositorio', err.message));
+    yield put(FavoriteActions.addFavoriteFailure('erro ao adicionar repositorio'));
   }
 }
