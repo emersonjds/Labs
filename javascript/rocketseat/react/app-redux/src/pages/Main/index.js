81484663eede1d91dc65c6486e12b3246413cdd3
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-14 23:53:49 -0200

    change to single quotes

--- javascript/rocketseat/react/app-redux/src/pages/Main/index.js
+++ javascript/rocketseat/react/app-redux/src/pages/Main/index.js
@@ -18,6 +18,7 @@ class Main extends Component {
           url: PropTypes.string,
         }),
       ),
+      error: PropTypes.oneOfType([null, PropTypes.string]),
     }).isRequired,
   };
 
@@ -48,10 +49,10 @@ class Main extends Component {
           />
           <button type="submit">Adicionar</button>
           {this.props.favorites.loading && <span>Carregando...</span>}
-          {
-            // eslint-disable-next-line react/destructuring-assignment
-            !!this.props.favorites.error && <span style={{ color: '#F00' }}>{this.props.favorites.error}</span>
-          }
+          {// eslint-disable-next-line react/destructuring-assignment
+          !!this.props.favorites.error && (
+            <span style={{ color: '#F00' }}>{this.props.favorites.error}</span>
+          )}
         </form>
 
         <ul>
--- javascript/rocketseat/react/app-redux/src/store/reducers/index.js
+++ javascript/rocketseat/react/app-redux/src/store/reducers/index.js
@@ -1,6 +1,6 @@
-import { combineReducers } from "redux";
-import favorites from "./favorites";
+import { combineReducers } from 'redux';
+import favorites from './favorites';
 
 export default combineReducers({
-  favorites
+  favorites,
 });
