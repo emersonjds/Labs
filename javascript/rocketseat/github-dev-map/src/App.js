bf7399fe7148d550881b2ad6fb14ef957210b693
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-15 18:01:24 -0200

    config mabox in app

--- javascript/rocketseat/github-dev-map/src/App.js
+++ javascript/rocketseat/github-dev-map/src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MapGL, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-class App extends Component {
+export default class Map extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: -23.5439948,
+      longitude: -46.6065452,
+      zoom: 14
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this._resize);
+  }
+
+  componentWillMount() {
+    window.removeEventListener("resize", this._resize);
+  }
+
+  _resize = () => {
+    this.setState({
+      ...this.state.viewport({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    });
+  };
+
+  handleMapClick(e) {
+    const [latitude, longitude] = e.lngLat;
+    console.log(latitude, longitude);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MapGL
+        {...this.state.viewport}
+        onClick={this.handleMapClick}
+        mapStyle="mapbox://styles/mapbox/basic-v9"
+        mapboxApiAccessToken={
+          "pk.eyJ1IjoiZW1lcnNvbmpkcyIsImEiOiJjanF5NGZjZHcwMDJ5M3lsandvaGlvcXBrIn0.mHlWfI4BpweXRc5sF6q1Jw"
+        }
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+        <Marker
+          latitude={-23.5439948}
+          longitude={-46.6065452}
+          onClick={this.handleMapClick}
+          captureClick={true}
+        >
+          <img
+            style={{
+              borderRadius: 100,
+              width: 48,
+              height: 48
+            }}
+            src="https://avatars2.githubusercontent.com/u/12503997?v=4"
+          />
+        </Marker>
+      </MapGL>
     );
   }
 }
-
-export default App;
