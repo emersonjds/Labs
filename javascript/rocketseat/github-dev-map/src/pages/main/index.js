4c937ab4e77a74da3f184d5c305b5d742f6b3446
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-17 16:17:24 -0200

    config Main page

--- javascript/rocketseat/github-dev-map/src/pages/main/index.js
+++ javascript/rocketseat/github-dev-map/src/pages/main/index.js
@@ -1,5 +1,15 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
 
-const Main = () => <Fragment />;
+import LeftBar from '../../components/LeftBar';
+import AddUser from '../../components/AddUser';
+import Map from '../../components/Map';
+
+const Main = () => (
+  <Fragment>
+    <Map />
+    <LeftBar />
+    <AddUser />
+  </Fragment>
+);
 
 export default Main;
