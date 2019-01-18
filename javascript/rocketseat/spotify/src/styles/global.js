e80806ff68c192140b5502bc518c4b5be2d8c8c8
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-18 20:57:31 -0200

    remove deprecated lib in application

---
+++ javascript/rocketseat/spotify/src/styles/global.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  outline: 0;
+}
+
+html, body, #root {
+  height: 100%;
+}
+
+body {
+  text-rendering: optimizeLegibility !important;
+  -webkit-font-smoothing: antialiased !important;
+  font-family: 'Montserrat', sans-serif;
+  height: 100%;
+  background-color: #181818;
+  color: #FFF;
+}
+`;
+
+export default GlobalStyle;
