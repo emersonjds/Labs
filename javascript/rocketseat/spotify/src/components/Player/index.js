3c0cc744fad319a50c6f5e0f76fd7fafa5e49dc7
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-19 23:36:10 -0200

    configure inferior nav

--- javascript/rocketseat/spotify/src/components/Player/index.js
+++ javascript/rocketseat/spotify/src/components/Player/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Current } from './styles';
 
-const Player = () => <Container />;
+const Player = () => (
+  <Container>
+    <Current>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/91OKWPQcuEL._SL1500_.jpg"
+        alt="Teste"
+      />
+      <div>
+        <span> Times Like This</span>
+        <small>Teste</small>
+      </div>
+    </Current>
+  </Container>
+);
 
 export default Player;
--- javascript/rocketseat/spotify/src/components/Player/styles.js
+++ javascript/rocketseat/spotify/src/components/Player/styles.js
@@ -8,3 +8,30 @@ export const Container = styled.div`
   background: #282828;
   padding: 12px;
 `;
+
+export const Current = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    height: 48px;
+    width: 48px;
+  }
+
+  div {
+    margin-left: 12px;
+    display: flex;
+    flex-direction: column;
+
+    span {
+      font-size: 13px;
+      color: #fff;
+    }
+
+    small {
+      font-size: 11px;
+      color: #b3b3b3;
+      margin-top: 5px;
+    }
+  }
+`;
