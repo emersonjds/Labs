5ac510adadb1dc1642f6ebd433d5edcfd8a07f94
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-21 23:54:35 -0200

    add header component

---
+++ javascript/rocketseat/spotify/src/components/Header/index.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Content = styled.header`
+  display: flex;
+  justify-content: space-between;
+`;
---
+++ javascript/rocketseat/spotify/src/components/Header/style.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Content} from './styles';
+
+const Header = () => (
+  <Content>
+
+  </Content>
+);
+
+export const Header;
