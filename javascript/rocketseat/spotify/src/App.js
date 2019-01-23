19b950faa0fc7f924cf132ffc0bf242da776d1f3
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-22 23:54:45 -0200

    config global content

--- javascript/rocketseat/spotify/src/App.js
+++ javascript/rocketseat/spotify/src/App.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
-import GlobalStyle from './styles/global';
+import GlobalStyle, { Content } from './styles/global';
 import { Wrapper, Container } from './styles/components';
+
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
+import Header from './components/Header';
 
 const App = () => (
   <Fragment>
@@ -10,10 +12,13 @@ const App = () => (
     <Wrapper>
       <Container>
         <Sidebar />
+        <Content>
+          <Header />
+        </Content>
       </Container>
       <Player />
     </Wrapper>
   </Fragment>
 );
- 
+
 export default App;
