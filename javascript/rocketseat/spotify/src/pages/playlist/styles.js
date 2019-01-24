474a1fd514df3e3ac736148172ebae22ed60d1b1
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-24 21:17:34 -0200

    add link in styled component

---
+++ javascript/rocketseat/spotify/src/pages/playlist/styles.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Playlist = () => <h1>playlist</h1>;
+
+export default Playlist;
--- javascript/rocketseat/spotify/src/routes/index.js
+++ javascript/rocketseat/spotify/src/routes/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 import Browse from '../pages/browse';
+import Playlist from '../pages/playlist/styles';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Browse} />
+    <Route path="/playlists/:id" component={Playlist} />
   </Switch>
 );
 
