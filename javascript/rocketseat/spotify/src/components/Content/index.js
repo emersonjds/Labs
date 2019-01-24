b8f6cb7c857c63db22873bdfcdafd8092d3e9260
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-23 23:35:11 -0200

    content

---
+++ javascript/rocketseat/spotify/src/components/Content/index.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class index extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Teste</h1>
+      </div>
+    );
+  }
+}
