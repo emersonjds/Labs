b83f4176f32f42c865195e7b2af48a5330a40848
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-25 21:25:03 -0200

    configure ducks and sagas

--- javascript/rocketseat/spotify/src/App.js
+++ javascript/rocketseat/spotify/src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import { Wrapper, Container, Content } from './styles/components';
@@ -8,23 +9,26 @@ import Player from './components/Player';
 import Header from './components/Header';
 
 import Routes from './routes';
+import store from './store';
 
 const App = () => (
-  <BrowserRouter>
-    <Fragment>
-      <GlobalStyle />
-      <Wrapper>
-        <Container>
-          <Sidebar />
-          <Content>
-            <Header />
-            <Routes />
-          </Content>
-        </Container>
-        <Player />
-      </Wrapper>
-    </Fragment>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <GlobalStyle />
+        <Wrapper>
+          <Container>
+            <Sidebar />
+            <Content>
+              <Header />
+              <Routes />
+            </Content>
+          </Container>
+          <Player />
+        </Wrapper>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
