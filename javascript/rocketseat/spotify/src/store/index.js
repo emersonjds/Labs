8bfbc12a44670a2130f8f80350a75e242427262b
Emerson Jr <emerson_jdss@hotmail.com>
2019-01-25 21:36:56 -0200

    config vscode snippet to config Store

--- javascript/rocketseat/spotify/src/store/index.js
+++ javascript/rocketseat/spotify/src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+
 import createSagaMiddleware from 'redux-saga';
 
 import sagas from './sagas';
+
 import reducers from './ducks';
 
 const middlewares = [];
