74d16fc397cb4cd391470fafe36bd3de62e8bc8b
Emerson Jr <emerson_jdss@hotmail.com>
2019-02-03 20:51:36 -0200

    utils duration and position of music

--- javascript/rocketseat/spotify/src/components/Player/index.js
+++ javascript/rocketseat/spotify/src/components/Player/index.js
@@ -19,11 +19,16 @@ import ForwardIcon from '../../assets/images/forward.svg';
 import RepeatIcon from '../../assets/images/repeat.svg';
 
 const Player = ({
-  player, play, pause, prev, next,
+  player, play, pause, prev, next, playing, position, duration,
 }) => (
   <Container>
     {!!player.currentSong && (
-      <Sound url={player.currentSong.file} playStatus={player.status} onFinishedPlaying={next} />
+      <Sound
+        url={player.currentSong.file}
+        playStatus={player.status}
+        onFinishedPlaying={next}
+        onPlaying={playing}
+      />
     )}
     <Current>
       {!!player.currentSong && (
@@ -68,7 +73,7 @@ const Player = ({
         </button>
       </Controls>
       <Time>
-        <span>1:39</span>
+        <span>{position}</span>
         <ProgressSlider>
           <Slider
             railStyle={{ background: '#404040' }}
@@ -76,7 +81,7 @@ const Player = ({
             handleStyle={{ border: 0 }}
           />
         </ProgressSlider>
-        <span>4:29</span>
+        <span>{duration}</span>
       </Time>
     </Progress>
 
@@ -106,10 +111,24 @@ Player.propTypes = {
   pause: PropTypes.func.isRequired,
   prev: PropTypes.func.isRequired,
   next: PropTypes.func.isRequired,
+  playing: PropTypes.func.isRequired,
+  position: PropTypes.string.isRequired,
+  duration: PropTypes.string.isRequired,
 };
+
+function msToTime(duration) {
+  let seconds = parseInt((duration / 1000) % 60, 10);
+  const minutes = parseInt((duration / (100 * 60)) % 60, 10);
+
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return `${minutes}:${seconds}`;
+}
 
 const mapStateToProps = state => ({
   player: state.player,
+  position: msToTime(state.player.position),
+  duration: msToTime(state.player.duration),
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
--- javascript/rocketseat/spotify/src/store/ducks/player.js
+++ javascript/rocketseat/spotify/src/store/ducks/player.js
@@ -6,12 +6,15 @@ export const Types = {
   PAUSE: 'player/PAUSE',
   PREV: 'player/PREV',
   NEXT: 'player/NEXT',
+  PLAYING: 'player/PLAYING',
 };
 
 const INITIAL_STATE = {
   currentSong: null,
   list: [],
   status: Sound.status.PLAYING,
+  position: null,
+  duration: null,
 };
 
 export default function player(state = INITIAL_STATE, action) {
@@ -49,6 +52,11 @@ export default function player(state = INITIAL_STATE, action) {
       }
       return { ...state };
     }
+    case Types.PLAYING:
+      return {
+        ...state,
+        ...action.payload,
+      };
     default:
       return state;
   }
@@ -60,4 +68,5 @@ export const Creators = {
   pause: () => ({ type: Types.PAUSE }),
   prev: () => ({ type: Types.PREV }),
   next: () => ({ type: Types.NEXT }),
+  playing: ({ position, duration }) => ({ type: Types.PLAYING, payload: { position, duration } }),
 };
