31b44057a1e442e93049a4f4509dd40c7f0b9453
Emerson Jr <emerson_jdss@hotmail.com>
2019-02-07 22:39:34 -0200

    errorFailure Test

--- javascript/rocketseat/tests/src/store/sagas/__tests__/todos.test.js
+++ javascript/rocketseat/tests/src/store/sagas/__tests__/todos.test.js
@@ -1,7 +1,11 @@
 import SagaTester from 'redux-saga-tester';
+import MockAdapter from 'axios-mock-adapter';
+import api from '../../../services/api';
 import rootSaga from '../index';
 
 import { Types as TodosTypes, Creators as TodosActions } from '../../ducks/todos';
+
+const apiMock = new MockAdapter(api);
 
 describe('Todo Sagas', () => {
   let sagaTester = null;
@@ -11,15 +15,23 @@ describe('Todo Sagas', () => {
   });
 
   it('should be able to fetch todos from API', async () => {
+    const todosFixure = [
+      { id: 0, text: 'Lavar a louça' },
+      { id: 1, text: 'Pythom' },
+      { id: 2, text: 'React' },
+    ];
+    apiMock.onGet('todos').reply(200, todosFixure);
     sagaTester.dispatch(TodosActions.getTodosRequest());
     await sagaTester.waitFor(TodosTypes.GET_SUCCESS);
+    expect(sagaTester.getLatestCalledAction()).toEqual(TodosActions.getTodosSuccess(todosFixure));
+  });
 
+  it('should fail if response is not ok', async () => {
+    apiMock.onGet('todos').reply(400, {});
+    sagaTester.dispatch(TodosActions.getTodosRequest());
+    await sagaTester.waitFor(TodosTypes.GET_FAILURE);
     expect(sagaTester.getLatestCalledAction()).toEqual(
-      TodosActions.getTodosSuccess([
-        { id: 0, text: 'Lavar a louça' },
-        { id: 1, text: 'Pythom' },
-        { id: 2, text: 'React' },
-      ]),
+      TodosActions.getTodosFailure('Erro na requisição'),
     );
   });
 });
