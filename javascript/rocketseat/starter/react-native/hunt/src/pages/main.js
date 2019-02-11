2b739319207cc2f2c1f4e143d7b2bb4b9fce4bb8
Emerson Jr <emerson_jdss@hotmail.com>
2019-02-10 23:41:30 -0200

    bugfix main page

---
+++ javascript/rocketseat/starter/react-native/hunt/src/pages/main.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export default class Main extends Component {
+  static navigationOptions = {
+    title: "Js Hunt"
+  };
+  render() {
+    return (
+      <View>
+        <Text> Pagina Main </Text>
+      </View>
+    );
+  }
+}
---
+++ javascript/rocketseat/starter/react-native/hunt/src/routes.js
@@ -0,0 +1,6 @@
+import { createStackNavigator } from "react-navigation";
+import Main from "./pages/main";
+
+export default createStackNavigator({
+  Main
+});
