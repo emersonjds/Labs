ada1de3d954fd89a27f71f793f4e13e0be1112b4
Emerson Jr <emerson_jdss@hotmail.com>
2019-02-13 19:32:21 -0200

    config api call

--- javascript/rocketseat/starter/react-native/hunt/src/pages/main.js
+++ javascript/rocketseat/starter/react-native/hunt/src/pages/main.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import api from "../services/api";
 
 export default class Main extends Component {
   static navigationOptions = {
@@ -9,10 +10,28 @@ export default class Main extends Component {
     },
     headerTintColor: "#FFF"
   };
+
+  state = {
+    docs: []
+  };
+
+  componentDidMount = () => {
+    this.loadProducts();
+  };
+
+  loadProducts = async () => {
+    const response = await api.get("/products");
+    const { docs } = response.data;
+    this.setState({ docs });
+  };
+
   render() {
     return (
       <View>
         <Text> Pagina Main </Text>
+        {this.state.docs.map(product => (
+          <Text key={product._id}>{product.title}</Text>
+        ))}
       </View>
     );
   }
