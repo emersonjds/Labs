8b9f32b7817ff5875f5fd4dabe2fca53f8eddf13
Emerson Silva <emerson_jdss@hotmail.com>
2019-02-19 17:35:49 -0300

    default props

--- javascript/rocketseat/goNative/modulo1/src/components/Todo.js
+++ javascript/rocketseat/goNative/modulo1/src/components/Todo.js
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 export default class Todo extends Component {
+  static defaultProps = {
+    title: "Todo Padrao"
+  };
+
   render() {
     return (
       <View>
--- javascript/rocketseat/goNative/modulo1/src/index.js
+++ javascript/rocketseat/goNative/modulo1/src/index.js
@@ -7,6 +7,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Todo title="fazer café" />
+        <Todo />
       </View>
     );
   }
