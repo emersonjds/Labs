2c0bdb765816393e7a06c59a5a1e052dcc4c218c
Emerson Silva <emerson_jdss@hotmail.com>
2019-02-22 13:12:58 -0300

    solved challenge 1

--- javascript/rocketseat/goNative/challenge1/src/index.js
+++ javascript/rocketseat/goNative/challenge1/src/index.js
@@ -1,23 +1,124 @@
 import './config/ReactotronConfig';
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Platform, StyleSheet, ScrollView, View, Text,
+} from 'react-native';
 
-const white = '#F5FCFF';
+import Post from './components/Post/Post';
+
+const white = '#FFF';
+const primary = '#EE7777';
+const title = '#333';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: primary,
+  },
+
+  header: {
     backgroundColor: white,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // No iOS precisamos aplicar 20px de padding superior
+    // já que a Status Bar não joga o conteúdo do App
+    // pra baixo automaticamente
+    ...Platform.select({
+      ios: {
+        height: 76,
+        paddingTop: 20,
+      },
+      android: {
+        height: 56,
+        paddingTop: 0,
+      },
+    }),
+  },
+
+  headerTitle: {
+    color: title,
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>Welcome to React Native!</Text>
-  </View>
-);
+export default class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'Aprendendo React Native',
+        author: 'Emerson Silva',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 2,
+        title: 'Aprendendo React Native',
+        author: 'Emerson Silva',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 3,
+        title: 'Aprendendo React Native',
+        author: 'Emerson Silva',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 4,
+        title: 'Aprendendo React Native',
+        author: 'Emerson Silva',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 5,
+        title: 'Aprendendo React Native',
+        author: 'Emerson Silva',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 6,
+        title: 'Aprendendo React Native',
+        author: 'Emerson Silva',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 7,
+        title: 'Aprendendo React Native',
+        author: 'Emerson Silva',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+      {
+        id: 8,
+        title: 'Aprendendo React Native',
+        author: 'Emerson Silva',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      },
+    ],
+  };
 
-export default App;
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Teste App</Text>
+        </View>
+        <ScrollView>
+          {posts.map(post => (
+            <Post key={post.id} data={post} />
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
