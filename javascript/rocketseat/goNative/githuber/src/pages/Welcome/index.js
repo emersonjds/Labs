a4963cf6ca0358a9601fd9d4b9409708aa2b261f
Emerson Silva <emerson_jdss@hotmail.com>
2019-02-22 14:39:16 -0300

    config styles

--- javascript/rocketseat/goNative/githuber/src/pages/Welcome/index.js
+++ javascript/rocketseat/goNative/githuber/src/pages/Welcome/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  View, Text, TextInput, TouchableOpacity, StatusBar,
+} from 'react-native';
 
 import styles from './styles';
 
 const Welcome = () => (
   <View style={styles.container}>
-    <Text>Teste</Text>
+    <Text style={styles.title}>Bem-vindo</Text>
+    <StatusBar barStyle="light-content" backgroundColor="" />
+    <Text style={styles.text}>
+      Para continuar precisamos que voce informe seu usuario do github
+    </Text>
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect="false"
+        placeholder="Digite seu usario"
+        underlineColorAndroid="transparent"
+      />
+    </View>
+    <TouchableOpacity style={styles.button} onPress={() => {}}>
+      <Text style={styles.text}> Prosseguir</Text>
+    </TouchableOpacity>
   </View>
 );
 
--- javascript/rocketseat/goNative/githuber/src/pages/Welcome/styles.js
+++ javascript/rocketseat/goNative/githuber/src/pages/Welcome/styles.js
@@ -1,11 +1,46 @@
 import { StyleSheet } from 'react-native';
-import { colors } from '../../styles/index';
+import { colors, metrics } from '../../styles/index';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors.primary,
+    backgroundColor: colors.secundary,
     flex: 1,
-    width: '100%',
+    padding: metrics.basePadding * 2,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  title: {
+    color: colors.white,
+    textAlign: 'center',
+    fontSize: 24,
+  },
+  text: {
+    textAlign: 'center',
+    marginTop: metrics.baseMargin,
+    fontSize: 15,
+    color: colors.light,
+    lineHeight: 21,
+  },
+  form: {
+    marginTop: metrics.baseMargin * 2,
+  },
+  input: {
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    paddingHorizontal: metrics.basePadding,
+  },
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: metrics.baseRadius,
+    height: 4,
+    marginTop: metrics.baseMargin,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: colors.white,
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
 
