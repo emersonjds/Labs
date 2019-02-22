a0ef0308d4410cbe05faa98b730e340daeeb8a59
Emerson Silva <emerson_jdss@hotmail.com>
2019-02-22 12:20:38 -0300

    config files

---
+++ javascript/rocketseat/goNative/challenge/src/index.js
@@ -0,0 +1,23 @@
+import './config/ReactotronConfig';
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const white = '#F5FCFF';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: white,
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to React Native!</Text>
+  </View>
+);
+
+export default App;
