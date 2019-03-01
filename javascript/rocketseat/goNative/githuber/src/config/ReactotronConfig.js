a9fdab134ad9628912b394aec5a68e79e45537c3
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-01 20:18:03 -0300

    config eslint

--- javascript/rocketseat/goNative/githuber/src/config/ReactotronConfig.js
+++ javascript/rocketseat/goNative/githuber/src/config/ReactotronConfig.js
@@ -1,5 +1,5 @@
 import Reactotron from "reactotron-react-native";
 
-Reactotron.configure({ host: "192.168.15.17" }) // controls connection & communication settings
-  .useReactNative() // add all built-in react native plugins
-  .connect(); // let's connect!
+Reactotron.configure()
+  .useReactNative()
+  .connect();
