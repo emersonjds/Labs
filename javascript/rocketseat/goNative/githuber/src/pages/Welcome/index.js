4c99fe64117d14bd21132f2d6278376968a015c1
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-05 16:05:15 -0300

    config signIn + AsyncStorage

--- javascript/rocketseat/goNative/githuber/src/pages/Welcome/index.js
+++ javascript/rocketseat/goNative/githuber/src/pages/Welcome/index.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
+import api from "../../services/api";
 
 import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
-  StatusBar
+  StatusBar,
+  AsyncStorage
 } from "react-native";
 import styles from "./styles";
 
 export default class Welcome extends Component {
+  state = {
+    username: ""
+  };
+
+  checkUserExists = async username => {
+    const user = await api.get(`/users/${username}`);
+    return user;
+  };
+
+  saveUser = async username => {
+    await AsyncStorage.getItem("@Githuber:username", username);
+  };
+
+  signIn = async () => {
+    const { username } = this.state;
+    try {
+      await this.checkUserExists(username);
+      await this.saveUser(username);
+    } catch (err) {
+      console.log("usuario inexistente");
+    }
+  };
+
   render() {
+    const { username } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
@@ -26,8 +52,10 @@ export default class Welcome extends Component {
             autoCorrect={false}
             placeholder="Digite seu usuario github"
             underlineColorAndroid="transparent"
+            value={username}
+            onChangeText={text => this.setState({ username: text })}
           />
-          <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <TouchableOpacity style={styles.button} onPress={this.signIn}>
             <Text style={styles.buttonText}>Prosseguir</Text>
           </TouchableOpacity>
         </View>
