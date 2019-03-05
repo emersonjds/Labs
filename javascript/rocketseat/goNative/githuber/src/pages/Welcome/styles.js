29a4f41a442e526f1bde1a1f6404cab0c38eef4d
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-04 21:43:52 -0300

    config colors and metrics

--- javascript/rocketseat/goNative/githuber/src/pages/Welcome/styles.js
+++ javascript/rocketseat/goNative/githuber/src/pages/Welcome/styles.js
@@ -1,10 +1,12 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../styles/index";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    backgroundColor: colors.primary
   }
 });
 
