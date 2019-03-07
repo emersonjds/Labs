badec6c403918863c8af43331095a4cdfeabb213
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-06 23:52:05 -0300

    config header

---
+++ javascript/rocketseat/goNative/githuber/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
+
+// import styles from './styles';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hedader</Text>
+      </View>
+    );
+  }
+}
