b78c299fb54a2e2cece3d93faa0fe6be714afeba
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-09 15:18:21 -0300

    config request repos

--- javascript/rocketseat/goNative/githuber/src/pages/Repositories/index.js
+++ javascript/rocketseat/goNative/githuber/src/pages/Repositories/index.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, AsyncStorage, Text } from 'react-native';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Header from '../../components/Header';
+import api from '../../services/api';
 
-const Repositories = () => (
-  <View>
-    <Header title="Repositorios" />
-  </View>
-);
-
-Repositories.navigationOptions = {
-  tabBarIcon: ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />,
+const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
 };
 
-export default Repositories;
+export default class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
+
+  state = {
+    data: [],
+  };
+
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber: username');
+    const response = await api.get(`/users/${username}/repos`);
+    this.setState({
+      data: response.data,
+    });
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <View>
+        <Header title="Repositorios" />
+        {data && data.map(repo => <Text>{repo.name}</Text>)}
+      </View>
+    );
+  }
+}
