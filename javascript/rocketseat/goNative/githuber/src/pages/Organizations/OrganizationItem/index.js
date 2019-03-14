a12a7c29824f4710910762cc2d74f6ca1a912ed6
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-13 21:25:29 -0300

    config initial start Organizations

---
+++ javascript/rocketseat/goNative/githuber/src/pages/Organizations/OrganizationItem/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+// import styles from './styles';
+
+const OrganizationItem = () => <View />;
+
+export default OrganizationItem;
--- javascript/rocketseat/goNative/githuber/src/pages/Organizations/index.js
+++ javascript/rocketseat/goNative/githuber/src/pages/Organizations/index.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import { View, AsyncStorage } from 'react-native';
+import {
+  View, AsyncStorage, FlatList, ActivityIndicator,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import PropTypes from 'prop-types';
 import Header from '../../components/Header';
+import OrganizationItem from './OrganizationItem';
+import api from '../../services/api';
+import styles from './styles';
 
 AsyncStorage.clear();
 
-const Organizations = () => (
-  <View>
-    <Header title="Organizações" />
-  </View>
-);
-
-Organizations.navigationOptions = {
-  tabBarIcon: ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />,
+const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
 };
 
-export default Organizations;
+export default class Organizations extends Component {
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />,
+  };
+
+  state = {
+    // eslint-disable-next-line react/no-unused-state
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
+
+  async componentDidMount() {
+    this.loadingOrganizations();
+  }
+
+  renderListItem = ({ item }) => <OrganizationItem organization={item} />;
+
+  loadingOrganizations = async () => {
+    this.setState({ refreshing: true });
+    const username = await AsyncStorage.getItem('@Githuber: username');
+    const { data } = await api.get(`/users/${username}/orgs`);
+    this.setState({
+      // eslint-disable-next-line react/no-unused-state
+      data,
+      loading: false,
+      refreshing: false,
+    });
+  };
+
+  renderlist = () => {
+    const { data, refreshing } = this.state;
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadingOrganizations}
+        refreshing={refreshing}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <View style={styles.container}>
+        <Header title="Organizações" />
+        {loading ? <ActivityIndicator styles={styles.loading} /> : this.renderList}
+      </View>
+    );
+  }
+}
