2fba1c4d86f08bdd878aae7ccf76c1adb02b6d0f
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-17 15:01:59 -0300

    config repoName

--- javascript/rocketseat/goNative/challenge02/src/pages/Repositories/index.js
+++ javascript/rocketseat/goNative/challenge02/src/pages/Repositories/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import styles from "./styles";
 import Header from "../../components/Header";
 import RepositoryCard from "./RepositoryCard";
 import Icon from "react-native-vector-icons/FontAwesome";
+import api from "../../services/api";
 
 export class Repositories extends Component {
+  state = {
+    repositoryName: "",
+    data: []
+  };
+
+  getRepositories = async () => {
+    const { repositoryName } = this.state;
+    const response = await api.get(`/${repositoryName}`);
+    this.setState({ data: response.data });
+  };
+
   render() {
+    const { repositoryName } = this.state;
     return (
       <View style={styles.container}>
         <Header title="Repositories" />
@@ -14,17 +27,23 @@ export class Repositories extends Component {
           <TextInput
             style={styles.input}
             placeholder="Informe um repositorio"
+            autoCapitalize="none"
+            autoCorrect={false}
+            value={repositoryName}
+            onChangeText={repository =>
+              this.setState({ repositoryName: repository })
+            }
           />
-          <Icon
-            name="search"
-            size={20}
-            style={{ color: "#000", marginTop: 10 }}
-          />
+          <TouchableOpacity onPress={this.getRepositories}>
+            <Icon
+              name="search"
+              size={20}
+              style={{ color: "#000", marginTop: 10 }}
+            />
+          </TouchableOpacity>
         </View>
         <View style={styles.line} />
 
-        <RepositoryCard />
-        <RepositoryCard />
         <RepositoryCard />
       </View>
     );
