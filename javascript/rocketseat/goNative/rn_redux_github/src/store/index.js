8d791d0568a35aac0d9eea416fe59ed46805612b
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-28 22:47:19 -0300

    configure generally reducer

--- javascript/rocketseat/goNative/rn_redux_github/src/store/index.js
+++ javascript/rocketseat/goNative/rn_redux_github/src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux";
+import reducer from "./reducers/login";
 const createAppripriateStore = __DEV__ ? console.tron.createStore : createStore;
 
-const store = createStore(createAppripriateStore());
+const store = createStore(createAppripriateStore(reducer));
 
 export default store;
