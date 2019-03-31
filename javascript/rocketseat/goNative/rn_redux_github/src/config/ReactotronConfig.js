8a905d2a0f82c22ca58b48b686d92b72dbe611d0
Emerson Silva <emerson_jdss@hotmail.com>
2019-03-31 18:34:31 -0300

    configure combineReducers

--- javascript/rocketseat/goNative/rn_redux_github/src/config/ReactotronConfig.js
+++ javascript/rocketseat/goNative/rn_redux_github/src/config/ReactotronConfig.js
@@ -1,5 +1,11 @@
 import Reactotron from "reactotron-react-native";
 
-Reactotron.configure() // controls connection & communication settings
-  .useReactNative() // add all built-in react native plugins
-  .connect(); // let's connect!
+if (__DEV__) {
+  const tron = Reactotron.configure()
+    .useReactNative()
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
--- javascript/rocketseat/goNative/rn_redux_github/src/store/index.js
+++ javascript/rocketseat/goNative/rn_redux_github/src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
-import reducer from "./reducers/login";
 
-const store = createStore(reducer);
+import reducers from "./reducers";
 
+// const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore;
+const store = createStore(reducers);
+
+console.tron.log(store.getState());
 export default store;
