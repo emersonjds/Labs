1b397ab8748c69ad95ba76a28a9d447b6d633cff
Emerson Silva <emerson_jdss@hotmail.com>
2019-04-04 01:20:33 -0300

    starter saga

--- javascript/rocketseat/goNative/rn_redux_github/src/store/index.js
+++ javascript/rocketseat/goNative/rn_redux_github/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore } from "redux";
-import createSagaMi from "redux-saga";
+import createSagaMiddleware from "redux-saga";
 
-import reducers from "./reducers";
+import rootReducers from "./reducers";
+
+const sagaMiddleware = createSagaMiddleware();
 
 // const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore;
-const store = createStore(reducers);
+const store = createStore(rootReducers, sagaMiddleware);
+
+sagaMiddleware.run();
 
 export default store;
--- javascript/rocketseat/goNative/rn_redux_github/src/store/sagas/index.js
+++ javascript/rocketseat/goNative/rn_redux_github/src/store/sagas/index.js
@@ -1,4 +1,12 @@
-import {} from "redux-saga/effects";
+import { all, takeLatest, put, call } from "redux-saga/effects";
+import api from "../../services/api";
+import * as loginActions from "../actions/login";
+
+// put call action
+// call emmiter of request
+// takeLatest take only call
+
+export function* login(action) {}
 
 export default function* rootSaga() {
   return yield all([]);
