a6b3666f9f896ae74b9e96cb9e9041e9ae4fcef6
Emerson Silva <emerson_jdss@hotmail.com>
2019-04-21 00:34:00 -0300

    config container in Repositories View

--- javascript/rocketseat/goNative/rn_redux_github/src/pages/Repositories/index.js
+++ javascript/rocketseat/goNative/rn_redux_github/src/pages/Repositories/index.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 
 import { View, Text } from "react-native";
+import { Container } from "./styles";
 export default class Repositories extends Component {
   state = {};
   render() {
-    return (
-      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
-        <Text>Repositories</Text>
-      </View>
-    );
+    return <Container />;
   }
 }
