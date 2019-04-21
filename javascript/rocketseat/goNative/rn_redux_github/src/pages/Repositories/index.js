89d9092459ca02920d743028775f206130537a42
Emerson Silva <emerson_jdss@hotmail.com>
2019-04-21 00:34:43 -0300

    changes in text

--- javascript/rocketseat/goNative/rn_redux_github/src/pages/Repositories/index.js
+++ javascript/rocketseat/goNative/rn_redux_github/src/pages/Repositories/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 import { Container } from "./styles";
 export default class Repositories extends Component {
   state = {};
   render() {
-    return <Container />;
+    return (
+      <Container>
+        <Text>Repositories</Text>
+      </Container>
+    );
   }
 }
