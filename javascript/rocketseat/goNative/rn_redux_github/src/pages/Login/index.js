3c5efc7df4b0e329c0e0cb170ce0f371df0c9ecf
Emerson Silva <emerson_jdss@hotmail.com>
2019-04-25 23:42:11 -0300

    changes on project

--- javascript/rocketseat/goNative/rn_redux_github/src/pages/Login/index.js
+++ javascript/rocketseat/goNative/rn_redux_github/src/pages/Login/index.js
@@ -39,6 +39,7 @@ class Login extends Component {
             <TextButton>Entrar</TextButton>
           )}
         </Button>
+        <Text>Changes on project</Text>
       </Container>
     );
   }
