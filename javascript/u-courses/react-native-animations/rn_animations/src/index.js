99a7caf213fc85d3d5ada2a85e9f42eef38bb58b
Emerson Silva <emerson_jdss@hotmail.com>
2019-05-01 23:21:43 -0300

    removed bugfix line

--- javascript/u-courses/react-native-animations/rn_animations/src/index.js
+++ javascript/u-courses/react-native-animations/rn_animations/src/index.js
@@ -14,7 +14,7 @@ export default class App extends Component {
         {/* <Opacity /> */}
         {/* <Translate /> */}
         <ScaleAnimation />
-        <WidthAndHeightAnimations />
+        {/* <WidthAndHeightAnimations /> */}
       </View>
     );
   }
