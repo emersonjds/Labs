b960b08daf1d8bc6fab5089fd9eaf73a22e847c1
Emerson Silva <emerson_jdss@hotmail.com>
2019-05-07 23:03:33 -0300

    config Interpolation

--- javascript/u-courses/react-native-animations/rn_animations/src/Interpolation/index.js
+++ javascript/u-courses/react-native-animations/rn_animations/src/Interpolation/index.js
@@ -6,30 +6,51 @@ export class InterPolation extends Component {
     animation: new Animated.Value(1)
   };
 
+  componentDidMount() {
+    Animated.timing(this.state.animation, {
+      toValue: 2,
+      duration: 1000
+    }).start();
+  }
+
   render() {
+    const yInterpolate = this.state.animation.interpolate({
+      inputRange: [1, 2],
+      outputRange: [0, -25]
+    });
+
+    const boxStyle = {
+      transform: [
+        { scaleY: this.state.animation },
+        { translateY: yInterpolate }
+      ]
+    };
+
     return (
       <View style={styles.container}>
-        <Text> textInComponent </Text>
+        <View style={styles.box2} />
+        <Animated.View style={[styles.box, boxStyle]} />
+        {/* </View> */}
       </View>
     );
   }
 }
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
   },
   box: {
-    height: 100,
     width: 100,
+    height: 100,
     backgroundColor: "tomato"
   },
-  box1: {
-    height: 100,
+  box2: {
     width: 100,
-    backgroundColor: "green"
+    height: 100,
+    backgroundColor: "blue"
   }
 });
 
