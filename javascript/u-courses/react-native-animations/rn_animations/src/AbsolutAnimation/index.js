78e352448d1dca0042a6d5993f53e7403e60d24a
Emerson Silva <emerson_jdss@hotmail.com>
2019-05-08 19:44:27 -0300

    absolute position Animation

--- javascript/u-courses/react-native-animations/rn_animations/src/AbsolutAnimation/index.js
+++ javascript/u-courses/react-native-animations/rn_animations/src/AbsolutAnimation/index.js
@@ -1,49 +1,77 @@
 import React, { Component } from "react";
-import { View, Animated, Dimensions, StyleSheet } from "react-native";
+import {
+  View,
+  Animated,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity
+} from "react-native";
 
 export class AbsolutAnimation extends Component {
   state = {
     animation: new Animated.Value(100)
   };
 
-  componentDidMount() {
+  startAnimation = () => {
     Animated.timing(this.state.animation, {
-      toValue: 400,
+      toValue: 700,
       duration: 1000
     }).start();
-  }
+  };
+
+  finishAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 100,
+      duration: 1000
+    }).start();
+  };
 
   render() {
-    const boxStyles = {
+    const boxStyle = {
       height: this.state.animation
     };
 
     return (
       <View style={styles.container}>
-        <View style={[styles.box, boxStyles]} />
+        <Animated.View style={[styles.box, boxStyle]}>
+          <TouchableOpacity
+            onPress={this.startAnimation}
+            style={styles.bottom}
+          />
+          <TouchableOpacity
+            onPress={this.finishAnimation}
+            style={styles.bottom1}
+          />
+        </Animated.View>
       </View>
     );
   }
 }
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
   },
   box: {
+    // height: 100,
     position: "absolute",
-    bottom: 0,
-    height: 130,
-    backgroundColor: "tomato",
     width: Dimensions.get("window").width,
-    borderRadius: 20
+    bottom: 0,
+    backgroundColor: "tomato",
+    borderTopRadius: 20
   },
   bottom: {
     height: 50,
     width: 120,
     backgroundColor: "blue"
+  },
+  bottom1: {
+    height: 50,
+    width: 120,
+    backgroundColor: "red"
   }
 });
 
