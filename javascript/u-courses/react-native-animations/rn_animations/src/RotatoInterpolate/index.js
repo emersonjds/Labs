27a5fa22ff924402a8201068f743efbf10188a81
Emerson Silva <emerson_jdss@hotmail.com>
2019-05-09 21:19:28 -0300

    config rotates normal, X and Y

--- javascript/u-courses/react-native-animations/rn_animations/src/RotatoInterpolate/index.js
+++ javascript/u-courses/react-native-animations/rn_animations/src/RotatoInterpolate/index.js
@@ -7,11 +7,34 @@ import {
 } from "react-native";
 
 export class RotatoInterpolate extends Component {
+  state = {
+    animation: new Animated.Value(0)
+  };
+
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: 360,
+      duration: 1000
+    }).start();
+  };
+
   render() {
+    const rotateInterpolate = this.state.animation.interpolate({
+      inputRange: [0, 360],
+      outputRange: ["0deg", "1080deg"]
+    });
+
+    const animatedStyles = {
+      transform: [
+        {
+          rotate: rotateInterpolate
+        }
+      ]
+    };
     return (
       <View style={styles.container}>
-        <TouchableWithoutFeedback>
-          <Animated.View style={styles.box}>
+        <TouchableWithoutFeedback onPress={this.startAnimation}>
+          <Animated.View style={[styles.box, animatedStyles]}>
             <Animated.Text>Animation Text</Animated.Text>
           </Animated.View>
         </TouchableWithoutFeedback>
