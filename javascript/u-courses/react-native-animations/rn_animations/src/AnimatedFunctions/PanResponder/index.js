6e7dc8ac6296829944bc8274b357f01b4c313ecb
Emerson Silva <emerson_jdss@hotmail.com>
2019-05-19 23:40:54 -0300

    config PanResponder

---
+++ javascript/u-courses/react-native-animations/rn_animations/src/AnimatedFunctions/PanResponder/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  Animated,
+  PanResponder,
+  TouchableWithoutFeedback
+} from "react-native";
+
+export default class PanResponder extends Component {
+  state = {
+    animatiom: new Animated.Value(0)
+  };
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
