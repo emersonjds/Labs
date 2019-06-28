a42e08599bbf7d2f609551322f2b1cca03b9f7c8
Emerson Silva <emerson_jdss@hotmail.com>
2019-06-28 15:27:10 -0300

    changes in all projects

--- javascript/rocketseat/goNative/rn_redux/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import todos from "./reducers/todos";
-
-const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore;
-const store = createStore(createAppropriateStore(todos));
-
-export default store;
