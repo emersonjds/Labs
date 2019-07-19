ba25bba69d352452e1a8f48a2b742e2f42c2a684
Emerson Silva <emerson_jdss@hotmail.com>
2019-07-18 23:06:23 -0300

    config editor config and Actions

--- javascript/rocketseat/goNative/rn_redux/src/TodoList.js
+++ javascript/rocketseat/goNative/rn_redux/src/TodoList.js
@@ -7,11 +7,19 @@ import { connect } from 'react-redux';
 const TodoList = ({ todos, dispatch }) => (
   // eslint-disable-next-line react/jsx-filename-extension
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    {
-      // eslint-disable-next-line react/prop-types
-      todos.map(todo => <Text key={todo}>{todo}</Text>)
-    }
-    <Button title="Adicionar Todo" onPress={() => dispatch({ type: 'ADD_TODO', text: 'Novo Todo' })} />
+    {// eslint-disable-next-line react/prop-types
+    todos.map(todo => (
+      <Text
+        style={{ textDecorationLine: todo.completed ? 'line-through' : 'none' }}
+        key={todo.id}
+      >
+        {todo.text}
+      </Text>
+    ))}
+    <Button
+      title="Adicionar Todo"
+      onPress={() => dispatch({ type: 'ADD_TODO', text: 'Novo Todo' })}
+    />
   </View>
 );
 
--- javascript/rocketseat/goNative/rn_redux/src/store/index.js
+++ javascript/rocketseat/goNative/rn_redux/src/store/index.js
@@ -1,12 +1,24 @@
 import { createStore } from 'redux';
 
-const INITIAL_STATE = ['Fazer cafe', 'react-native'];
+const INITIAL_STATE = [
+  { id: 1, text: 'Fazer cafe', completed: false },
+  { id: 2, text: 'react-native', completed: false },
+  { id: 3, text: 'React', completed: true },
+];
 
 function reducer(state = INITIAL_STATE, action) {
-  if (action.type === 'ADD_TODO') {
-    return [...state, action.text];
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, {
+        id: Math.random(),
+        text: action.text,
+        completed: false,
+      }];
+    case 'MARK_AS_COMPLETED':
+      return state;
+    default:
+      return state;
   }
-  return state;
 }
 
 const store = createStore(reducer);
