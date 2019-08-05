76aa5becfb4674838e9814b3a2055996e99f47e8
Emerson Silva <emerson_jdss@hotmail.com>
2019-08-05 09:38:51 -0300

    config saga at reactotron

--- javascript/rocketseat/goNative/rn_redux/src/config/ReactotronConfig.js
+++ javascript/rocketseat/goNative/rn_redux/src/config/ReactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 // eslint-disable-next-line no-undef
 if (__DEV__) {
   const tron = Reactotron.configure()
     .useReactNative()
     .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect();
 
   tron.clear();
