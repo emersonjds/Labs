3614e81d3222adec27c342eb9171fb0c40e92551
Emerson Silva <emerson_jdss@hotmail.com>
2019-08-17 23:15:39 -0300

    config new project to studies with redux

--- javascript/rocketseat/goNative/rn_redux/src/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import login from './login';
-import repositories from './repositories';
-
-export default combineReducers({
-  login,
-  repositories,
-});
