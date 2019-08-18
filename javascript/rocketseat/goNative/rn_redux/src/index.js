e067a9ea86699d3e93731b3638cf1105d9655b97
Emerson Silva <emerson_jdss@hotmail.com>
2019-08-17 23:19:11 -0300

    config initial template

---
+++ javascript/rocketseat/goNative/rn_redux/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Stater Project</Text>
+    </View>
+  );
+};
+
+export default App;
