1d720731d8654a0e2092152f1365cfa628a46d2f
Emerson Silva <emerson_jdss@hotmail.com>
2019-08-28 16:06:10 -0300

    config api requests

--- javascript/rocketseat/goNative/rn_first_project/src/pages/Main/index.js
+++ javascript/rocketseat/goNative/rn_first_project/src/pages/Main/index.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Container, Form, Input, SubmitButton } from './styles';
+import api from '../../services/api';
 
 Icon.loadFont();
 
 export default class Main extends Component {
-  static navigationOptions = {
-    title: 'Usuarios',
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
   };
 
   render() {
+    const { users, newUser } = this.state;
+
     return (
       <Container>
         <Form>
-          <Input autoCapitalize="none" autoCorrect={false} />
-          <SubmitButton>
+          <Input
+            autoCapitalize="none"
+            autoCorrect={false}
+            value={newUser}
+            onChangeText={text =>
+              this.setState({
+                newUser: text,
+              })
+            }
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
             <Icon name="add" size={20} color="#eee" />
           </SubmitButton>
         </Form>
