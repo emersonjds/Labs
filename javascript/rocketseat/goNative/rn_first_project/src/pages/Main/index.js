d909f795599fdc080ede7bbaaab7aba965fc3bd5
Emerson Silva <emerson_jdss@hotmail.com>
2019-08-29 12:44:19 -0300

    config app loaoding

--- javascript/rocketseat/goNative/rn_first_project/src/pages/Main/index.js
+++ javascript/rocketseat/goNative/rn_first_project/src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Keyboard } from 'react-native';
+import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   Container,
@@ -24,11 +24,15 @@ export default class Main extends Component {
     this.state = {
       newUser: '',
       users: [],
+      loading: false,
     };
   }
 
   handleAddUser = async () => {
     const { users, newUser } = this.state;
+    this.setState({
+      loading: true,
+    });
     const response = await api.get(`/users/${newUser}`);
 
     const data = {
@@ -41,13 +45,14 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
   render() {
-    const { users, newUser } = this.state;
+    const { users, newUser, loading } = this.state;
 
     return (
       <Container>
@@ -64,8 +69,12 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#eee" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#FFF" />
+            ) : (
+              <Icon name="add" size={20} color="#eee" />
+            )}
           </SubmitButton>
         </Form>
         <List
