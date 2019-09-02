617969e29d2c926ab006cec327cf480dc08165ab
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-02 11:44:34 -0300

    configs with prettier

--- javascript/rocketseat/redux/src/App.js
+++ javascript/rocketseat/redux/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
-import GlobalStyles from './styles/global'
-import Header from './components/Header'
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
-    <GlobalStyles />
+      <Header />
+      <GlobalStyles />
       <Routes />
     </BrowserRouter>
   );
