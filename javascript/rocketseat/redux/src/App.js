73a232d671e4f623f10f2f23cf3c245045bc34ab
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-02 11:25:59 -0300

    config global styles

--- javascript/rocketseat/redux/src/App.js
+++ javascript/rocketseat/redux/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyles from './styles/global'
 
 function App() {
   return (
     <BrowserRouter>
+    <GlobalStyles />
       <Routes />
     </BrowserRouter>
   );
