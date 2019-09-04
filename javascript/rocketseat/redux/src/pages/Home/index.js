c9308f9802ab4a6f280aa8ccfdddfebb8b87fe6f
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-04 11:57:56 -0300

    config utils forma function

--- javascript/rocketseat/redux/src/pages/Home/index.js
+++ javascript/rocketseat/redux/src/pages/Home/index.js
@@ -1,56 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
-export default function Home() {
-  return (
-    <>
-      <ProductList>
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
-            alt="tenis"
-          />
-          <strong>Tenis</strong>
-          <span>R$ 129,90</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" />
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+    };
+  }
 
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
-            alt="tenis"
-          />
-          <strong>Tenis</strong>
-          <span>R$ 129,90</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" />
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
+  async componentDidMount() {
+    const response = await api.get('products');
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }))
 
-        <li>
-          <img
-            src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
-            alt="tenis"
-          />
-          <strong>Tenis</strong>
-          <span>R$ 129,90</span>
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" />
-            </div>
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-      </ProductList>
-    </>
-  );
+    this.setState({
+      products: data,
+    });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <>
+        <ProductList>
+          {products.map(product => (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>
+              <button type="button">
+                <div>
+                  <MdAddShoppingCart size={16} color="#FFF" />
+                </div>
+                <span>ADICIONAR AO CARRINHO</span>
+              </button>
+            </li>
+          ))}
+        </ProductList>
+      </>
+    );
+  }
 }
