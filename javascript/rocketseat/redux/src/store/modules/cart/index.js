12ba678487fb44f2ca125f2dfb34d295cfd5453a
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-05 16:09:21 -0300

    solved bugfix reducer action name

--- javascript/rocketseat/redux/src/store/modules/cart/index.js
+++ javascript/rocketseat/redux/src/store/modules/cart/index.js
@@ -3,7 +3,7 @@ const INITIAL_STATE = [];
 export default function cart(state = INITIAL_STATE, action) {
   console.log(action)
   switch(action.type) {
-    case 'ADD_TO_CARD':
+    case 'ADD_TO_CART':
       return [...state, action.payload.product]
     default:
       return state;
