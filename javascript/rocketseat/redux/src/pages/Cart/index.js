400e53bb9cc3756db66c80253fdaa72aee9b7cd3
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-07 19:04:53 -0300

    transform cart in a Class

--- javascript/rocketseat/redux/src/pages/Cart/index.js
+++ javascript/rocketseat/redux/src/pages/Cart/index.js
@@ -1,65 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   MdAddCircleOutline,
   MdRemoveCircleOutline,
   MdDelete,
 } from 'react-icons/md';
 import { Container, ProductTable, Total } from './styles';
+import { connect } from 'react-redux';
 
-export default function Cart() {
-  return (
-    <Container>
-      <ProductTable>
-        <thead>
-          <tr>
-            <th />
-            <th>PRODUTO</th>
-            <th>QTDE</th>
-            <th>SUBTOTAL</th>
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <img
-                src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
-                alt="tenis"
-              />
-            </td>
-            <td>
-              <strong>Tenis</strong>
-              <span>R$129,90</span>
-            </td>
-            <td>
-              <div>
+class Cart extends Component {
+  constructor() {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+  render() {
+    return (
+      <Container>
+        <ProductTable>
+          <thead>
+            <tr>
+              <th />
+              <th>PRODUTO</th>
+              <th>QTDE</th>
+              <th>SUBTOTAL</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img
+                  src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_detalhe2.jpg?ims=326x"
+                  alt="tenis"
+                />
+              </td>
+              <td>
+                <strong>Tenis</strong>
+                <span>R$129,90</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  </button>
+                  <input type="number" readOnly value={1} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="#7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$ 258,80</strong>
+              </td>
+              <td>
                 <button type="button">
-                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                  <MdDelete size="20" ccolor="#7159c1" />
                 </button>
-                <input type="number" readOnly value={1} />
-                <button type="button">
-                  <MdAddCircleOutline size={20} color="#7159c1" />
-                </button>
-              </div>
-            </td>
-            <td>
-              <strong>R$ 258,80</strong>
-            </td>
-            <td>
-              <button type="button">
-                <MdDelete size="20" ccolor="#7159c1" />
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </ProductTable>
-      <footer>
-        <button type="button">Finalizar Pedido</button>
-        <Total>
-          <span>TOTAL</span>
-          <strong>R$ 129,90</strong>
-        </Total>
-      </footer>
-    </Container>
-  );
+              </td>
+            </tr>
+          </tbody>
+        </ProductTable>
+        <footer>
+          <button type="button">Finalizar Pedido</button>
+          <Total>
+            <span>TOTAL</span>
+            <strong>R$ 129,90</strong>
+          </Total>
+        </footer>
+      </Container>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  data: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
