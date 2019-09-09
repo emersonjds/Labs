6ca0d5a40ddad108bbb87ddfb2cecf0ab755b3d0
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-09 06:29:53 -0300

    initial config

---
+++ javascript/egghead/real_world_rn_animations/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text> Texto</Text>
+    </View>
+  );
+};
+
+export default App;
