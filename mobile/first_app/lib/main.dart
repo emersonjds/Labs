bfe67d0a546eb77d10b42bc1fa084007d54a4dab
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-14 21:44:47 -0300

    config icons material

--- mobile/first_app/lib/main.dart
+++ mobile/first_app/lib/main.dart
@@ -15,18 +15,53 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatefulWidget {                     //modified
-  @override                                                        //new
-  State createState() => new ChatScreenState();                    //new
-} 
+class ChatScreen extends StatefulWidget {
+  //modified
+  @override //new
+  State createState() => new ChatScreenState(); //new
+}
 // Add the ChatScreenState class definition in main.dart.
 
-class ChatScreenState extends State<ChatScreen> {                  //new
-  @override                                                        //new
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+  //new
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          children: <Widget>[
+            //new
+            new Flexible(
+              //new
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
+            ), //new
+            new Container(
+              //new
+              margin: new EdgeInsets.symmetric(horizontal: 4.0), //new
+              child: new IconButton(
+                  //new
+                  icon: new Icon(Icons.send), //new
+                  onPressed: () =>
+                      _handleSubmitted(_textController.text)), //new
+            ),
+          ],
+        ));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Friendlychat")),
-
+      body: _buildTextComposer(), //new
     );
   }
 }
