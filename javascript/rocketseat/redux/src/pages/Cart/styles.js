170bd88c2d59c4e47a1b9bf18fdffa358e9ac2ec
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-20 19:24:21 -0300

    changes in styles

--- javascript/rocketseat/redux/src/pages/Cart/styles.js
+++ javascript/rocketseat/redux/src/pages/Cart/styles.js
@@ -1,81 +1,80 @@
 import styled from 'styled-components';
-import {darken} from 'polished';
+import { darken } from 'polished';
 
 export const Container = styled.div`
-padding: 30px;
-background: #FFF;
-border-radius:4px;
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
 
-footer {
-  margin-top: 30px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-  button {
-    background: #7159c1;
-    color: #FFF;
-    border: 0;
-    border-radius: 4px;
-    padding: 12px 20px;
-    font-weight: bold;
-    text-transform: uppercase;
+    button {
+      background: #7159c1;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
 
-    &:hover {
-      background: ${darken(0.03, '#7159c1')};
+      &:hover {
+        background: ${darken(0.03, '#7159c1')};
+      }
     }
   }
-}
 `;
 
 export const ProductTable = styled.table`
-width: 100%;
+  width: 100%;
 
-thead th {
-  color: #999;
-  text-align: left;
-  padding: 12px;
-
-}
-
-tbody td {
-  padding: 12px;
-  border-bottom: 1px solid #eee;
-}
-
-img {
-  height: 100px;
-}
-
-strong {
-  color: #333;
-  display: block;
-}
-
-span {
-  display: block;
-  margin-top: 5px;
-  font-size: 18px;
-  font-weight: bold;
-}
-
-div {
-  display: flex;
-  align-items: center;
-
-  input {
-    border: 1px solid #ddd;
-    border-radius: 4px;
-    color: #666;
-    width: 50px;
+  thead th {
+    color: #999;
+    text-align: left;
+    padding: 12px;
   }
-}
 
-button {
-  background: none;
-  border: 0;
-  padding: 6px;
-}
+  tbody td {
+    padding: 12px;
+    border-bottom: 1px solid #eee;
+  }
+
+  img {
+    height: 100px;
+  }
+
+  strong {
+    color: #333;
+    display: block;
+  }
+
+  span {
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      width: 50px;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
 `;
 
 export const Total = styled.div`
