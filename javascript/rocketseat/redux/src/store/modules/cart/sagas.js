215e8072778c2bdeda40514319147fc2936fa580
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-28 16:24:59 -0300

    wrapper amount in saga

--- javascript/rocketseat/redux/src/store/modules/cart/sagas.js
+++ javascript/rocketseat/redux/src/store/modules/cart/sagas.js
@@ -1,11 +1,25 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addToCartSuccess } from './actions';
+import { call, put, all, takeLatest, select } from 'redux-saga/effects';
+import { addToCartSuccess, updateAmount } from './actions';
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
-  const response = yield call(api.get, `/products/${id}`);
+  const productExists = yield select(state => {
+    state.cart.find(p => p.id === id);
+  });
 
-  yield put(addToCartSuccess(response.data));
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
