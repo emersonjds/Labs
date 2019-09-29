b1765dc01431e1705d500ed790ff42bf9f30a41e
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-29 15:02:05 -0300

    understanding useEffects

--- javascript/rocketseat/react_hoocks/src/App.js
+++ javascript/rocketseat/react_hoocks/src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [names, setName] = useState(['Emerson', 'Maria', 'Jose']);
+  const [names, setName] = useState([]);
   const [newName, setNewName] = useState('');
 
   const addName = () => {
     setName([...names, newName]);
     setNewName('');
   };
+
+  useEffect(() => {
+    const dataNames = localStorage.getItem('names');
+    if (dataNames) {
+      setName(JSON.parse(dataNames));
+    }
+  }, []);
+
+  //componentDidUpdate, execute all times when change value on names
+  useEffect(() => {
+    localStorage.setItem('names', JSON.stringify(names));
+  }, [names]);
 
   return (
     <>
