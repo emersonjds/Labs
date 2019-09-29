cc83caa582302f7bb96710eb9f6a4fbb5a118607
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-29 13:39:37 -0300

    config rules with hooks

--- javascript/rocketseat/react_hoocks/.eslintrc.js
+++ javascript/rocketseat/react_hoocks/.eslintrc.js
@@ -1,20 +1,38 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: ['airbnb'],
+  extends: ["airbnb"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: ['react', 'prettier', 'react-hooks'],
-  rules: {},
+  plugins: ["react", "prettier", "react-hooks"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx/filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", ".js"]
+      }
+    ],
+    "import/prefer-default-export": "off",
+    "no-param-reassign": "off",
+    "no-console": [
+      "error",
+      {
+        allow: ["tron"]
+      }
+    ],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
 };
