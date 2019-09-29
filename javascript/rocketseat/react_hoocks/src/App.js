9b60d9705b344197b8dd0393747a6947569a8e72
Emerson Silva <emerson_jdss@hotmail.com>
2019-09-29 13:57:24 -0300

    understanding useState

--- javascript/rocketseat/react_hoocks/src/App.js
+++ javascript/rocketseat/react_hoocks/src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from 'react'
 
 function App() {
+  const [names, setName] = useState(['Emerson', 'Maria', 'Jose'])
+  const [newName, setNewName] = useState('')
+
+  const addName = () => {
+    setName([...names, newName])
+    setNewName('')
+  }
+
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <>
-      <h1>Hello World</h1>
+      <ul>
+        {names.map(name => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <input value={newName} onChange={e => setNewName(e.target.value)} />
+      <button type="button" onClick={addName}>
+        Adicionar novo nome
+      </button>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
