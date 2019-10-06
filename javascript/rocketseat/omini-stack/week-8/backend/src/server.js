13286a402eda99acba104c3992e877b83df2ce63
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-06 12:04:31 -0300

    understanding query params

---
+++ javascript/rocketseat/omini-stack/week-8/backend/src/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  return res.json({ message: `Hello ${req.query.name}` });
+});
+
+server.listen(3333);
