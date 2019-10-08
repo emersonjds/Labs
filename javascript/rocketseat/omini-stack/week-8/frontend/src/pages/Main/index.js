d7860a08f08c0417949b15d0f0c3f99c4781e644
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-08 14:03:17 -0300

    construct visual web

--- javascript/rocketseat/omini-stack/week-8/frontend/src/pages/Main/index.js
+++ javascript/rocketseat/omini-stack/week-8/frontend/src/pages/Main/index.js
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
 
 // import { Container } from './styles';
+import "./styles.css";
 
-export default function Main() {
+export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id
+        }
+      });
+
+      setUsers(response.data);
+    }
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    await api.post(`devs/${id}/likes`, null, {
+      headers: {
+        user: match.params.id
+      }
+    });
+    setUsers(users.filter(user => user._id !== id));
+  }
+  async function handleDesLike(id) {
+    await api.post(`devs/${id}/deslikes`, null, {
+      headers: {
+        user: match.params.id
+      }
+    });
+    setUsers(users.filter(user => user._id !== id));
+  }
+
   return (
-    <div>
-      <h1>Main</h1>
+    <div className="main-container">
+      {users.length ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name} />
+              <footer>
+                <strong>{user.name}}</strong>
+                <p>{user.bio}</p>
+              </footer>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDesLike(user._id)}>
+                  Core
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  Core
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">Acabou :( </div>
+      )}
     </div>
   );
 }
