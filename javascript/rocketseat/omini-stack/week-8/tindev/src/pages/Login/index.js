2e9fab97c6f86d089b78d5b811446d981a0382f9
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-09 14:10:11 -0300

    config async manipulate to verify user on application

--- javascript/rocketseat/omini-stack/week-8/tindev/src/pages/Login/index.js
+++ javascript/rocketseat/omini-stack/week-8/tindev/src/pages/Login/index.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Container, Logo, UserInput, ButtonLogin, TextButton} from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
 import {Platform} from 'react-native';
 import logo from '../../assets/img/log.png';
 import api from '../../services/api';
@@ -7,12 +8,21 @@ import api from '../../services/api';
 export default function Login({navigation}) {
   const [user, setUser] = useState('');
 
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', {user});
+      }
+    });
+  }, [navigation]);
+
   async function handleLogin() {
     const response = await api.post('/devs', {
       username: user,
     });
     const {_id} = response.data;
     console.log(_id);
+    await AsyncStorage.setItem('user', _id);
     navigation.navigate('Main', {_id});
   }
 
