c295ba20b4499be5a0cd874352ebf9e8319a07e8
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-11 20:29:55 -0300

    config socket.io

--- javascript/rocketseat/omini-stack/week-8/backend/src/server.js
+++ javascript/rocketseat/omini-stack/week-8/backend/src/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const moongose = require("mongoose");
+
 const routes = require("./routes");
 const cors = require("cors");
 
-const server = express();
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server); //configuracao de socket io
+
+io.on("connection", socket => {
+  console.log("nova conexao", socket.id);
+});
 
 moongose.connect(
   "mongodb+srv://oministack8:oministack8@omstack8-awcfj.mongodb.net/oministack8?retryWrites=true&w=majority",
@@ -12,8 +19,8 @@ moongose.connect(
     useUnifiedTopology: true
   }
 );
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
