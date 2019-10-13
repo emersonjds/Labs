749298a2028da88b0b295158cc2f60ba91b55dd6
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-13 13:09:50 -0300

    add cors to access anywhere domains

--- javascript/rocketseat/starter/nodejs/server.js
+++ javascript/rocketseat/starter/nodejs/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const moongose = require("mongoose");
 const requireDir = require("require-dir");
 
 //iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //iniciando o DB
 moongose.connect("mongodb://localhost:27017/nodeapi", {
