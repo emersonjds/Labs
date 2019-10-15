e0c519a338ed2a21ca5ca515f2c1c961fb655fb4
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-14 23:58:30 -0300

    config changes

--- javascript/rocketseat/react-animations/rn_animations/src/index.js
+++ javascript/rocketseat/react-animations/rn_animations/src/index.js
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF"
+    backgroundColor: "orange"
   },
   ball: {
     height: 70,
