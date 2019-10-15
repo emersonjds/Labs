5acc577b612c968469e8745779865e2fadda90c4
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-15 16:09:24 -0300

    remove old projects

--- javascript/angular2 - @Alura/client/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Http } from '@angular/http';
-
-@Component({
-    moduleId: module.id,
-    selector: 'app', 
-    templateUrl: './app.component.html'
-})
-export class AppComponent { }
--- javascript/angular2 - @Alura/client/app/botao/botao.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { BotaoComponent } from './botao.component';
-@NgModule({
-    declarations: [ BotaoComponent ],
-    exports: [ BotaoComponent ]
-})
-export class BotaoModule { }
