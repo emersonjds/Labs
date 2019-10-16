429693db615253e3275583baca544a135f446e84
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-15 21:56:37 -0300

    config global loop in the table

--- javascript/alura/introducao-javascript/js/main.js
+++ javascript/alura/introducao-javascript/js/main.js
@@ -1,33 +1,37 @@
 var title = document.querySelector(".titulo");
 title.textContent = "Maximo Nutriçäo";
 
-var paciente = document.querySelector(".paciente"); //return an array os patients
+var pacientes = document.querySelectorAll(".paciente"); //return an array os patients
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var tdAltura = paciente.querySelector(".info-altura");
 
-var tdIMC = paciente.querySelector(".info-imc");
+  var tdIMC = paciente.querySelector(".info-imc");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
 
-var pesoValido = true;
-var alturaValida = true;
+  var pesoValido = true;
+  var alturaValida = true;
 
-if (peso < 0 || peso > 1000) {
-  console.log("peso invalido");
-  pesoValido = false;
-  tdIMC.textContent = "Peso invalido";
-}
+  if (peso <= 0 || peso >= 1000) {
+    console.log("peso invalido");
+    pesoValido = false;
+    tdIMC.textContent = "Peso invalido";
+  }
 
-if (altura < 0 || altura > 3.0) {
-  console.log("altura invalida");
-  alturaValida = false;
-  tdIMC.textContent = "Altura Invalida";
-}
+  if (altura <= 0 || altura >= 3.0) {
+    console.log("altura invalida");
+    alturaValida = false;
+    tdIMC.textContent = "Altura Invalida";
+  }
 
-if (pesoValido && alturaValida) {
-  var imc = peso / (altura * altura);
-  tdIMC.textContent = imc;
+  if (pesoValido && alturaValida) {
+    var imc = peso / (altura * altura);
+    tdIMC.textContent = imc.toFixed;
+  } else {
+    tdIMC.textContent = "Altura e ou peso invalidos";
+  }
 }
