62695ceb881907d74c244c4dc63428dd28a07f3e
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-26 14:46:15 -0300

    animations with css and setTimeout

--- javascript/alura/introducao-javascript/js/removePatient.js
+++ javascript/alura/introducao-javascript/js/removePatient.js
@@ -3,9 +3,13 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", event => {
-  target = event.target;
-  fatherNode = target.parentNode;
-  fatherNode.remove();
+  // target = event.target;
+  // fatherNode = target.parentNode;
+  // fatherNode.remove();
+  event.target.parentNode.classList.add(".fadeOut");
+  setTimeout(() => {
+    event.target.parentNode.remove();
+  }, 500);
 });
 
 // pacientes.forEach(paciente => {
