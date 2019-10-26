2c87ff796e1201838f2c743accbce50369b0bd0d
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-26 17:47:42 -0300

    XMLHttpRequest

--- javascript/alura/introducao-javascript/js/loadPatients.js
+++ javascript/alura/introducao-javascript/js/loadPatients.js
@@ -1,16 +1,23 @@
 var loadPatients = document.querySelector("#load-patients");
 
 loadPatients.addEventListener("click", function() {
-  console.log("Aguarde, carregando dados");
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-  xhr.addEventListener("load", function() {
-    var resposta = xhr.responseText;
-    var pacientes = JSON.parse(resposta);
 
-    pacientes.forEach(paciente => {
-      AddPatientsOnTable(paciente);
-    });
+  xhr.addEventListener("load", function() {
+    var ajaxError = document.querySelector("#ajax-error");
+    if (xhr.status === 200) {
+      ajaxError.classList.add("hideELement");
+      var resposta = xhr.responseText;
+      var pacientes = JSON.parse(resposta);
+      pacientes.forEach(paciente => {
+        AddPatientsOnTable(paciente);
+      });
+    } else {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+      ajaxError.classList.remove("hideELement");
+    }
   });
   xhr.send();
 });
