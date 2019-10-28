33723c388088180c3840e32a28f64c3ba5bbc5bb
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-27 21:01:50 -0300

    deep understanding

--- javascript/alura/react/react_pt1/src/App.js
+++ javascript/alura/react/react_pt1/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project React
         </a>
       </header>
     </div>
