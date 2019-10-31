4b412d2f87ebae45833f658e216c399287620ccc
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-30 23:46:56 -0300

    config initial template

---
+++ javascript/alura/angularjs/js/controllers/photos-controller.js
@@ -0,0 +1,7 @@
+angular.module("project").controller("PhotoController", function($scope) {
+  $scope.photo = {
+    title: "Spark",
+    url:
+      "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
+  };
+});
