943ae6d70370be1722fd869def2685cc3628410b
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-31 23:48:42 -0300

    config  core module

--- javascript/alura/angularjs/js/controllers/photos-controller.js
+++ javascript/alura/angularjs/js/controllers/photos-controller.js
@@ -1,19 +1,23 @@
-angular.module("project").controller("PhotoController", function($scope) {
-  $scope.photos = [
-    {
-      title: "Spark",
-      url:
-        "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
-    },
-    {
-      title: "Spark",
-      url:
-        "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
-    },
-    {
-      title: "Spark",
-      url:
-        "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
-    }
-  ];
-});
+// $http -> service anguarl to treatment requests
+
+angular
+  .module("project")
+  .controller("PhotoController", function($scope, $http) {
+    $scope.photos = [
+      {
+        title: "Spark",
+        url:
+          "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
+      },
+      {
+        title: "Spark",
+        url:
+          "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
+      },
+      {
+        title: "Spark",
+        url:
+          "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
+      }
+    ];
+  });
