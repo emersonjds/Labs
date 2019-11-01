40572f83423b9ac58f2fd28eaa9a8c4559d2ccc5
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-31 23:40:34 -0300

    undestand directives and forms

--- javascript/alura/angularjs/js/controllers/photos-controller.js
+++ javascript/alura/angularjs/js/controllers/photos-controller.js
@@ -1,7 +1,19 @@
 angular.module("project").controller("PhotoController", function($scope) {
-  $scope.photo = {
-    title: "Spark",
-    url:
-      "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
-  };
+  $scope.photos = [
+    {
+      title: "Spark",
+      url:
+        "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
+    },
+    {
+      title: "Spark",
+      url:
+        "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
+    },
+    {
+      title: "Spark",
+      url:
+        "https://pbs.twimg.com/profile_images/1110548476042063873/JiDaZlw4_400x400.png"
+    }
+  ];
 });
