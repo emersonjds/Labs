c63478c274d558e5bc57de16fad69190f10f53fe
Emerson Silva <emerson_jdss@hotmail.com>
2019-10-31 23:56:08 -0300

    use core functions of

--- javascript/alura/angularjs/js/controllers/photos-controller.js
+++ javascript/alura/angularjs/js/controllers/photos-controller.js
@@ -21,12 +21,20 @@ angular
     //   }
     // ];
     $scope.photos = [];
-    var promise = $http.get("v1/fotos");
-    promise
-      .then(function(response) {
-        $scope.photos = response.data;
+    // var promise = $http.get("v1/fotos");
+    // promise
+    //   .then(function(response) {
+    //     $scope.photos = response.data;
+    //   })
+    //   .catch(function(error) {
+    //     console.log(error.message);
+    //   });
+    $http
+      .get("v1/fotos")
+      .success(function(photos) {
+        $scope.photos = photos;
       })
-      .catch(function(error) {
-        console.log(error.message);
+      .error(function(erro) {
+        console.log(erro);
       });
   });
