0b01cc46b39a7b69e380fd3df65fd107f9e67892
Emerson Silva <emerson_jdss@hotmail.com>
2020-01-12 13:11:16 -0300

    conditional expressions

--- dart/dynamic.dart
+++ dart/dynamic.dart
@@ -1,4 +1,9 @@
 void main() {
+// it's possible use a dynamic type, but it's cause a lot of use of memory
+  dynamic data = 30;
+  data = 3.1415;
+  data = 'I have $data years old and pi value is $data';
+
   var age = 30;
   var pi = 3.1415;
   var text = 'I have $age years old and pi value is $pi';
