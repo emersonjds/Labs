fe0002e36d91c89465cf1f2ad0c597f2b6e62569
Emerson Silva <emerson_jdss@hotmail.com>
2020-02-09 23:57:26 -0300

    changes

--- javascript/alura/react/react_pt1/src/App.js
+++ javascript/alura/react/react_pt1/src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit Text
-        </p>
+        <p>Edit Text</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Change Project
+          Change to update
         </a>
       </header>
     </div>
