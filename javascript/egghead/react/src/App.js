02cecd8255dd6f1ce6515da7f028704d729792e2
Emerson Silva <emerson_jdss@hotmail.com>
2020-03-16 17:12:04 -0300

    tests with classes

--- javascript/egghead/react/src/App.js
+++ javascript/egghead/react/src/App.js
@@ -2,15 +2,33 @@ import React from "react";
 import "./styles.css";
 import PropTypes from "prop-types";
 
-App.propTypes = {
-  firstName: PropTypes.string.isRequired,
-  lastName: PropTypes.string.isRequired
-};
+// App.propTypes = {
+//   firstName: PropTypes.string.isRequired,
+//   lastName: PropTypes.string.isRequired
+// };
 
-export default function App(props) {
-  return (
-    <div className="App">
-      Hello {props.firstName} {props.lastName} !
-    </div>
-  );
+// export default function App(props) {
+//   return (
+//     <div className="App">
+//       Hello {props.firstName} {props.lastName} !
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  static proptTypes = {
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired
+  };
+
+  render() {
+    const { firstName, lastName } = this.props;
+    return (
+      <div className="App">
+        Hello {firstName} {lastName} !
+      </div>
+    );
+  }
 }
+
+export default App;
