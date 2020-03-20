dd22b6796bd194df1c8484956bdb9a5592213ac4
Emerson Silva <emerson_jdss@hotmail.com>
2020-03-20 17:23:36 -0300

    initial config data with mobx and webpack

--- javascript/egghead/mobx/src/index.js
+++ javascript/egghead/mobx/src/index.js
@@ -2,27 +2,14 @@ import React from "react";
 import { render } from "react-dom";
 import DevTools from "mobx-react-devtools";
 
-import TodoList from "./components/TodoList";
-import TodoListModel from "./models/TodoListModel";
-import TodoModel from "./models/TodoModel";
-
-const store = new TodoListModel();
+export default function App() {
+  return <div>Emerson</div>;
+}
 
 render(
   <div>
     <DevTools />
-    <TodoList store={store} />
+    <App />
   </div>,
   document.getElementById("root")
 );
-
-store.addTodo("Get Coffee");
-store.addTodo("Write simpler code");
-store.todos[0].finished = true;
-
-setTimeout(() => {
-  store.addTodo("Get a cookie as well");
-}, 2000);
-
-// playing around in the console
-window.store = store;
