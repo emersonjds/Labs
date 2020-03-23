a6dcb9c6101b14b9dbbfde2400e05e4a0830c1d0
Emerson Silva <emerson_jdss@hotmail.com>
2020-03-22 22:11:52 -0300

    add notes to undertanding mobx

--- javascript/egghead/mobx/src/components/Counter/index.js
+++ javascript/egghead/mobx/src/components/Counter/index.js
@@ -6,21 +6,43 @@ const appState = observable({
   count: 0
 });
 
-const increment = () => {
-  this;
+appState.increment = () => {
+  appState.count++;
+};
+
+appState.decrement = () => {
+  appState.count--;
 };
 
 @observer
 export default class Counter extends React.Component {
-  handleInc = () => {};
-  handleDec = () => {};
+  // in this case the state was declared and put in observable on this class
+  // @observable count = 0;
 
+  // How observable was declared on state, the functions access the values and change the values of them, but the page is not re rendering
+  // this bring more performance on component, or in anothe case, it's common too declare state out of class
+
+  // handleInc =() => {
+  //   count++;
+  // }
+
+  // handleDec =() => {
+  //   count++;
+  // }
+
+  handleInc = () => {
+    appState.increment();
+  };
+
+  handleDec = () => {
+    appState.decrement();
+  };
   render() {
     return (
       <div>
-        Counter : {this.count} <br />
-        <button> + </button>
-        <button> - </button>
+        Counter : {appState.count} <br />
+        <button onClick={this.handleInc}> + </button>
+        <button onClick={this.handleDec}> - </button>
       </div>
     );
   }
