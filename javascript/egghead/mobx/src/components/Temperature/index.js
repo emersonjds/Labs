fbe557d256195d04df77ae112104b445dad67753
Emerson Silva <emerson_jdss@hotmail.com>
2020-03-22 22:43:38 -0300

    solved error on computed metod

---
+++ javascript/egghead/mobx/src/components/Temperature/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { observable, computed } from "mobx";
+import { observer } from "mobx-react";
+
+@observer
+export default class Temperature extends Component {
+  @observable unit = "C";
+  @observable tempetatureCelsius = 25;
+
+  @computed get temperatureKelvin() {
+    return this.tempetatureCelsius + 9 / 5 + 32;
+  }
+
+  @computed get temperatureFahrenheit() {
+    return this.tempetatureCelsius + 273.15;
+  }
+
+  @computed get temperature() {
+    switch (this.unit) {
+      case "K":
+        return this.temperatureKelvin + "K*";
+      case "F":
+        return this.temperatureFahrenheit + "F*";
+      case "C":
+        return this.tempetatureCelsius + "C*";
+    }
+  }
+
+  render() {
+    return <div>Emerson </div>;
+  }
+}
--- javascript/egghead/mobx/src/index.js
+++ javascript/egghead/mobx/src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import { render } from "react-dom";
 import DevTools from "mobx-react-devtools";
 import Counter from "./components/Counter";
+import Temperature from "./components/Temperature";
 
 export default function App() {
-  return <Counter />;
+  return (
+    <div>
+      <Counter />
+      <Temperature />
+    </div>
+  );
 }
 
 render(
