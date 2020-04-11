eb8df9613cbcce023c9d9370581237a53d8a3a93
Emerson Silva <emerson_jdss@hotmail.com>
2020-04-10 23:22:41 -0300

    construct functional component

--- udacity/todo/src/components/ColumList/index.js
+++ udacity/todo/src/components/ColumList/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import If from "../../utils/If";
 
 const ColumList = ({ tasks, columTitle, addTodo, updateTodo }) => {
-  return <h3>{columTitle}</h3>;
+  let tarefas = [
+    {
+      id: Math.random(),
+      name: "Emerson",
+      status: "Add Todo",
+    },
+  ];
+  return (
+    <>
+      <h3>{columTitle}</h3>
+      <ul>
+        <br />
+        {tarefas.map((task) => (
+          <If test={task.status === columTitle}>
+            <form onSubmit={addTodo}>
+              <input type="text" />
+              <button>Add Todo</button>
+            </form>
+            <li>
+              {task.name}
+              <input type="checkbox"></input>
+            </li>
+          </If>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default ColumList;
