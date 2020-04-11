afea40affd087028cb263cafd6ab65506fde6d95
Emerson Silva <emerson_jdss@hotmail.com>
2020-04-10 22:16:02 -0300

    create utils

--- udacity/todo/src/App.js
+++ udacity/todo/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import ColumList from "./components/ColumList";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    tasks: [],
+  };
+  addTodo = () => console.log("return add ");
+  updateTodo = () => console.log("return update");
+
+  render() {
+    const { tasks } = this.state;
+    const columns = [
+      {
+        title: "Add Todo",
+        tasks,
+      },
+      {
+        title: "Done",
+        tasks,
+      },
+    ];
+
+    return (
+      <div className="App">
+        <h3>Todo React</h3>
+        <div className="container-tasks">
+          {/* TODO form adicionar task */}
+          {columns.map((colum) => (
+            <ColumList
+              key={colum.title}
+              columTitle={colum.title}
+              tasks={colum.tasks}
+              addTodo={this.addTodo}
+              updateTodo={this.updateTodo}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ udacity/todo/src/components/ColumList/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ColumList = ({ tasks, columTitle, addTodo, updateTodo }) => {
+  return <h3>{columTitle}</h3>;
+};
+
+export default ColumList;
