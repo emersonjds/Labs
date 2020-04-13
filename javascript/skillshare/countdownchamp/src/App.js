378fc5cc50b687bd4cb5cf1ad446910cc71e3fb7
Emerson Silva <emerson_jdss@hotmail.com>
2020-04-13 20:16:31 -0300

    changes with refs

--- javascript/skillshare/countdownchamp/src/App.js
+++ javascript/skillshare/countdownchamp/src/App.js
@@ -2,18 +2,37 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      deadline: 'December, 14th, 2020',
+      newDeadline: null
+    }
+  }
+
+  handleChanged  = () => {
+    this.setState({
+      deadline: this.state.newDeadline,
+    })
+   this.refs.input.value = '';
+  }
+
   render() {
+    const {deadline} = this.state
     return (
       <div className="App">
-        <div className="App-title">Countdown to Champ</div>
+        <div className="App-title">{deadline}</div>
         <div className="App-days">14 days</div>
         <div className="App-hr">37 hrs</div>
         <div className="App-min">22 min</div>
         <div className="App-sec">10 sec</div>
-        <form onSubmit="" className="form-submit">
-          <input type="text" placeholder="Digite a data" />
-          <button>Send data</button>
-        </form>
+        <div>
+          <input type="text" placeholder="Digite a data" onChange={e => this.setState({
+            newDeadline: e.target.value,
+          })} ref="input"/>
+          <button onClick={() => this.handleChanged()}>Send data</button>
+        </div>
       </div>
     );
   }
