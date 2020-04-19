e7f6f132e6f0f64efeb4c7116f03740551e669eb
Emerson Silva <emerson_jdss@hotmail.com>
2020-04-19 19:54:46 -0300

    add clean function

--- javascript/logic/js/calc/index.js
+++ javascript/logic/js/calc/index.js
@@ -2,11 +2,15 @@ var number = "0";
 
 function incluirDigito(value) {
   number === "0" ? (number = value) : (number += value);
-
   mostraNoDisplay(number);
 }
 
 function mostraNoDisplay(valor) {
   var display = document.getElementById("display");
   display.innerHTML = valor;
+}
+
+function limpar() {
+  var display = document.getElementById("display");
+  display.innerHTML = "0";
 }
