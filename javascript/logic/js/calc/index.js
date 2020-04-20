52204d586069a1517f3ea9dee065d435629bc09f
Emerson Silva <emerson_jdss@hotmail.com>
2020-04-20 11:38:09 -0300

    add operations; validations of return of operations; keep operation

--- javascript/logic/js/calc/index.js
+++ javascript/logic/js/calc/index.js
@@ -1,10 +1,37 @@
-var number = "0";
+var n1 = "0";
 var operacao = null;
-var n1 = "";
+var n2 = "";
+var clicadoEmIgual = false;
 
 function incluirDigito(value) {
-  number === "0" ? (number = value) : (number += value);
-  mostraNoDisplay(number);
+  if (operacao !== null) {
+    n2 += value;
+    mostraNoDisplay(n2);
+  } else {
+    n1 === "0" ? (n1 = value) : (n1 += value);
+    mostraNoDisplay(n1);
+  }
+}
+
+function calcular() {
+  var nCalculado = 0;
+  var _n1 = parseFloat(n1);
+  var _n2 = parseFloat(n2);
+  switch (operacao) {
+    case "+":
+      nCalculado = _n1 + _n2;
+      break;
+    case "-":
+      nCalculado = _n1 - _n2;
+      break;
+    case "*":
+      nCalculado = _n1 * _n2;
+      break;
+    case "/":
+      nCalculado = _n1 / _n2;
+      break;
+  }
+  return nCalculado;
 }
 
 function mostraNoDisplay(valor) {
@@ -18,5 +45,28 @@ function limpar() {
 }
 
 function iniciarCalculo(simbolo) {
-  return (operacao = simbolo);
+  if (clicadoEmIgual) {
+    clicadoEmIgual = false;
+    n2 = "";
+  }
+
+  if (operacao === null || n2 === "") {
+    operacao = simbolo;
+  } else {
+    // already contains n1, operation and n2
+    var result = calcular();
+    n1 = result;
+    operacao = simbolo;
+    n2 = "";
+    mostraNoDisplay(n1);
+  }
+
+  console.log(n1, operacao, n2);
+}
+
+function finalizarCalculo() {
+  clicadoEmIgual = true;
+  var result = calcular();
+  n1 = result;
+  mostraNoDisplay(n1);
 }
