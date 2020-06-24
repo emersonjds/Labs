8c590461ee9aa971d95fb52df2045fc9f775a4b8
Emerson Silva <emerson_jdss@hotmail.com>
2020-06-24 17:07:55 -0300

    config new routes

--- javascript/rocketseat/goNode/src/index.js
+++ javascript/rocketseat/goNode/src/index.js
@@ -1,14 +1,40 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({
-        message: 'Hello World'
-    })
-})
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Node running",
+  });
+});
 
+app.get("/projects", (req, res) => {
+  return res.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
+});
+
+app.post("/projects", (req, res) => {
+  return res.json([
+    "Projeto 1",
+    "Projeto 2",
+    "Projeto 3",
+    "Novo Projeto Adicionado",
+  ]);
+});
+
+app.put("/projects/:id", (req, res) => {
+  return res.json([
+    "Projeto 4",
+    "Projeto 2",
+    "Projeto 3",
+    "Novo Projeto Adicionado",
+  ]);
+});
+
+app.delete("/projects/:id", (req, res) => {
+  return res.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
+});
 
 //Always created an application in node it's necessary to say where the application will be listening
-app.listen(3000);
-
+app.listen(3000, () => {
+  console.log("🚀 back-end rodando");
+});
