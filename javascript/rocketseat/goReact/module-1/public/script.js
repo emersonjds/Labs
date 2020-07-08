8ddb3b6f119e3f51e74c05c9cf5ffc71c147036c
Emerson Silva <emerson_jdss@hotmail.com>
2020-07-08 15:53:26 -0300

    config web pack and babel

---
+++ javascript/rocketseat/goReact/module-1/public/script.js
@@ -0,0 +1,4 @@
+//Babel : converter (transpilar) codigo React para que o navegador consiga entender
+//WebPack : Pra cada tipo de arquivo (.js, .css, .png) ele converte o codigo de uma maneira diferente
+
+//Loaders ( estao dentro do Webpack): babel-loader , css-loader , image-loader, file-loader
