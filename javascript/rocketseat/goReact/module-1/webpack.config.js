6885ad9db3fe90c0aee320346f3edc5a8fee3d50
Emerson Silva <emerson_jdss@hotmail.com>
2020-07-09 11:46:40 -0300

    config webpack with live reloading

---
+++ javascript/rocketseat/goReact/module-1/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      //loaders
+      {
+        //todos os arquivos que terminam com js
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
