b145b4c7b3992b1565eaee6d69d8763b8bf8ec7b
Emerson Silva <emerson_jdss@hotmail.com>
2020-07-13 23:23:56 -0300

    refactor class

--- javascript/rocketseat/goReact/module-1/src/Header.js
+++ javascript/rocketseat/goReact/module-1/src/Header.js
@@ -1,16 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
 
-// export default function Header(props) {
-//   return <header>{props.data}</header>;
-// }
-
-export default class Header extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { data } = this.props;
-    return <h1>{data}</h1>;
-  }
+export default function Header({ data }) {
+  return <h1>{data}</h1>;
 }
