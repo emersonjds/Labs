5a4a28f765dab7de45863e1f02b13cd72ac6abf3
Emerson Silva <emerson_jdss@hotmail.com>
2020-07-15 16:54:26 -0300

    working with useEffect and axios

--- javascript/rocketseat/goReact/module-1/src/App.js
+++ javascript/rocketseat/goReact/module-1/src/App.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import "./App.css";
+import api from "./services/api";
 
 export default function App() {
-  const [projects, setProjects] = useState(["projeto 1", "projeto 2"]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("/projects").then((response) => {
+      console.log(response);
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() {
     //neste momento em que um novo array foi chamado, uma nova estrutura esta sendo criada nao alterando a anterior
@@ -15,7 +23,9 @@ export default function App() {
       <Header title="Emerson"></Header>
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>
+            {project.name} - {project.owner}{" "}
+          </li>
         ))}
       </ul>
       <button type="button" onClick={handleAddProject}>
