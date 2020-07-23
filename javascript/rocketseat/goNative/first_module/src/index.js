e69f1cb286d47ede9d2595e9816da29e3267647c
Emerson Silva <emerson_jdss@hotmail.com>
2020-07-23 15:22:35 -0300

    first styles

--- javascript/rocketseat/goNative/first_module/src/index.js
+++ javascript/rocketseat/goNative/first_module/src/index.js
@@ -1,11 +1,24 @@
-import React from 'react;';
+import React from 'react';
 
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
     </View>
   );
 }
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: '#FFF',
+  },
+});
