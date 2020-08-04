fa7165316ea54cb32f499625fe77adbd8a1f58f3
Emerson Silva <emerson_jdss@hotmail.com>
2020-08-04 12:06:33 -0300

    tests with localhost and machine ip to call apis

--- javascript/rocketseat/goNative/first_module/src/index.js
+++ javascript/rocketseat/goNative/first_module/src/index.js
@@ -41,7 +41,7 @@ export default function App() {
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- javascript/rocketseat/goNative/first_module/src/services/api.js
+++ javascript/rocketseat/goNative/first_module/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseUrl: 'http://localhost:3333',
+  baseUrl: 'http://192.168.15.51:3333',
 });
 
 export default api;
