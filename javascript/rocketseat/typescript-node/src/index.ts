109d88f77b5384a9ccadc38b3a48e484e035bd77
Emerson Silva <emerson_jdss@hotmail.com>
2020-10-21 14:39:48 -0300

    enable debug

--- javascript/rocketseat/typescript-node/src/index.ts
+++ javascript/rocketseat/typescript-node/src/index.ts
@@ -2,7 +2,18 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
+
 app.use(cors());
+
+//habilitar que a aplicação consiga entender dados vindos em JSON
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const {name, email} = req.body;
+
+  const user = {name, email}
+  return res.json(user);
+})
 
 app.get('/', (req, res) => {
   return res.json({message: 'GET'})
