1f2c60a5dd1890b7183cbfd064c95a2c4c241d2d
Emerson Silva <emerson_jdss@hotmail.com>
2020-11-27 12:21:23 -0300

    config changes on micro repositories

--- javascript/rocketseat/typescript-node/routes/appointment.routes.ts
+++ javascript/rocketseat/typescript-node/routes/appointment.routes.ts
@@ -4,9 +4,6 @@ import uniqid from 'uniqid';
 const appointmentRouter = Router();
 
 const appointments = [];
-appointmentRouter.get('/', (req, res) => {
-  return res.json({message: 'Emerson'});
-})
 
 appointmentRouter.post('/', (req, res) => {
   const {provider, date} = req.body;
@@ -17,7 +14,7 @@ appointmentRouter.post('/', (req, res) => {
     date
   }
 
-  appointments.push(appointment)
+  appointments.push(appointment);
 
   return res.json(appointment);
 
