7427661ac56a8f8b9de6ae53df68a3fb0d27bb34
Emerson Silva <emerson_jdss@hotmail.com>
2021-08-02 22:46:32 -0300

    config home test

--- javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/App.test.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/App.test.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { render } from '@testing-library/react-native'
 
 import App from '../App';
+import Home from '../screens/Home';
 
 describe('App', () => {
   test('should render correctly', () => {
@@ -10,5 +11,13 @@ describe('App', () => {
     wrapper.getByTestId('app')
   })
 })
+
+describe('Home', () => {
+  test('render correctly', () => {
+    const wrapper = render(<Home />)
+    wrapper.getByTestId('home')
+  })
+})
+
 
 
---
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Home: React.FC = () => {
+  return <View testID="home" />;
+}
+
+export default Home;
