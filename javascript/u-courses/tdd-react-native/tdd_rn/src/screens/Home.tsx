676389d06e94d14d280f7b06736047351a508af7
Emerson Silva <emerson_jdss@hotmail.com>
2021-08-06 11:59:38 -0300

    adjust colors and contants

--- javascript/u-courses/tdd-react-native/tdd_rn/src/screens/Home.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/screens/Home.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import { StyleSheet, Text } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { Colors } from './constants';
 
 const Home: React.FC = () => {
-  return <View testID="home" />;
+  return <LinearGradient
+    colors={[
+      Colors.DARKER_GRAY,
+      Colors.LIGHT_GRAY,
+    ]}
+    style={styles.container}>
+    <Text>
+      Emerson
+    </Text>
+  </LinearGradient>
+
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 40,
+    alignContent: 'space-between',
+    justifyContent: 'space-evenly',
+  }
+})
+
 
 export default Home;
---
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/screens/constants.tsx
@@ -0,0 +1,6 @@
+export enum Colors {
+  LIGHT_GRAY = '#f5f5f5',
+  GRAY = '#e8e8e8',
+  DARK_GRAY = '#a0a0a0',
+  DARKER_GRAY = '#808080',
+}
