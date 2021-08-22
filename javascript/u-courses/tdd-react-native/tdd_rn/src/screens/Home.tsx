fd7bbf5cd9818e079cac7cf10831da156d36ad6f
Emerson Silva <emerson_jdss@hotmail.com>
2021-08-22 15:33:28 -0300

    mock date for tests

--- javascript/u-courses/tdd-react-native/tdd_rn/src/screens/Home.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/screens/Home.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { Colors } from './constants';
 
+function formatDate(date: Date) {
+  const today = date.getDate();
+  const month = `${
+    [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ][date.getMonth()]
+  }`;
+
+  return `${month} ${today}, ${date.getFullYear()}`;
+}
+
 const Home: React.FC = () => {
+  const now = new Date();
   return (
     <LinearGradient
       colors={[Colors.DARKER_GRAY, Colors.LIGHT_GRAY]}
       style={styles.container}
       testID="home">
-      <Text>Emerson</Text>
+      <View style={styles.title}>
+        <View style={styles.date}>
+          <Text>{formatDate(now)}</Text>
+        </View>
+      </View>
     </LinearGradient>
   );
 };
@@ -20,6 +47,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 40,
     alignContent: 'space-between',
     justifyContent: 'space-evenly',
+  },
+  title: {
+    justifyContent: 'flex-end',
+  },
+  date: {
+    color: Colors.GRAY,
+    fontSize: 13,
   },
 });
 
