9239b66eaf772db8df23725dd02253b2406f4d30
Emerson Silva <emerson_jdss@hotmail.com>
2021-08-22 14:46:31 -0300

    config eslint rules

--- javascript/u-courses/tdd-react-native/tdd_rn/src/screens/Home.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/screens/Home.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import { Colors } from './constants';
+import {Colors} from './constants';
 
 const Home: React.FC = () => {
-  return <LinearGradient
-    colors={[
-      Colors.DARKER_GRAY,
-      Colors.LIGHT_GRAY,
-    ]}
-    style={styles.container}
-    testID="home">
-    <Text>
-      Emerson
-    </Text>
-  </LinearGradient>
-
-}
+  return (
+    <LinearGradient
+      colors={[Colors.DARKER_GRAY, Colors.LIGHT_GRAY]}
+      style={styles.container}
+      testID="home">
+      <Text>Emerson</Text>
+    </LinearGradient>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -24,8 +20,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 40,
     alignContent: 'space-between',
     justifyContent: 'space-evenly',
-  }
-})
-
+  },
+});
 
 export default Home;
