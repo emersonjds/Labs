0f1d1cdc062cf2fb7329c809158695f40415a95e
Emerson Silva <emerson_jdss@hotmail.com>
2021-09-16 07:47:20 -0300

    adjust errors on project

--- javascript/rocketseat/ignite/rn/mySkills/src/pages/Home/index.js
+++ javascript/rocketseat/ignite/rn/mySkills/src/pages/Home/index.js
@@ -1,39 +1,10 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
+import { View } from 'react-native';
 
-import {
-  ButtonAdd,
-  Container,
-  InputText,
-  TextButton,
-  Title,
-  TextSkill,
-} from './styles';
+// import { Container } from './styles';
 
-import { ButtonComponent } from '../../components/Button';
-
-export const Home = () => {
-  const [mySkills, setMySkills] = useState('');
-  const [skills, setSkills] = useState([]);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const handleAddSkill = useCallback(() => {
-    setSkills([...skills, mySkills]);
-    setMySkills('');
-  });
-
-  return (
-    <Container>
-      <Title>Welcome, Emerson</Title>
-      <InputText
-        placeholder="your skill"
-        placeholderTextColor="#cecece"
-        onChangeText={text => setMySkills(text)}
-        value={mySkills}
-      />
-      <Title style={{ marginTop: 20 }}>Welcome, Emerson</Title>w
-      <ButtonComponent onPress={handleAddSkill} />
-      <Title style={{ marginTop: 20 }}>My Skills</Title>
-      {skills && skills.map(data => <TextSkill key={data}>{data}</TextSkill>)}
-    </Container>
-  );
+const Home = () => {
+  return <View />;
 };
+
+export default Home;
--- javascript/rocketseat/ignite/rn/mySkills/src/pages/Home/styles.js
+++ javascript/rocketseat/ignite/rn/mySkills/src/pages/Home/styles.js
@@ -1,29 +1,3 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  background-color: black;
-  flex: 1;
-  padding: 20px;
-`;
-
-export const Title = styled.Text`
-  color: #fff;
-  margin-top: 10px;
-  font-weight: bold;
-  font-size: 24px;
-`;
-
-export const InputText = styled.TextInput`
-  padding: 10px;
-  background-color: #999;
-  margin-top: 20px;
-  border-radius: 5px;
-`;
-
-export const TextSkill = styled.Text`
-  color: #000;
-  font-size: 16px;
-  background-color: #cecece;
-  padding: 10px;
-  margin-top: 10px;
-`;
+const Container = styled.View``;
