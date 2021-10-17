e1892db40a7820a02f337bfd759f1e09b856cd94
Emerson Silva <emerson_jdss@hotmail.com>
2021-10-16 22:25:00 -0300

    enable styled system

--- javascript/rocketseat/ignite/rn/mySkills/src/pages/Home/styles.js
+++ javascript/rocketseat/ignite/rn/mySkills/src/pages/Home/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import Colors from '../../Contants';
+import { space } from 'styled-system';
 
 export const Container = styled.View`
   flex: 1;
@@ -10,6 +11,7 @@ export const Container = styled.View`
 export const Title = styled.Text`
   color: ${Colors.white}
   font-size: 20px;
+  ${space}
 `;
 
 export const InputDefault = styled.TextInput.attrs({
@@ -27,11 +29,10 @@ export const ButtonAdd = styled.TouchableOpacity`
   padding: 15px;
   margin-top: 10px;
   align-items: center;
-
-  `
+`;
 
 export const TextButtonAdd = styled.Text`
-    color: ${Colors.white};
-    font-size: 20px;
-    font-weight: bold;
-    `;
+  color: ${Colors.white};
+  font-size: 20px;
+  font-weight: bold;
+`;
