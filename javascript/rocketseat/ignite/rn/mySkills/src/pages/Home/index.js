f8a91c1994701abe8a76a6852bb63333015ed45c
Emerson Silva <emerson_jdss@hotmail.com>
2021-10-16 22:25:21 -0300

    configure new States

--- javascript/rocketseat/ignite/rn/mySkills/src/pages/Home/index.js
+++ javascript/rocketseat/ignite/rn/mySkills/src/pages/Home/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { ButtonAdd, Container, InputDefault, Title, TextButtonAdd } from './styles';
+import React, { useState } from 'react';
+import {
+  ButtonAdd,
+  Container,
+  InputDefault,
+  Title,
+  TextButtonAdd,
+} from './styles';
 
 const Home = () => {
+  const [mySkill, setMySkill] = useState('');
+  const [skylls, setSkylls] = useState([]);
+
+  const handleAddSkill = () => {
+    setSkylls([...skylls, mySkill]);
+    setMySkill('');
+  };
+
   return (
     <Container>
       <Title>Your Skills</Title>
-      <InputDefault placeholder="What do you know?" />
+      <InputDefault placeholder="What do you know?" onChangeText={setMySkill} />
+      <Title mt={20}>Your Skills</Title>
 
-      <Title style={{ marginTop: 20 }}>Your Skills</Title>
-
-      <ButtonAdd>
+      <ButtonAdd onPress={handleAddSkill}>
         <TextButtonAdd>Add</TextButtonAdd>
       </ButtonAdd>
     </Container>
