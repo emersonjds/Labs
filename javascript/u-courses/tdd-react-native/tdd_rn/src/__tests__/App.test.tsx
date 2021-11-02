068e43518d164fe1b778b3666e3be734cd26e90f
Emerson Silva <emerson_jdss@hotmail.com>
2021-11-02 13:09:27 -0300

    change for render library project

--- javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/App.test.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/App.test.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 import App from '../App';
 
 describe('Test App', () => {
-  it('should render correctly', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
+  it('should render corretly', () => {
+    render(<App />);
   });
 });
 
