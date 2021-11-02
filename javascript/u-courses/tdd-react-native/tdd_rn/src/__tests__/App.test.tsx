18f171b0febf632cba2833932b8ff47a27af138b
Emerson Silva <emerson_jdss@hotmail.com>
2021-11-02 11:05:21 -0300

    adjust rule of function

--- javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/App.test.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/App.test.tsx
@@ -2,7 +2,7 @@ function sum(a: number, b: number): number {
     return a + b;
 }
 
-describe('should be return a correct value of function', () => {
+describe('Test function sum', () => {
     it('should be return a correct value of function', () => {
         expect(sum(1, 2)).toBe(3);
     });
