08588d1da905ccf0c1aa75a13e592e733fb15c76
Emerson Silva <emerson_jdss@hotmail.com>
2021-11-06 19:33:43 -0300

    create color constants

--- javascript/u-courses/tdd-react-native/tdd_rn/src/screens/Home/index.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/screens/Home/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
 import { Box, BoxFull } from '../../components/Box/styles';
+import { Colors } from '../../Contants';
 
 function Home() {
   return (
-    <BoxFull bg="blue" p={20} testID="home-screen">
-      <Box />
-    </BoxFull>
+    <SafeAreaView style={{
+      flex: 1
+    }}>
+      <BoxFull p={20} testID="home-screen" bg={Colors.dark}>
+        <Box mt={40} width="100%" height={30} justifyContent="center" mb={10}>
+          <Text
+            style={{
+              fontSize: 20,
+              color: Colors.white,
+            }}>
+            Date
+          </Text>
+        </Box>
+        <Box width="100%" justifyContent="center" style={{
+          bordertoWidth: 1,
+          borderColor: Colors.accent,
+        }}>
+          <Text style={{
+            fontSize: 60,
+            color: Colors.white
+          }}>
+            Date
+          </Text>
+        </Box>
+      </BoxFull>
+    </SafeAreaView>
+
   );
 }
 
