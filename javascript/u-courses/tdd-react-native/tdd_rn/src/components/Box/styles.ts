f68045c6b25906b7395b979eaeacea7e4c720131
Emerson Silva <emerson_jdss@hotmail.com>
2021-11-06 15:02:47 -0300

    adjust styles

---
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/components/Box/styles.ts
@@ -0,0 +1,19 @@
+import { ViewProps } from 'react-native';
+import styled from 'styled-components/native';
+
+import {
+  FlexboxProps,
+  ColorProps,
+  BorderRadiusProps,
+  HeightProps,
+  WidthProps,
+} from 'styled-system';
+
+type BoxProps = FlexboxProps &
+  ColorProps &
+  BorderRadiusProps &
+  HeightProps &
+  WidthProps &
+  ViewProps;
+
+export const Container = styled.View<BoxProps>``;
