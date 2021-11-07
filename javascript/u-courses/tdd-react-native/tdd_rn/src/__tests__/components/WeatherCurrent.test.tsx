74974961ffcbf63719cb3eab96124c2c56dc82c0
Emerson Silva <emerson_jdss@hotmail.com>
2021-11-07 10:02:39 -0300

    config error test

--- javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/components/WeatherCurrent.test.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/components/WeatherCurrent.test.tsx
@@ -1,10 +1,11 @@
-import { render } from "@testing-library/react-native";
-import React from "react";
-import WeatherCurrent from "../../components/WeatherCurrent";
+import { render } from '@testing-library/react-native';
+import React from 'react';
+import WeatherCurrent from '../../components/WeatherCurrent';
 
-describe("WeatherCoordinates", () => {
-    test("renders correctly", () => {
-        const wrapper = render(<WeatherCurrent />);
-        wrapper.getByTestId("WeatherCurrent");
-    })
-})
+describe('WeatherCoordinates', () => {
+  test('renders correctly', () => {
+    const wrapper = render(<WeatherCurrent />);
+    wrapper.getByTestId('WeatherCurrent');
+  });
+  test('should navigate to another screen', () => {});
+});
