461f26e021a0d8c271767aa1c3647224c6d79671
Emerson Silva <emerson_jdss@hotmail.com>
2021-11-07 20:26:32 -0300

    rebase branch

--- javascript/u-courses/tdd-react-native/tdd_rn/src/services/LocationService.ts
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/services/LocationService.ts
@@ -1,15 +1,14 @@
-import Geolocation from '@react-native-community/geolocation'
+import Geolocation from '@react-native-community/geolocation';
 
 class LocationService {
   static async getCurrentPosition() {
     return Geolocation.getCurrentPosition(
-      position => {
-        return {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        }
-      }
-    )
+      (position: { coords: { latitude: any; longitude: any } }) => {
+      return {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      };
+    });
   }
 }
 
