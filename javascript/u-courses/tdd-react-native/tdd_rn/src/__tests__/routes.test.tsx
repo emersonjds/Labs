6f8307db0381ff73a2bfdc5aeb0dcd23cea497a9
Emerson Silva <emerson_jdss@hotmail.com>
2021-11-07 12:56:10 -0300

    config tests for AppNavigator

---
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/routes.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import AppNavigator from '../routes';
+import Home from '../screens/Home';
+
+jest.mock('../screens/Home', () => jest.fn());
+
+describe('AppNavigator', () => {
+    test('Should render Home by default', async () => {
+        (Home as jest.Mock).mockReturnValueOnce(<View testID="home" />);
+    })
+})
--- javascript/u-courses/tdd-react-native/tdd_rn/src/routes.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/routes.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { View } from 'react-native';
 
 function AppNavigator() {
-
+    return <View />
 }
+
+export default AppNavigator;
