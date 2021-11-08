2341c52125f23d8cd50adb1e8f187315e8f61674
Emerson Silva <emerson_jdss@hotmail.com>
2021-11-07 23:10:20 -0300

    config routes

--- javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/routes.test.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/__tests__/routes.test.tsx
@@ -1,6 +1,8 @@
+import { render, waitFor } from '@testing-library/react-native';
 import React from 'react';
 import { View } from 'react-native';
 import AppNavigator from '../routes';
+
 import Home from '../screens/Home';
 
 jest.mock('../screens/Home', () => jest.fn());
@@ -8,5 +10,10 @@ jest.mock('../screens/Home', () => jest.fn());
 describe('AppNavigator', () => {
   test('Should render Home by default', async () => {
     (Home as jest.Mock).mockReturnValueOnce(<View testID="home" />);
+    const wrapper = render(<AppNavigator />);
+
+    await waitFor(() => {
+      wrapper.getByTestId('home');
+    });
   });
 });
--- javascript/u-courses/tdd-react-native/tdd_rn/src/routes.tsx
+++ javascript/u-courses/tdd-react-native/tdd_rn/src/routes.tsx
@@ -1,8 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import { View } from 'react-native';
+import Home from './screens/Home';
+
+export type RootStackParamList = {
+  Home: undefined;
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
 
 function AppNavigator() {
-    return <View />
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator screenOptions={{ headerShown: false }}>
+        <RootStack.Screen name="Home" component={Home} />
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default AppNavigator;
