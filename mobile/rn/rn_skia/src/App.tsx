2b355ac70007b3c7fca9a9478b5f19f6951021b2
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-17 23:23:00 -0300

    adjust space in brackets

--- mobile/rn/rn_skia/src/App.tsx
+++ mobile/rn/rn_skia/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
 const App: React.FC = () => (
   <SafeAreaView>
