6d40d3b02c3b31e46f6947514b29916b234f2c92
Emerson Silva <emerson_jdss@hotmail.com>
2022-03-17 23:21:51 -0300

    remove unused App structure

---
+++ mobile/rn/rn_skia/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App: React.FC = () => (
+  <SafeAreaView>
+    <View
+      style={{
+        padding: 10,
+      }}>
+      <Text>Skia Studies</Text>
+    </View>
+  </SafeAreaView>
+);
+
+export default App;
