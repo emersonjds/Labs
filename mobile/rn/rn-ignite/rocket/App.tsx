32910bd8171149d3fa2a57eff34c44bed715a565
Emerson Silva <emerson_jdss@hotmail.com>
2022-07-27 18:35:01 -0300

    adjust error on load fonts

--- mobile/rn/rn-ignite/rocket/App.tsx
+++ mobile/rn/rn-ignite/rocket/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import SignIn from "./src/screens/SignIn";
 import {NativeBaseProvider, Box} from 'native-base'
+import {THEME} from "./src/styles/theme";
+import AppLoading from 'expo-app-loading';
+import {useFonts} from "expo-font";
+import {Roboto_400Regular, Roboto_700Bold} from "@expo-google-fonts/roboto";
+import {Text, View} from "react-native";
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View>
+        <Text>
+          Error on find fonts
+        </Text>
+      </View>
+    );
+  }
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider
+      theme={THEME}
+    >
       <Box>
         Emerson
       </Box>
