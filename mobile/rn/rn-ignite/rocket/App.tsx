8aa82916b3f86a3b3e01ed37becf258c07c2fa1e
Emerson Silva <emerson_jdss@hotmail.com>
2022-07-28 23:07:18 -0300

    adjust for test in Home

--- mobile/rn/rn-ignite/rocket/App.tsx
+++ mobile/rn/rn-ignite/rocket/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {NativeBaseProvider, StatusBar} from 'native-base'
-import {useFonts} from "expo-font";
-import {Roboto_400Regular, Roboto_700Bold} from "@expo-google-fonts/roboto";
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import { useFonts } from "expo-font";
+import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import Loading from "./src/components/Loading";
 import SignIn from "./src/screens/SignIn";
-import {THEME} from "./src/styles/theme";
+import { THEME } from "./src/styles/theme";
+import Home from './src/screens/Home';
 export default function App() {
   let [fontsLoaded] = useFonts({
     Roboto_700Bold,
@@ -16,11 +17,11 @@ export default function App() {
       theme={THEME}
     >
       <StatusBar barStyle="light-content"
-      backgroundColor="transparent"
-      translucent={true}
+        backgroundColor="transparent"
+        translucent={true}
       />
       {
-        fontsLoaded ? <SignIn/> : <Loading/>
+        fontsLoaded ? <Home /> : <Loading />
       }
     </NativeBaseProvider>
   );
