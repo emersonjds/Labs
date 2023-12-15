cd24fd454f547d1c185983120a934fe329a24363
Emerson Silva <emerson_jds@live.com>
2023-12-14 23:34:42 -0300

    add starter

--- javascript/rocketseat/ignite/react/fundamentals/src/app/layout.tsx
+++ javascript/rocketseat/ignite/react/fundamentals/src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Fundamentals',
-  description: 'Fundamentals',
+  description: 'Stater Fundamentals',
 }
 
 export default function RootLayout({
