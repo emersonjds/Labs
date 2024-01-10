f0e0a7d3d39a6f6942d491424c191b2923551335
emersonjds <emerson_jdss@hotmail.com>
2024-01-10 09:40:40 -0300

    adjust importo

--- javascript/angular-houses/src/app/home/home.component.ts
+++ javascript/angular-houses/src/app/home/home.component.ts
@@ -15,16 +15,7 @@ import { CommonModule } from '@angular/common';
       </form>
     </section>
   `,
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
 })
 
-export class HomeComponent {
-
-}
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
+export class HomeComponent {}
