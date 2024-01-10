01d2dc362e6a3832da7eb6d1b46f7d90b2a936fe
emersonjds <emerson_jdss@hotmail.com>
2024-01-10 10:54:29 -0300

    add new class to button

--- javascript/angular-houses/src/app/home/home.component.ts
+++ javascript/angular-houses/src/app/home/home.component.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
     <section>
       <form>
         <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <button class="buttonText" type="button">Search</button>
       </form>
     </section>
 `,
