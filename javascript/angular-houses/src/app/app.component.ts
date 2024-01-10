71d785e8ba30f9728b446e35b5e4eb3c42866a3d
emersonjds <emerson_jds@live.com>
2024-01-09 23:05:00 -0300

    update on TS

--- javascript/angular-houses/src/app/app.component.ts
+++ javascript/angular-houses/src/app/app.component.ts
@@ -9,15 +9,12 @@ import { HomeComponent } from './home/home.component';
   imports: [CommonModule, RouterOutlet, HomeComponent],
   // templateUrl: './app.component.html',
   template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-home></app-home>
-    </section>
-  </main>
-`,
+    <main>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
--- javascript/angular-houses/src/app/home/home.component.ts
+++ javascript/angular-houses/src/app/home/home.component.ts
@@ -4,11 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
+  templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
