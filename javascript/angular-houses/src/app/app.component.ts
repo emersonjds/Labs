7a839831cd252bd10421355a4873c0b067554fcc
emersonjds <emerson_jds@live.com>
2024-01-09 23:03:03 -0300

    call HomeComponent

--- javascript/angular-houses/src/app/app.component.ts
+++ javascript/angular-houses/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent],
   // templateUrl: './app.component.html',
-  template: `<h1>Hello world!</h1>`,
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
