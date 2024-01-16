4f30801d932f09d0c6a230deea8947a22eec32e4
emersonjds <emerson_jdss@hotmail.com>
2024-01-16 16:31:53 -0300

    add Link with passHref

--- javascript/alura/nextjs/pages/faq.js
+++ javascript/alura/nextjs/pages/faq.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function FaqPage() {
   return (
     <div>
       <h1>FAQ</h1>
       <p>Here is the FAQ page</p>
-      <a href="/">Go back home</a>
+      <Link href="/" passHref>
+        <a>Go back home</a>
+      </Link>
     </div>
-  )
+  );
 }
