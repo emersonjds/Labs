22dfd7ae8497435458c823c47052b5f8d6c78cc4
emersonjds <emerson_jdss@hotmail.com>
2024-01-16 16:39:24 -0300

    adjust navigation on Faq

--- javascript/alura/nextjs/pages/faq.js
+++ javascript/alura/nextjs/pages/faq.js
@@ -1,13 +1,12 @@
-import Link from "next/link";
+import Link from "./src/components/Link";
 
 export default function FaqPage() {
   return (
     <div>
       <h1>FAQ</h1>
       <p>Here is the FAQ page</p>
-
       <Link href="/" passHref>
-        <a>Go back home</a>
+        <a>Go to Home</a>
       </Link>
     </div>
   );
