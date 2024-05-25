b9179d402741c72fe2131f816974be9feb8ea35b
Emerson Silva <emerson_jdss@hotmail.com>
2024-05-25 20:32:42 -0300

    add otherNumber for tests

--- javascript/alura/js-logic-one/app.js
+++ javascript/alura/js-logic-one/app.js
@@ -2,7 +2,8 @@ alert("Welcome to secret number game");
 
 let secretNumber = 29;
 
-prompt('Choose a number between 0 and 30');
+let otherNumber = prompt("Choose a number between 0 and 30");
 
-
-
+if (otherNumber == secretNumber) {
+  console.log("You got ir right the number");
+}
