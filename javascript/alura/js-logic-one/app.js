be3a822b41e8d894056e9cd4a810a1c33f213157
Emerson Silva <emerson_jdss@hotmail.com>
2024-05-27 22:32:20 -0300

    remove comment

--- javascript/alura/js-logic-one/app.js
+++ javascript/alura/js-logic-one/app.js
@@ -4,7 +4,6 @@ let secretNumber = 29;
 
 let otherNumber = prompt("Choose a number between 0 and 30");
 
-// Compare one number with other number
 if (otherNumber == secretNumber) {
   console.log("You got ir right the number");
 } else {
