55ae7c084e5392a76ae08e7ee5b71d619dd1a85b
Emerson Silva <emerson_jdss@hotmail.com>
2024-05-31 09:48:11 -0300

    adjust worng message

--- javascript/alura/js-logic-one/app.js
+++ javascript/alura/js-logic-one/app.js
@@ -7,5 +7,5 @@ let otherNumber = prompt("Choose a number between 0 and 30");
 if (otherNumber == secretNumber) {
   console.log("You got ir right the number");
 } else {
-  alert("Voce errou");
+  alert("You wrong the answer");
 }
