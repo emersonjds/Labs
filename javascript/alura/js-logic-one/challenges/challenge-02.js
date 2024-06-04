1d63eb1414322f7784ea15e063d75005a5d966fd
Emerson Silva <emerson_jdss@hotmail.com>
2024-06-03 23:18:16 -0300

    get weekDay

---
+++ javascript/alura/js-logic-one/challenges/challenge-02.js
@@ -0,0 +1,11 @@
+// Week Day
+
+let weekDay = "";
+
+weekDay = prompt("What day is today ?!");
+
+if (weekDay == "Saturday" || weekDay == "Sunday") {
+  console.log("Have a nice weekend");
+} else {
+  console.log("Have a nice week");
+}
