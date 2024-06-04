9fe22b688b8796b7c8e05d2f5bb8e37bd5c6a8cd
Emerson Silva <emerson_jdss@hotmail.com>
2024-06-03 22:59:26 -0300

    template string

--- javascript/alura/js-logic-one/app.js
+++ javascript/alura/js-logic-one/app.js
@@ -5,7 +5,7 @@ let secretNumber = 40;
 let otherNumber = prompt("Choose a number between 0 and 30");
 
 if (otherNumber == secretNumber) {
-  console.log("You got ir right the number");
+  console.log(`You got it the right number ${secretNumber}`);
 } else {
   alert("You wrong the answer");
 }
