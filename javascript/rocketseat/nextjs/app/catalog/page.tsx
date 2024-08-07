03b28f311ba2eae27dc7581939723606f65956a6
Emerson Silva <emerson_jdss@hotmail.com>
2024-08-06 23:16:48 -0300

    adjust ident

--- javascript/rocketseat/nextjs/app/catalog/page.tsx
+++ javascript/rocketseat/nextjs/app/catalog/page.tsx
@@ -1,7 +1,7 @@
 export default function Catalog() {
-    return (
-        <div>
-        <h1>Catalog</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Catalog</h1>
+    </div>
+  );
 }
