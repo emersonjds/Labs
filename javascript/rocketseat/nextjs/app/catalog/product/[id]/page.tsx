a4ff517e49813a724fcbc853f722e8babce7f784
Emerson Silva <emerson_jdss@hotmail.com>
2024-08-15 23:53:39 -0300

    add data to catalog product

--- javascript/rocketseat/nextjs/app/catalog/product/[id]/page.tsx
+++ javascript/rocketseat/nextjs/app/catalog/product/[id]/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 interface ProductProps {
     params: {
         id: string;
@@ -8,10 +9,17 @@ export default function Product({params}: ProductProps) {
 
     // this property dont exist in the interface cause data it's loaded in the getServerSideProps
     console.log(params);
+    
+    const {id} = params;
+
+    function addToCart() {
+        console.log('Add to cart');
+    }
 
     return (
         <div>
-            <h1>Product {params.id}</h1>
+            <h1>Product {id}</h1>
+            <button onClick={addToCart}>Adicionar ao carrinho </button>
         </div>
     )
 }
