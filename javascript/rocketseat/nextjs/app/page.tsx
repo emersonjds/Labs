5c2a66d90fb355aa94e914ac9364dc74d525603c
Emerson Silva <emerson_jdss@hotmail.com>
2024-08-17 18:52:22 -0300

    understanding server components

--- javascript/rocketseat/nextjs/app/page.tsx
+++ javascript/rocketseat/nextjs/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  const response = await fetch('https://api.github.com/users/emersonjds');
+  const data = await response.json();
   return (
     <h1>
-      Project Next
+      {data.name} - {data.bio}
     </h1>
   );
 }
