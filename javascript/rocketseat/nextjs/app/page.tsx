0a29ac541a7adec3f127eb87ecad291db1c50b0f
Emerson Silva <emerson_jdss@hotmail.com>
2024-08-17 18:55:40 -0300

    adjust delay

--- javascript/rocketseat/nextjs/app/page.tsx
+++ javascript/rocketseat/nextjs/app/page.tsx
@@ -1,13 +1,9 @@
 export default async function Home() {
   // Simulate a delay
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 1500));
 
-  const response = await fetch('https://api.github.com/users/emersonjds');
+  const response = await fetch("https://api.github.com/users/emersonjds");
   const data = await response.json();
 
-  return (
-    <pre>
-      {JSON.stringify(data, null, 2)}
-    </pre>
-  );
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
