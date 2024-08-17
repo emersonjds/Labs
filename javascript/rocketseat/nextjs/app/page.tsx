5fb4583758c49ab979b1886bc3996f8e42918f19
Emerson Silva <emerson_jdss@hotmail.com>
2024-08-17 18:52:43 -0300

    simulate a delay

--- javascript/rocketseat/nextjs/app/page.tsx
+++ javascript/rocketseat/nextjs/app/page.tsx
@@ -1,8 +1,10 @@
 export default async function Home() {
+  // Simulate a delay
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
   const response = await fetch('https://api.github.com/users/emersonjds');
   const data = await response.json();
+
   return (
     <h1>
       {data.name} - {data.bio}
