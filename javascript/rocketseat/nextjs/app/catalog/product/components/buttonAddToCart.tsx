438731ae1de21c5a729231732385cd6e3e0fd3b9
Emerson Silva <emerson_jdss@hotmail.com>
2024-08-18 22:28:07 -0300

    create micro component

---
+++ javascript/rocketseat/nextjs/app/catalog/product/components/buttonAddToCart.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { useState } from "react";
+
+export default function ButtonAddToCart() {
+    const [count, setCount] = useState(0);
+
+    function addToCart() {
+        setCount(count + 1);
+    }
+
+    console.log('conunt', count);
+
+    return (
+        <div>
+            <button onClick={addToCart}>Add to cart</button>
+            <p>Count: {count}</p>
+        </div>
+    )
+}
